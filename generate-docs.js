b68a2aa037e2171deaf25fef3fda393b5608f3f6
Junjie Tang <junjie.tang@sap.com>
2020-03-11 11:28:50 +0100

    fix docs

--- generate-docs.js
+++ generate-docs.js
@@ -50,14 +50,14 @@ function replaceSearchbar(path) {
 
 function replaceUnderlinePrefixAndHtmlSuffixFromLinks(path){
   const html = fs.readFileSync(path, { encoding: 'utf8' });
-  const replaced = html.replace(/<a href="[^>]*_[^>]*.html">/gi, removeUnderlinePrefixAndHtmlSuffixFromLink);
+  const replaced = html.replace(/<a href="[^>]*_[^>]*.html[^>]*>/gi, removeUnderlinePrefixAndHtmlSuffixFromLink);
   fs.writeFileSync(path, replaced, { encoding: 'utf8' });
 }
 
 function removeUnderlinePrefixAndHtmlSuffixFromLink(aHref) {
   const i = aHref.indexOf('_');
-  // remove the first `_` and `.html`
-  return aHref.substring(0, i) +  aHref.substring( i+1, aHref.length - 7 ) + '\">'
+  const firstUnderlineRemoved = aHref.substring(0, i) +  aHref.substring( i + 1);
+  return firstUnderlineRemoved.replace('.html', '');
 }
 
 function removeUnderlinePrefixFromFileName(path){

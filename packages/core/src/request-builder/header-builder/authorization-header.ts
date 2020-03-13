95a759b9f8dff260d90ffe932ebe03a86b41e608
Frank Essenberger <frank.essenberger@sap.com>
2020-03-13 17:11:33 +0100

    some refactoring

--- packages/core/src/request-builder/header-builder/authorization-header.ts
+++ packages/core/src/request-builder/header-builder/authorization-header.ts
@@ -3,7 +3,7 @@
  */
 
 import { createLogger, errorWithCause, MapType } from '@sap-cloud-sdk/util';
-import { assoc, filter, head, identity, ifElse, isEmpty, isNil, map, path } from 'rambda';
+import { filter, head, identity, ifElse, isEmpty, isNil, map, path, pipe } from 'rambda';
 import { Destination, DestinationAuthToken, getOAuth2ClientCredentialsToken } from '../../scp-cf';
 import { ODataRequest, ODataRequestConfig } from '../request';
 
@@ -43,18 +43,18 @@ export async function addAuthorizationHeader<RequestT extends ODataRequestConfig
  * @returns The provided headers with the new authorization headers.
  */
 export const buildAndAddAuthorizationHeader = (destination: Destination) => async (headers: MapType<any>): Promise<MapType<string>> => {
+  if (destination.proxyType === 'OnPremise' && destination.authentication === 'NoAuthentication') {
+    logger.warn(
+      `You are using \'NoAuthentication\' in destiantion: ${destination.name} which is an OnPremise destination. This is a deprecated configuration, most likely you wanted to set-up \'PrincipalPropagation\' so please change the destination property to the desired authentication scheme.`
+    );
+  }
   if (destination.authentication === 'NoAuthentication' || destination.authentication === 'ClientCertificateAuthentication') {
-    if (destination.proxyType === 'OnPremise') {
-      logger.warn(
-        `You are using \'NoAuthentication\' in destiantion: ${destination.name} which is an OnPremise destination. This is a deprecated configuration and you should change it to \'PrincipalPropagation\'.`
-      );
-    }
     return headers;
   } else if (!destination.authentication) {
     logger.warn('No authentication type is specified on the destination! Assuming "NoAuthentication".');
     return headers;
   }
-  return buildAuthHeader(destination).then(authHeader => ({ ...headers, ...authHeader }));
+  return { ...headers, ...(await buildAuthHeader(destination)), ...headerForOnPremProxyAuth(destination) };
 };
 
 const authorizationHeaderFromCustomHeaders = <T extends ODataRequestConfig>(request: ODataRequest<T>): string | undefined =>
@@ -62,8 +62,6 @@ const authorizationHeaderFromCustomHeaders = <T extends ODataRequestConfig>(requ
 
 const toAuthHeaderObject = (authHeader: string) => ({ authorization: authHeader });
 
-// TODO remove addProxyHeaders from header-builder.ts
-// This would be a breaking change because it would enforce setting the authentication to PrincipalPropagation, currently also NoAuthentication also works.
 const buildAuthHeader = async (destination: Destination): Promise<MapType<any>> => {
   switch (destination.authentication) {
     case 'OAuth2SAMLBearerAssertion':
@@ -72,17 +70,11 @@ const buildAuthHeader = async (destination: Destination): Promise<MapType<any>> 
       }
       return toAuthHeaderObject(headerFromTokens(destination.authTokens));
     case 'OAuth2ClientCredentials':
-      return headerFromOAuth2ClientCredentialsDestination(destination).then(value => toAuthHeaderObject(value));
+      return toAuthHeaderObject(await headerFromOAuth2ClientCredentialsDestination(destination));
     case 'BasicAuthentication':
       return toAuthHeaderObject(headerFromBasicAuthDestination(destination));
     case 'PrincipalPropagation':
-      const proxyHeader = destination?.proxyConfiguration?.headers;
-      if (!proxyHeader || !proxyHeader['SAP-Connectivity-Authentication']) {
-        throw new Error(
-          'Principal propagation was selected in destination, but no SAP-Connectivity-Authentication bearer header was added by connectivity-service.'
-        );
-      }
-      return proxyHeader;
+      return headerForUserPropagation(destination);
     default:
       throw new Error(
         'Failed to build authorization header for the given destination. Make sure to either correctly configure your destination for principal propagation, provide both a username and a password or select "NoAuthentication" in your destination configuration.'
@@ -98,6 +90,24 @@ const throwAllTokensErrored = (authTokens: DestinationAuthToken[]) => {
       ...map(token => token.error, authTokens)
     ].join('\n')
   );
+};
+
+const headerForOnPremProxyAuth = (destination: Destination): MapType<any> => {
+  if (destination.proxyType !== 'OnPremise') {
+    return {};
+  }
+  // The connectivity service will raise an exception if it can not obtain the 'Proxy-Authorization' and the destination lookup will fail early
+  return { 'Proxy-Authorization': destination!.proxyConfiguration!.headers!['Proxy-Authorization'] };
+};
+
+const headerForUserPropagation = (destination: Destination): MapType<any> => {
+  const proxyHeaders = destination?.proxyConfiguration?.headers;
+  if (!proxyHeaders || !proxyHeaders['SAP-Connectivity-Authentication']) {
+    throw new Error(
+      'Principal propagation was selected in destination, but no SAP-Connectivity-Authentication bearer header was added by connectivity-service.'
+    );
+  }
+  return { 'SAP-Connectivity-Authentication': proxyHeaders['SAP-Connectivity-Authentication'] };
 };
 
 const headerFromOAuth2ClientCredentialsDestination = (destination: Destination): Promise<string> =>

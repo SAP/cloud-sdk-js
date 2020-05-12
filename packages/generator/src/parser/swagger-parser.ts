06c477d1f7a77b9ff6de76b05c6bc16ac5225fad
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-05-12 10:25:28 +0200

    chore: Improve openapi fix (#130)
    * Update swagger-parser.ts
    
    * Update swagger-parser.ts

--- packages/generator/src/parser/swagger-parser.ts
+++ packages/generator/src/parser/swagger-parser.ts
@@ -13,9 +13,8 @@ export function parseSwaggerFromPath(swaggerPath: PathLike): SwaggerMetadata {
 
 function parseSwaggerFile(swaggerFile: string): SwaggerMetadata {
   const swaggerMetaData = JSON.parse(swaggerFile);
-  // If the file is not swagger but openapi
-  if (swaggerMetaData.openapi) {
-    swaggerMetaData.definitions = swaggerMetaData?.components?.schemas;
-  }
+  // Get definitions from schema in case there are no definitions present, this is typically the case for openapi files (version >= 3.0.0)
+  swaggerMetaData.definitions =
+    swaggerMetaData.definitions || swaggerMetaData?.components?.schemas;
   return swaggerMetaData;
 }

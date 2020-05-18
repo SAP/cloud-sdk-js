92d9e48b40427ada5085e48898a365fb971942be
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-05-18 11:20:09 +0200

    Use common config

--- packages/core/test/request-builder/request/odata-function-import-request-config.spec.ts
+++ packages/core/test/request-builder/request/odata-function-import-request-config.spec.ts
@@ -1,9 +1,6 @@
 /* Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. */
-import {
-  FunctionImportParameter,
-  ODataFunctionImportRequestConfig,
-  oDataUri
-} from '../../../src';
+import { FunctionImportParameter, oDataUri } from '../../../src';
+import { ODataFunctionImportRequestConfig } from '../../../src/odata/common/request/odata-function-import-request-config';
 
 interface TestParameterType {
   test1: string;

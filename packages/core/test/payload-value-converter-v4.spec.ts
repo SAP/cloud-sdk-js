cd291b24077d7081f78b2804ea7ae0966878ddc3
Frank Essenberger <frank.essenberger@sap.com>
2020-05-18 15:03:20 +0200

    switch to marika poc II

--- packages/core/test/payload-value-converter-v4.spec.ts
+++ packages/core/test/payload-value-converter-v4.spec.ts
@@ -1,13 +1,12 @@
 /* Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. */
 import moment, { Duration, Moment } from 'moment';
 import {
-  tsToEdm
 } from '../src/payload-value-converter';
 import { EdmType } from '../src';
 
 
 
-//replace later with real map
+//replace later in the desrializer map
 function edmToTs(value:string,edmType:EdmType|'Edm.Date'|'Edm.Duration'|'Edm.TimeOfDay'){
   switch (edmType) {
     case 'Edm.Date':
@@ -18,6 +17,37 @@ function edmToTs(value:string,edmType:EdmType|'Edm.Date'|'Edm.Duration'|'Edm.Tim
       return edmDurationToMoment(value)
     case 'Edm.TimeOfDay':
       return edmTimeOfDay(value)
+    default:
+      throw new Error('Implementation is missing.')
+  }
+}
+
+function momentToEdmDate(value: Moment):string {
+  return value.format('YYYY-MM-DD')
+}
+
+function momentToEdmDateTimeOffsetToMoment(value: Moment):string {
+  return value.utc().format('YYYY-MM-DDTHH:mm:ss.SSS') + 'Z'
+}
+
+function momentToEdmDuration(value: Duration):string {
+  return value.toISOString();
+}
+
+function momentToEdmTimeOfDay(value: Moment):string {
+  return value.format('HH:mm:ss.SSS')
+}
+
+function tsToEdm(value:any,edmType:EdmType|'Edm.Date'|'Edm.Duration'|'Edm.TimeOfDay'){
+  switch (edmType) {
+    case 'Edm.Date':
+      return momentToEdmDate(value)
+    case 'Edm.DateTimeOffset':
+      return momentToEdmDateTimeOffsetToMoment(value)
+    case 'Edm.Duration':
+      return momentToEdmDuration(value)
+    case 'Edm.TimeOfDay':
+      return momentToEdmTimeOfDay(value)
     default:
       throw new Error('Implementation is missing.')
   }
@@ -44,6 +74,10 @@ function edmDateTimeOffsetToMoment(dateTime: string):Moment {
 }
 
 function edmDurationToMoment(value: string):Duration {
+  const durationPattern = /([\+,\-]{1,1})?P(\d+D)?T(\d{1,2}H)?(\d{1,2}M)?(\d{1,2}S)?(\d{2,2}\.\d+S)?/;
+  if(!durationPattern.test(value)){
+    throw new Error(`Provided duration value ${value} does not follow the Edm.Duration pattern: +/- P0DT0H0M0S`)
+  }
   return moment.duration(value)
 }
 
@@ -109,9 +143,12 @@ describe('edmToTs()', () => {
     expect(actual.asMilliseconds()).toBe(expected)
   })
 
-  it('should return duration 0 for wrong formatted Edm.Duration', () =>{
-    //Discuss if we should be more strict with some regex pattern
-    expect((edmToTs('someThingWrong','Edm.Duration') as Duration).asMilliseconds()).toBe(0)
+  it('should throw on a wrongly formatted Edm.Duration',()=>{
+    expect(()=>edmToTs('+P4DT23H13m50S','Edm.Duration')).toThrow(/ does not follow the Edm.Duration pattern/)
+
+    expect(()=>edmToTs('PT501S','Edm.Duration')).toThrow(/ does not follow the Edm.Duration pattern/)
+
+    expect(()=>edmToTs('P23H','Edm.Duration')).toThrow(/ does not follow the Edm.Duration pattern/)
   })
 
   it('should parse Edm.TimeOfDay to moment.utc since there is proper time object', ()=>{
@@ -134,23 +171,40 @@ describe('edmToTs()', () => {
 
 describe('tsToEdm()', () => {
   it('should convert moment to Edm.Date',()=>{
-
+    expect(tsToEdm(moment(0),'Edm.Date')).toBe('1970-01-01')
   })
 
   it('should convert moment to Edm.DateTimeOffset',()=>{
-
+    expect(tsToEdm(moment(0),'Edm.DateTimeOffset')).toBe('1970-01-01T00:00:00.000Z')
   })
 
   it('should convert moment.duration to Edm.Duration',()=>{
-
+    expect(tsToEdm(moment.duration({d:1,m:20}),'Edm.Duration')).toBe('P1DT20M')
   })
 
-
   it('should convert moment to Edm.TimeOfDay',()=>{
-
+    expect(tsToEdm(moment((17 * 3600 + 900 + 57)*1000 + 987),'Edm.TimeOfDay')).toBe('18:15:57.987')
   })
 });
 
 describe('edm to ts to edm does not lead to information loss', () => {
+it('should not lose information for Edm.Date',()=>{
+  const expected ='2020-05-13';
+  expect(tsToEdm(edmToTs(expected,'Edm.Date'),'Edm.Date')).toBe(expected)
+})
 
+  it('should not lose information for Edm.DateTimeOffset',()=>{
+    const expected ='2020-05-13T16:14:23.000Z';
+    expect(tsToEdm(edmToTs(expected,'Edm.DateTimeOffset'),'Edm.DateTimeOffset')).toBe(expected)
+  })
+
+  it('should not lose information for Edm.Duration',()=>{
+    const expected ='-P3DT6H49.987S';
+    expect(tsToEdm(edmToTs(expected,'Edm.Duration'),'Edm.Duration')).toBe(expected)
+  })
+
+  it('should not lose information for Edm.TimeOfDay',()=>{
+    const expected ='18:27:32';
+    expect(tsToEdm(edmToTs(expected,'Edm.TimeOfDay'),'Edm.TimeOfDay')).toBe(expected+".000")
+  })
 });

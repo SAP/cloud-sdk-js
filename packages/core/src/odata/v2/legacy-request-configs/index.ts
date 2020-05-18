542597a2864f6257af17a01f4e8aed228d6a9f95
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-05-18 09:40:59 +0200

    add legacy configs

--- packages/core/src/odata/v2/legacy-request-configs/index.ts
+++ packages/core/src/odata/v2/legacy-request-configs/index.ts
@@ -1,3 +1,4 @@
+/* Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. */
 export * from './odata-batch-request-config';
 export * from './odata-create-request-config';
 export * from './odata-delete-request-config';

a31d31bfcdb3d87fa478f954dabfcb8be0d1f099
Artem Kovalov <artem.kovalyov@gmail.com>
2020-06-02 23:19:26 +0200

    Replace tracking snippet

--- static/js/swa.js
+++ static/js/swa.js
@@ -1,12 +1,9 @@
 var swa = {
   pubToken: '84bf4b8e-f8b0-42e7-8c8a-e03377786443',
   baseUrl: 'https://webanalytics2.cfapps.eu10.hana.ondemand.com/tracker/',
-  owner: null,
-  // loggingEnabled: true,
-  // consentDeferTimeout: '864000',
-  consentStyle: 'popup'
+  owner: null
+  // set this to a function that returns the identifier of the tracked user
 };
-
 (function () {
   var d = document; var g = d.createElement('script'); var s = d.getElementsByTagName('script')[0]
   g.type = 'text/javascript'; g.defer = true; g.async = true; g.src = swa.baseUrl + 'js/track.js'

36d403c5cdbb153711342df558471153c41bbeba
Artem Kovalov <artem.kovalyov@gmail.com>
2020-06-08 13:11:09 +0200

    Fix linting

--- src/sap/sdk-js/BuildBadge/index.js
+++ src/sap/sdk-js/BuildBadge/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-function BuildBadge ({align}) {
+function BuildBadge ({ align }) {
   return (
     <span className={align ? 'col text--right' : ''}>
-      <a target="_blank" rel="noopener noreferrer" href="https://github.com/SAP/cloud-sdk">
-	<img src="https://github.com/SAP/cloud-sdk/workflows/build/badge.svg" alt="build" className="max-width:100%;" /></a>
+      <a target='_blank' rel='noopener noreferrer' href='https://github.com/SAP/cloud-sdk'>
+        <img src='https://github.com/SAP/cloud-sdk/workflows/build/badge.svg' alt='build' className='max-width:100%;' />
+      </a>
     </span>
   )
 }

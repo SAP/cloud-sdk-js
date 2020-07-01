e2232aebbd4db508e4d6fc79f2c75744c575b138
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-07-01 10:14:36 +0200

    Introduce oderable filter abstraction

--- packages/core/src/odata/v4/date-filter-function.ts
+++ packages/core/src/odata/v4/date-filter-function.ts
@@ -2,18 +2,15 @@
 
 import { Moment } from 'moment';
 import { EntityBase } from '../common/entity';
-import {
-  Filter,
-  FilterFunction,
-  FilterFunctionParameterType
-} from '../common/filter';
+import { FilterFunctionParameterType } from '../common/filter';
+import { OrderableFilterFunction } from '../common/filter/orderable-filter-function';
 
 /**
  * Representation of a filter function, that returns a value of type date. This supports DateTimeOffset values.
  */
 export class DateFilterFunction<
   EntityT extends EntityBase
-> extends FilterFunction<EntityT, Moment> {
+> extends OrderableFilterFunction<EntityT, Moment> {
   /**
    * Creates an instance of DateFilterFunction.
    * @param functionName - Name of the function that returns a numeric value
@@ -25,49 +22,5 @@ export class DateFilterFunction<
     parameters: FilterFunctionParameterType<EntityT>[]
   ) {
     super(functionName, parameters, 'Edm.DateTimeOffset');
-  }
-
-  /**
-   * Creates an instance of Filter for this filter function and the given value using the operator 'gt', i.e. `>`.
-   *
-   * @param value - Value to be used in the filter
-   * @param edmType - EdmType of the field to filter on
-   * @returns The resulting filter
-   */
-  greaterThan(value: Moment): Filter<EntityT, Moment> {
-    return new Filter(this, 'gt', value, this.edmType);
-  }
-
-  /**
-   * Creates an instance of Filter for this filter function and the given value using the operator 'ge', i.e. `>=`.
-   *
-   * @param value - Value to be used in the filter
-   * @param edmType - EdmType of the field to filter on
-   * @returns The resulting filter
-   */
-  greaterOrEqual(value: Moment): Filter<EntityT, Moment> {
-    return new Filter(this, 'ge', value, this.edmType);
-  }
-
-  /**
-   * Creates an instance of Filter for this filter function and the given value using the operator 'lt', i.e. `<`.
-   *
-   * @param value - Value to be used in the filter
-   * @param edmType - EdmType of the field to filter on
-   * @returns The resulting filter
-   */
-  lessThan(value: Moment): Filter<EntityT, Moment> {
-    return new Filter(this, 'lt', value, this.edmType);
-  }
-
-  /**
-   * Creates an instance of Filter for this filter function and the given value using the operator 'le', i.e. `<=`.
-   *
-   * @param value - Value to be used in the filter
-   * @param edmType - EdmType of the field to filter on
-   * @returns The resulting filter
-   */
-  lessOrEqual(value: Moment): Filter<EntityT, Moment> {
-    return new Filter(this, 'le', value, this.edmType);
   }
 }

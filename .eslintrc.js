4944d95fb3b2bdd0151f0f2bc7eb5004d48ea06f
Junjie Tang <junjie.tang@sap.com>
2020-07-07 00:32:39 +0200

    build

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
                 "**/dist/**/*",
                 "**/node_modules/**/*",
                 "test-packages/test-services/**/*",
+                "test-packages/test-services-e2e/**/*",
                 "packages/core/test/test-util/test-services/**/*"
             ]
         },

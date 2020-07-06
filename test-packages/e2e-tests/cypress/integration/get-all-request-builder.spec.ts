087bbbd36732c8f5c9bfab96ab8683ffd3b7a483
Junjie Tang <junjie.tang@sap.com>
2020-07-06 18:42:59 +0200

    e2e tests in pipeline

---
+++ test-packages/e2e-tests/cypress/integration/get-all-request-builder.spec.ts
@@ -0,0 +1,22 @@
+/* Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. */
+
+
+describe('My Test', () => {
+  it('ping index', () => {
+    cy.request('http://localhost:4004')
+      .should((response) => {
+        expect(response.status).to.eq(200)
+      })
+  });
+
+  it('ping test entity', () => {
+    cy.request('http://localhost:4004/admin/TestEntity')
+      .should((response) => {
+        expect(response.status).to.eq(200)
+      })
+      .its('body')
+      .its('value')
+      .should('be.an', 'array')
+      .and('have.length', 4)
+  })
+});

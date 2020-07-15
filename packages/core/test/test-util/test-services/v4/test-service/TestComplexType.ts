041e9292cc692bed3cb3e182931662b4566aee5e
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-07-15 15:58:25 +0200

    Formatter

--- packages/core/test/test-util/test-services/v4/test-service/TestComplexType.ts
+++ packages/core/test/test-util/test-services/v4/test-service/TestComplexType.ts
@@ -202,95 +202,77 @@ export class TestComplexTypeField<EntityT extends Entity> extends ComplexTypeFie
 
 export namespace TestComplexType {
 
-  export const _propertyMetadata: PropertyMetadata[] = [{originalName: 'StringProperty',
-name: 'stringProperty',
-type: 'Edm.String'
-},
-{originalName: 'BooleanProperty',
-name: 'booleanProperty',
-type: 'Edm.Boolean'
-},
-{originalName: 'GuidProperty',
-name: 'guidProperty',
-type: 'Edm.Guid'
-},
-{originalName: 'Int16Property',
-name: 'int16Property',
-type: 'Edm.Int16'
-},
-{originalName: 'Int32Property',
-name: 'int32Property',
-type: 'Edm.Int32'
-},
-{originalName: 'Int64Property',
-name: 'int64Property',
-type: 'Edm.Int64'
-},
-{originalName: 'DecimalProperty',
-name: 'decimalProperty',
-type: 'Edm.Decimal'
-},
-{originalName: 'SingleProperty',
-name: 'singleProperty',
-type: 'Edm.Single'
-},
-{originalName: 'DoubleProperty',
-name: 'doubleProperty',
-type: 'Edm.Double'
-},
-{originalName: 'FloatProperty',
-name: 'floatProperty',
-type: 'Edm.Float'
-},
-{originalName: 'TimeProperty',
-name: 'timeProperty',
-type: 'Edm.Time'
-},
-{originalName: 'DateTimeProperty',
-name: 'dateTimeProperty',
-type: 'Edm.DateTime'
-},
-{originalName: 'DateTimeOffSetProperty',
-name: 'dateTimeOffSetProperty',
-type: 'Edm.DateTimeOffset'
-},
-{originalName: 'ByteProperty',
-name: 'byteProperty',
-type: 'Edm.Byte'
-},
-{originalName: 'SByteProperty',
-name: 'sByteProperty',
-type: 'Edm.SByte'
-},
-{originalName: 'ComplexTypeProperty',
-name: 'complexTypeProperty',
-type: TestNestedComplexType
-},
-{originalName: 'BaseStringProperty',
-name: 'baseStringProperty',
-type: 'Edm.String'
-}];
+  export const _propertyMetadata: PropertyMetadata[] = [{
+    originalName: 'StringProperty',
+    name: 'stringProperty',
+    type: 'Edm.String'
+  }, {
+    originalName: 'BooleanProperty',
+    name: 'booleanProperty',
+    type: 'Edm.Boolean'
+  }, {
+    originalName: 'GuidProperty',
+    name: 'guidProperty',
+    type: 'Edm.Guid'
+  }, {
+    originalName: 'Int16Property',
+    name: 'int16Property',
+    type: 'Edm.Int16'
+  }, {
+    originalName: 'Int32Property',
+    name: 'int32Property',
+    type: 'Edm.Int32'
+  }, {
+    originalName: 'Int64Property',
+    name: 'int64Property',
+    type: 'Edm.Int64'
+  }, {
+    originalName: 'DecimalProperty',
+    name: 'decimalProperty',
+    type: 'Edm.Decimal'
+  }, {
+    originalName: 'SingleProperty',
+    name: 'singleProperty',
+    type: 'Edm.Single'
+  }, {
+    originalName: 'DoubleProperty',
+    name: 'doubleProperty',
+    type: 'Edm.Double'
+  }, {
+    originalName: 'FloatProperty',
+    name: 'floatProperty',
+    type: 'Edm.Float'
+  }, {
+    originalName: 'TimeProperty',
+    name: 'timeProperty',
+    type: 'Edm.Time'
+  }, {
+    originalName: 'DateTimeProperty',
+    name: 'dateTimeProperty',
+    type: 'Edm.DateTime'
+  }, {
+    originalName: 'DateTimeOffSetProperty',
+    name: 'dateTimeOffSetProperty',
+    type: 'Edm.DateTimeOffset'
+  }, {
+    originalName: 'ByteProperty',
+    name: 'byteProperty',
+    type: 'Edm.Byte'
+  }, {
+    originalName: 'SByteProperty',
+    name: 'sByteProperty',
+    type: 'Edm.SByte'
+  }, {
+    originalName: 'ComplexTypeProperty',
+    name: 'complexTypeProperty',
+    type: TestNestedComplexType
+  }, {
+    originalName: 'BaseStringProperty',
+    name: 'baseStringProperty',
+    type: 'Edm.String'
+  }];
 
   export function build(json: { [keys: string]: FieldType | TestNestedComplexType }): TestComplexType {
     return deserializeComplexType(json, TestComplexType);
-    // return createComplexType(json, {
-    //   StringProperty: (stringProperty: string) => ({ stringProperty: edmToTs(stringProperty, 'Edm.String') }),
-    //   BooleanProperty: (booleanProperty: boolean) => ({ booleanProperty: edmToTs(booleanProperty, 'Edm.Boolean') }),
-    //   GuidProperty: (guidProperty: string) => ({ guidProperty: edmToTs(guidProperty, 'Edm.Guid') }),
-    //   Int16Property: (int16Property: number) => ({ int16Property: edmToTs(int16Property, 'Edm.Int16') }),
-    //   Int32Property: (int32Property: number) => ({ int32Property: edmToTs(int32Property, 'Edm.Int32') }),
-    //   Int64Property: (int64Property: BigNumber) => ({ int64Property: edmToTs(int64Property, 'Edm.Int64') }),
-    //   DecimalProperty: (decimalProperty: BigNumber) => ({ decimalProperty: edmToTs(decimalProperty, 'Edm.Decimal') }),
-    //   SingleProperty: (singleProperty: number) => ({ singleProperty: edmToTs(singleProperty, 'Edm.Single') }),
-    //   DoubleProperty: (doubleProperty: number) => ({ doubleProperty: edmToTs(doubleProperty, 'Edm.Double') }),
-    //   FloatProperty: (floatProperty: number) => ({ floatProperty: edmToTs(floatProperty, 'Edm.Float') }),
-    //   TimeProperty: (timeProperty: Time) => ({ timeProperty: edmToTs(timeProperty, 'Edm.Time') }),
-    //   DateTimeProperty: (dateTimeProperty: Moment) => ({ dateTimeProperty: edmToTs(dateTimeProperty, 'Edm.DateTime') }),
-    //   DateTimeOffSetProperty: (dateTimeOffSetProperty: Moment) => ({ dateTimeOffSetProperty: edmToTs(dateTimeOffSetProperty, 'Edm.DateTimeOffset') }),
-    //   ByteProperty: (byteProperty: number) => ({ byteProperty: edmToTs(byteProperty, 'Edm.Byte') }),
-    //   SByteProperty: (sByteProperty: number) => ({ sByteProperty: edmToTs(sByteProperty, 'Edm.SByte') }),
-    //   ComplexTypeProperty: (complexTypeProperty: TestNestedComplexType) => ({ complexTypeProperty: TestNestedComplexType.build(complexTypeProperty) }),
-    //   BaseStringProperty: (baseStringProperty: string) => ({ baseStringProperty: edmToTs(baseStringProperty, 'Edm.String') })
-    // });
   }
 }

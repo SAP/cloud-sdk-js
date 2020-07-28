7f8dbcedcc17d26f3fc5441536f2cfba3552ad01
Frank Essenberger <frank.essenberger@sap.com>
2020-07-28 11:29:06 +0200

    use base

--- packages/core/src/odata/v2/request-builder/function-import-request-builder.ts
+++ packages/core/src/odata/v2/request-builder/function-import-request-builder.ts
@@ -1,14 +1,8 @@
 /* Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. */
 
-import { DestinationOptions } from '../../../scp-cf';
-import {
-  Destination,
-  DestinationNameAndJwt
-} from '../../../scp-cf/destination-service-types';
-import { MethodRequestBuilderBase } from '../../common/request-builder/request-builder-base';
-import { ODataFunctionImportRequestConfig } from '../../common/request/odata-function-import-request-config';
 import { FunctionImportParameters, RequestMethodType } from '../../common';
 import { oDataUri } from '../uri-conversion';
+import { ActionAndFunctionImportRequestBuilderBase } from '../../common/request-builder/action-and-function-import-request-builder-base';
 
 /**
  * Create OData request to execute a function import.
@@ -19,9 +13,7 @@ export class FunctionImportRequestBuilder<
   // reuse
   ParametersT,
   ReturnT
-> extends MethodRequestBuilderBase<
-  ODataFunctionImportRequestConfig<ParametersT>
-> {
+> extends ActionAndFunctionImportRequestBuilderBase<ParametersT, ReturnT> {
   /**
    * Creates an instance of FunctionImportRequestBuilder.
    * @param method - HTTP method to be used for the request
@@ -38,28 +30,12 @@ export class FunctionImportRequestBuilder<
     parameters: FunctionImportParameters<ParametersT>
   ) {
     super(
-      new ODataFunctionImportRequestConfig(
-        method,
-        defaultServicePath,
-        functionImportName,
-        parameters,
-        oDataUri
-      )
+      method,
+      defaultServicePath,
+      functionImportName,
+      responseTransformer,
+      parameters,
+      oDataUri
     );
-  }
-
-  /**
-   * Execute request
-   * @param destination - Destination to execute the request against
-   * @param options - Options to employ when fetching destinations
-   * @returns A promise resolving to the requested return type
-   */
-  async execute(
-    destination: Destination | DestinationNameAndJwt,
-    options?: DestinationOptions
-  ): Promise<ReturnT> {
-    return this.build(destination, options)
-      .then(request => request.execute())
-      .then(({ data }) => this.responseTransformer(data));
   }
 }

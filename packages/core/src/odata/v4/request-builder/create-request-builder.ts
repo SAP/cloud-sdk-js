62a84b7821dd3d6083723261a3d1af60d7a4ee68
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-07-29 16:46:59 +0200

    Introduce response data accessors

--- packages/core/src/odata/v4/request-builder/create-request-builder.ts
+++ packages/core/src/odata/v4/request-builder/create-request-builder.ts
@@ -1,6 +1,6 @@
 /* Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. */
 
-import { errorWithCause, MapType } from '@sap-cloud-sdk/util';
+import { errorWithCause } from '@sap-cloud-sdk/util';
 import { Constructable, EntityIdentifiable, Link } from '../../common';
 import { MethodRequestBuilderBase } from '../../common/request-builder/request-builder-base';
 import { ODataCreateRequestConfig } from '../../common/request/odata-create-request-config';
@@ -13,7 +13,7 @@ import {
   DestinationNameAndJwt
 } from '../../../scp-cf/destination-service-types';
 import { oDataUri } from '../uri-conversion';
-import { HttpReponse } from '../../../http-client';
+import { getSingleResult } from './response-data-accessor';
 /**
  * Create OData request to create an entity.
  *
@@ -87,7 +87,7 @@ export class CreateRequestBuilder<EntityT extends Entity>
       .then(request => request.execute())
       .then(response =>
         deserializeEntity(
-          extractData(response),
+          getSingleResult(response.data),
           this._entityConstructor,
           response.headers
         )
@@ -96,8 +96,4 @@ export class CreateRequestBuilder<EntityT extends Entity>
         Promise.reject(errorWithCause('Create request failed!', error))
       );
   }
-}
-
-function extractData(response: HttpReponse): MapType<any> {
-  return response.data;
 }
--- packages/core/src/odata/v4/request-builder/get-by-key-request-builder.ts
+++ packages/core/src/odata/v4/request-builder/get-by-key-request-builder.ts
@@ -19,6 +19,7 @@ import { ODataGetByKeyRequestConfig } from '../../common/request/odata-get-by-ke
 import { Expandable } from '../../common/expandable';
 import { oDataUri } from '../uri-conversion';
 import { HttpReponse } from '../../../http-client';
+import { getSingleResult } from './response-data-accessor';
 /**
  * Create OData request to get a single entity based on its key properties. A `GetByKeyRequestBuilder` allows to restrict the response to a selection of fields,
  * where no selection is equal to selecting all fields.
@@ -75,7 +76,7 @@ export class GetByKeyRequestBuilder<EntityT extends Entity>
       .then(request => request.execute())
       .then(response =>
         deserializeEntity(
-          extractData(response),
+          getSingleResult(response.data),
           this._entityConstructor,
           response.headers
         )

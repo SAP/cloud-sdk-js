9117e533a78e41c5d40bc293606011960f0c131b
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-07-30 16:04:21 +0200

    Fix tests

--- packages/core/src/odata/v2/request-builder/response-data-accessor.ts
+++ packages/core/src/odata/v2/request-builder/response-data-accessor.ts
@@ -1,6 +1,6 @@
 /* Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. */
 
-export function getCollectionResult(data): any[] {
+export function getCollectionResult(data): any[] | any {
   return data.d.results;
 }
 
@@ -9,10 +9,12 @@ export function isCollectionResult(data): boolean {
 }
 
 export function getSingleResult(data): Record<string, any> {
-  return isSingleResultAsCollection(data) ? getCollectionResult(data) : data.d;
+  return isSingleResultAsCollection(data)
+    ? getCollectionResult(data)
+    : data?.d || {};
 }
 
 // Workaround to be compatible with services that wrongly implement the OData v2 protocol and serve single responses in the same format as collections
 function isSingleResultAsCollection(data): boolean {
-  return data.d.results && Object.keys(data.d).length > 1;
+  return !!data.d?.results && !isCollectionResult(data);
 }

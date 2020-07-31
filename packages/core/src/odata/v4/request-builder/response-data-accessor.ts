c8aaa7e194c64d83e2d9a6e7cbfec5870747dc7f
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-07-31 10:45:27 +0200

    Add warnings

--- packages/core/src/odata/v4/request-builder/response-data-accessor.ts
+++ packages/core/src/odata/v4/request-builder/response-data-accessor.ts
@@ -1,13 +1,38 @@
 /* Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. */
 
+import { createLogger } from '@sap-cloud-sdk/util';
+
+const logger = createLogger({
+  package: 'core',
+  messageContext: 'response-data-accessor'
+});
+
 export function getCollectionResult(data): any[] {
-  return data.value;
+  validateCollectionResult(data);
+  return data.value || [];
 }
 
 export function isCollectionResult(data): boolean {
   return Array.isArray(data.value);
 }
 
+function validateCollectionResult(data): void {
+  if (!isCollectionResult(data)) {
+    logger.warn(
+      'The given reponse data does not have the standard OData v4 format for collections.'
+    );
+  }
+}
+
 export function getSingleResult(data): Record<string, any> {
-  return data;
+  validateSingleResult(data);
+  return data || {};
+}
+
+function validateSingleResult(data): void {
+  if (!data) {
+    logger.warn(
+      'The given reponse data does not have the standard OData v4 format for single results.'
+    );
+  }
 }

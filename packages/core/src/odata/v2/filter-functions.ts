5c09d6c22fbdff3063cd38501e58c14fdaef23da
Junjie Tang <junjie.tang@sap.com>
2020-08-05 14:55:52 +0200

    lint

--- packages/core/src/odata/v2/filter-functions.ts
+++ packages/core/src/odata/v2/filter-functions.ts
@@ -1,6 +1,12 @@
 /* Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. */
 
-import { Field, StringFilterFunction, BooleanFilterFunction, EntityBase, NumberFilterFunction } from '../common';
+import {
+  Field,
+  StringFilterFunction,
+  BooleanFilterFunction,
+  EntityBase,
+  NumberFilterFunction
+} from '../common';
 import { filterFunction } from '../common/filter/filter-function';
 import { filterFunctions as filterFunctionsCommon } from '../common/filter/filter-functions';
 import { Entity } from './entity';

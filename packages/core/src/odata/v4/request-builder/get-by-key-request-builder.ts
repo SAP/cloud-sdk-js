918ff500035dcc7f2e7d1ecaea9faf05f3dada33
Frank Essenberger <56544999+FrankEssenberger@users.noreply.github.com>
2020-08-18 13:49:16 +0200

    Update packages/core/src/odata/v4/request-builder/get-by-key-request-builder.ts
    Co-authored-by: Artem Kovalov <artem.kovalyov@gmail.com>

--- packages/core/src/odata/v4/request-builder/get-by-key-request-builder.ts
+++ packages/core/src/odata/v4/request-builder/get-by-key-request-builder.ts
@@ -21,7 +21,7 @@ import { oDataUri } from '../uri-conversion';
 import { HttpReponse } from '../../../http-client';
 import { getSingleResult } from './response-data-accessor';
 /**
- * Create OData request to get a single entity based on its key properties.
+ * Create an OData request to get a single entity based on its key properties.
  * The properties available in the response can be restricted by creating a [[GetByKeyRequestBuilder.select selection]], where no selection is equal to selecting all fields of the entity.
  * Navigational properties need to expanded explicitly by [[GetAllRequestBuilder.expand]].
  * where no selection is equal to selecting all fields.

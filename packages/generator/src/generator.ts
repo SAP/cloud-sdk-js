08e90eafc71c68db216efa27a25b30170d6898c1
Frank Essenberger <frank.essenberger@sap.com>
2020-08-26 16:13:07 +0200

    create batch only for services with entities

--- packages/generator/src/generator.ts
+++ packages/generator/src/generator.ts
@@ -214,12 +214,15 @@ export async function generateSourcesForService(
 
   otherFile(serviceDir, 'tsconfig.json', tsConfig(), options.forceOverwrite);
 
-  sourceFile(
-    serviceDir,
-    'BatchRequest',
-    batchSourceFile(service),
-    options.forceOverwrite
-  );
+  if(service.entities && service.entities.length > 0) {
+    logger.info(`Generating batch file service: ${service.namespace}...`);
+    sourceFile(
+      serviceDir,
+      'BatchRequest',
+      batchSourceFile(service),
+      options.forceOverwrite
+    );
+  }
 
   service.entities.forEach(entity => {
     logger.info(`Generating entity: ${entity.className}...`);

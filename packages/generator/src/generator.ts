6fe2d7beb3ab443b595f310af48941083f7b9957
Frank Essenberger <frank.essenberger@sap.com>
2020-08-26 16:47:30 +0200

    linter

--- packages/generator/src/generator.ts
+++ packages/generator/src/generator.ts
@@ -215,7 +215,9 @@ export async function generateSourcesForService(
   otherFile(serviceDir, 'tsconfig.json', tsConfig(), options.forceOverwrite);
 
   if (service.entities && service.entities.length > 0) {
-    logger.info(`Generating batch request builder for: ${service.namespace}...`);
+    logger.info(
+      `Generating batch request builder for: ${service.namespace}...`
+    );
     sourceFile(
       serviceDir,
       'BatchRequest',

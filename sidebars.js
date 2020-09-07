9151a1682bb36aba064c56ec1290cd39d7b0afdb
Swati Nair <swati.nair@sap.com>
2020-09-07 11:59:58 +0530

    Test Commit
    Change-Id: I0422840369172904378dcda50f64dd0a08b825e3
    Signed-off-by: Swati Nair <swati.nair@sap.com>

--- sidebars.js
+++ sidebars.js
@@ -66,15 +66,15 @@ module.exports = {
             type: 'category',
             label: 'SDK extensions',
             items: [
-              'java/features/extensions/extensions-supported-by-sap-cloud-sdk-for-java'
-              // {
-              //   type: 'category',
-              //   label: 'Extension library',
-              //   items: [
-              //     'java/features/extensions/extension-library/curconv/sap-currency-conversion-extension-library-for-cloud-sdk-for-java',
+              'java/features/extensions/extensions-supported-by-sap-cloud-sdk-for-java',
+               {
+                 type: 'category',
+                 label: 'Extension library',
+                 items: [
+                   'java/features/extensions/extension-library/curconv/sap-currency-conversion-extension-library-for-cloud-sdk-for-java',
               //     'java/features/extensions/extension-library/blockchain/sap-blockchain-extension-library-for-cloud-sdk-for-java',
-              //   ]
-              // }
+                 ]
+               }
             ]
           }
         ]

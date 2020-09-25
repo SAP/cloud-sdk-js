140b95ff364e8562f12ab7c903a42968b8241262
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-09-25 15:39:31 +0200

    Delete response-data-accessor.ts

--- packages/core/src/odata/common/response-data-accessor.ts
+++
@@ -1,7 +0,0 @@
-/* Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. */
-
-export interface ResponseDataAccessor {
-  getCollectionResult: (data) => any[];
-  isCollectionResult: (data) => boolean;
-  getSingleResult: (data: any) => Record<string, any>;
-}

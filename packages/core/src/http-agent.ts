2abfa0f8de2b8c1a7787e1798827b2d10bce482d
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-09-25 11:49:34 +0200

    Save file

--- packages/core/src/http-agent.ts
+++ packages/core/src/http-agent.ts
@@ -2,7 +2,7 @@
 
 import * as http from 'http';
 import * as https from 'https';
-import { createLogger } from '@sap-cloud-sdk/util';
+import { createLogger, last } from '@sap-cloud-sdk/util';
 import { assoc, pipe } from 'rambda';
 import { Destination, DestinationCertificate } from './scp-cf';
 import { proxyAgent } from './util/proxy-util';

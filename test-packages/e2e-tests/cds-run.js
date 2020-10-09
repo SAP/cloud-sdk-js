eb4c1d047c07ccafa4f3e8d7b0c172a49bf66006
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-10-09 15:20:27 +0200

    Add deploy

--- test-packages/e2e-tests/cds-run.js
+++ test-packages/e2e-tests/cds-run.js
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { spawn } = require('child_process');
 
-spawn('cds', ['run'], {
-  stdio: ['inherit', 'inherit', 'inherit']
-});
+spawn('cds', ['run'], { stdio: 'inherit' });

3a401827bcae8f28b044a7d483096c318d56025f
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-10-13 09:50:18 +0200

    Update packages/core/src/odata/common/request-builder/batch/batch-response-parser.ts

--- packages/core/src/odata/common/request-builder/batch/batch-response-parser.ts
+++ packages/core/src/odata/common/request-builder/batch/batch-response-parser.ts
@@ -149,7 +149,7 @@ export function parseHttpCode(response: string): number {
  * @param response The string representation of a single response.
  * @returns The parsed JSON representation of the response body.
  */
-function parseResponseBody(response: string): Record<string, any> | any[] {
+function parseResponseBody(response: string): Record<string, any> {
   const responseBody = getResponseBody(response);
   if (responseBody) {
     try {

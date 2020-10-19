239bb0929bb9d477ba6df281fe9c1dc85d202dd7
Junjie Tang <junjie.tang@sap.com>
2020-10-19 22:32:24 +0200

    update service

--- packages/generator-rest/test/poc/promotion.service.ts
+++ packages/generator-rest/test/poc/promotion.service.ts
@@ -1,17 +1,16 @@
-import { AxiosRequestConfig } from 'axios';
-import { buildAxiosRequestConfig } from '@sap-cloud-sdk/core';
 import {
-  CalculationApi,
-  PriceCalculateResponse
-} from './generated/calculation';
+  PriceCalculateResponse, CalculationApi
+} from './generated-v3/PriceCalculation';
 import { transaction } from './promotion-data';
-import { CalculationApiWrapper } from './generated/calculation/wrapper';
-import { CalculationApiRequestBuilder } from './generated/calculation/request-builder';
+// import { PriceCalculationApiRequestBuilder } from './generated-v3/PriceCalculation/request-builder';
+import { buildAxiosRequestConfig } from '@sap-cloud-sdk/core';
+import { AxiosRequestConfig } from 'axios';
+import { PriceCalculationApiRequestBuilder } from './generated-v3/PriceCalculation/request-builder';
 
 const token = 'ask the team';
 
 export async function getPromotionV3(): Promise<PriceCalculateResponse> {
-  return CalculationApiRequestBuilder.calculateViaRestWithTenant(
+  return PriceCalculationApiRequestBuilder.calculateViaRestWithTenant(
     'oppsapihub',
     transaction
   )
@@ -24,38 +23,38 @@ export async function getPromotionV3(): Promise<PriceCalculateResponse> {
     .then(response => response.data);
 }
 
-export async function getPromotionV2(): Promise<PriceCalculateResponse> {
-  return (
-    CalculationApiWrapper.calculateViaRestWithTenant('oppsapihub', transaction)
-      // TODO more flexible api is needed instead of execute function only
-      .execute(
-        {
-          destinationName: 'VLAB'
-        },
-        {
-          headers: {
-            apikey: token
-          }
-        }
-      )
-      .then(response => response.data)
-  );
-}
-
-export async function getPromotion(): Promise<PriceCalculateResponse> {
-  const requestConfig: AxiosRequestConfig = await buildAxiosRequestConfig({
-    destinationName: 'VLAB'
-  });
-  const calculationApi = new CalculationApi({
-    basePath: requestConfig.baseURL
-  });
-  return calculationApi
-    .calculateViaRestWithTenant('oppsapihub', transaction, {
-      // TODO type: any, additional wrapper function or modify the generated file?
-      ...requestConfig,
-      headers: {
-        apikey: token
-      }
-    })
-    .then(response => response.data);
-}
+// export async function getPromotionV2(): Promise<PriceCalculateResponse> {
+//   return (
+//     CalculationApiWrapper.calculateViaRestWithTenant('oppsapihub', transaction)
+//       // TODO more flexible api is needed instead of execute function only
+//       .execute(
+//         {
+//           destinationName: 'VLAB'
+//         },
+//         {
+//           headers: {
+//             apikey: token
+//           }
+//         }
+//       )
+//       .then(response => response.data)
+//   );
+// }
+//
+// export async function getPromotion(): Promise<PriceCalculateResponse> {
+//   const requestConfig: AxiosRequestConfig = await buildAxiosRequestConfig({
+//     destinationName: 'VLAB'
+//   });
+//   const calculationApi = new CalculationApi({
+//     basePath: requestConfig.baseURL
+//   });
+//   return calculationApi
+//     .calculateViaRestWithTenant('oppsapihub', transaction, {
+//       // TODO type: any, additional wrapper function or modify the generated file?
+//       ...requestConfig,
+//       headers: {
+//         apikey: token
+//       }
+//     })
+//     .then(response => response.data);
+// }

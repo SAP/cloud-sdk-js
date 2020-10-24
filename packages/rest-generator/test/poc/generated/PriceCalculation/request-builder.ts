841ebdc02f2948a6c1768b06bed3205e7a8fd349
Junjie Tang <junjie.tang@sap.com>
2020-10-24 10:38:27 +0200

    regenerate vdm

--- packages/rest-generator/test/poc/generated/PriceCalculation/request-builder.ts
+++ packages/rest-generator/test/poc/generated/PriceCalculation/request-builder.ts
@@ -3,47 +3,23 @@
  *
  * This is a generated file powered by the SAP Cloud SDK for JavaScript.
  */
-import {
-  Destination,
-  DestinationNameAndJwt,
-  RestRequestBuilder,
-  RestRequestConfig
-} from '@sap-cloud-sdk/core';
+import { Destination, DestinationNameAndJwt, RestRequestBuilder, RestRequestConfig } from '@sap-cloud-sdk/core';
 import { AxiosRequestConfig } from 'axios';
 import { PriceCalculationApi, PriceCalculate } from './api';
 
 export class PriceCalculationApiRequestBuilder {
-  static calculateViaRestWithTenant(
-    tenantName: string,
-    priceCalculate: PriceCalculate
-  ): CalculateViaRestWithTenantRequestBuilder {
-    return new CalculateViaRestWithTenantRequestBuilder(
-      new RestRequestConfig(),
-      tenantName,
-      priceCalculate
-    );
+  static calculateViaRestWithTenant(tenantName: string, priceCalculate: PriceCalculate): CalculateViaRestWithTenantRequestBuilder {
+    return new CalculateViaRestWithTenantRequestBuilder(new RestRequestConfig(), tenantName, priceCalculate);
   }
 }
 
 export class CalculateViaRestWithTenantRequestBuilder extends RestRequestBuilder {
-  constructor(
-    public requestConfig: RestRequestConfig,
-    public tenantName: string,
-    public priceCalculate: PriceCalculate
-  ) {
+  constructor(public requestConfig: RestRequestConfig, public tenantName: string, public priceCalculate: PriceCalculate) {
     super(requestConfig);
   }
 
   async execute(destination: Destination | DestinationNameAndJwt) {
-    const requestConfig: AxiosRequestConfig = await this.buildRequestConfig(
-      destination
-    );
-    return new PriceCalculationApi({
-      basePath: requestConfig.baseURL
-    }).calculateViaRestWithTenant(
-      this.tenantName,
-      this.priceCalculate,
-      requestConfig
-    );
+    const requestConfig: AxiosRequestConfig = await this.buildRequestConfig(destination);
+    return new PriceCalculationApi({ basePath: requestConfig.baseURL }).calculateViaRestWithTenant(this.tenantName, this.priceCalculate, requestConfig);
   }
 }

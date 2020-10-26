7aa2af87e8661ecd21623be6da359f28733d60ab
Junjie Tang <junjie.tang@sap.com>
2020-10-26 11:44:00 +0100

    fix a test

--- packages/core/test/http-client/http-client.spec.ts
+++ packages/core/test/http-client/http-client.spec.ts
@@ -1,6 +1,7 @@
 import https from 'https';
 import Axios from 'axios';
 import nock from 'nock';
+import { createLogger } from '@sap-cloud-sdk/util';
 import {
   addDestinationToRequestConfig,
   buildHttpRequest,
@@ -11,7 +12,6 @@ import {
   HttpRequest,
   Protocol
 } from '../../src';
-import { createLogger } from '@sap-cloud-sdk/util';
 
 describe('generic http client', () => {
   const httpsDestination: Destination = {
@@ -80,8 +80,14 @@ describe('generic http client', () => {
       });
       const warnSpy = jest.spyOn(logger, 'warn');
 
-      await buildHttpRequest(httpsDestination, {'authorization' : 'abc', 'sap-client': '001', 'SAP-Connectivity-SCC-Location_ID': 'efg'})
-      expect(warnSpy).toBeCalledWith('The custom headers are provided with the keys: authorization,sap-client,SAP-Connectivity-SCC-Location_ID.');
+      await buildHttpRequest(httpsDestination, {
+        authorization: 'abc',
+        'sap-client': '001',
+        'SAP-Connectivity-SCC-Location_ID': 'efg'
+      });
+      expect(warnSpy).toBeCalledWith(
+        'The custom headers are provided with the keys: authorization,sap-client,SAP-Connectivity-SCC-Location_ID.'
+      );
     });
 
     it('throws useful error messages when finding the destination fails', async () => {

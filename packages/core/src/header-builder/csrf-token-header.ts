a0b63b36091aaa065d29dd39fc69cf1d3d27a083
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-11-04 17:40:01 +0100

    Lint

--- packages/core/src/header-builder/csrf-token-header.ts
+++ packages/core/src/header-builder/csrf-token-header.ts
@@ -66,10 +66,7 @@ function axiosWorkaround<T extends HttpRequestConfig>(
   axiosConfig: Partial<T>,
   destination: Destination | DestinationNameAndJwt
 ) {
-  if (
-    error.request?._isRedirect &&
-    error.request?._options?.path
-  ) {
+  if (error.request?._isRedirect && error.request?._options?.path) {
     logger.warn(
       'Csrf fetch was redirected and failed. This might be a bug in the underlying request library (https://github.com/axios/axios/issues/3369).\nRetrying with full configuration.'
     );

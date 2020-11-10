947532a3bc2d44b324cf4bbfa236af70e37d0197
Frank Essenberger <frank.essenberger@sap.com>
2020-11-10 18:15:14 +0100

    first version

--- packages/core/src/odata-v2/request-builder/delete-request-builder.ts
+++ packages/core/src/odata-v2/request-builder/delete-request-builder.ts
@@ -1,29 +1,18 @@
-import { errorWithCause } from '@sap-cloud-sdk/util';
 import {
   Constructable,
-  EntityIdentifiable,
-  FieldType,
-  MethodRequestBuilderBase,
-  ODataDeleteRequestConfig
+  DeleteRequestBuilderBase,
+  FieldType
 } from '../../odata-common';
 import { EntityV2 } from '../entity';
-import {
-  DestinationOptions,
-  Destination,
-  DestinationNameAndJwt
-} from '../../connectivity/scp-cf';
 import { oDataUriV2 } from '../uri-conversion';
 /**
  * Create OData query to delete an entity.
  *
  * @typeparam EntityT - Type of the entity to be deleted
  */
-export class DeleteRequestBuilderV2<EntityT extends EntityV2>
-  extends MethodRequestBuilderBase<ODataDeleteRequestConfig<EntityT>>
-  implements EntityIdentifiable<EntityT> {
-  readonly _entityConstructor: Constructable<EntityT>;
-  readonly _entity: EntityT;
-
+export class DeleteRequestBuilderV2<
+  EntityT extends EntityV2
+> extends DeleteRequestBuilderBase<EntityT> {
   /**
    * Creates an instance of DeleteRequestBuilder. If the entity is passed, version identifier will also be added.
    *
@@ -34,18 +23,7 @@ export class DeleteRequestBuilderV2<EntityT extends EntityV2>
     entityConstructor: Constructable<EntityT>,
     keysOrEntity: Record<string, FieldType> | EntityV2
   ) {
-    super(new ODataDeleteRequestConfig(entityConstructor, oDataUriV2));
-    this._entityConstructor = entityConstructor;
-
-    if (keysOrEntity instanceof EntityV2) {
-      this.requestConfig.keys = oDataUriV2.getEntityKeys(
-        keysOrEntity,
-        entityConstructor
-      );
-      this.setVersionIdentifier(keysOrEntity.versionIdentifier);
-    } else {
-      this.requestConfig.keys = keysOrEntity;
-    }
+    super(entityConstructor, oDataUriV2, keysOrEntity);
   }
 
   /**
@@ -57,35 +35,6 @@ export class DeleteRequestBuilderV2<EntityT extends EntityV2>
   setVersionIdentifier(eTag: string): this {
     this.requestConfig.eTag = eTag;
     return this;
-  }
-
-  /**
-   * Instructs the request to force an overwrite of the entity by sending an 'If-Match: *' header instead of sending the ETag version identifier.
-   *
-   * @returns this The request itself to ease chaining while executing the request
-   */
-  ignoreVersionIdentifier(): this {
-    this.requestConfig.versionIdentifierIgnored = true;
-    return this;
-  }
-
-  /**
-   * Execute query.
-   *
-   * @param destination - Destination to execute the request against
-   * @param options - Options to employ when fetching destinations
-   * @returns A promise resolving once the entity was deleted
-   */
-  async execute(
-    destination: Destination | DestinationNameAndJwt,
-    options?: DestinationOptions
-  ): Promise<void> {
-    return this.build(destination, options)
-      .then(request => request.execute())
-      .then(() => Promise.resolve())
-      .catch(error =>
-        Promise.reject(errorWithCause('OData delete request failed!', error))
-      );
   }
 }
 

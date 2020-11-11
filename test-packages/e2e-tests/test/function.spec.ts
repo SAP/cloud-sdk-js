4c656088acd17f01698a69cf615ff99a39d9310c
Florian Richter <florian.richter@sap.com>
2020-11-11 10:38:45 +0100

    Add tests for function import with and w/o params

--- test-packages/e2e-tests/test/function.spec.ts
+++ test-packages/e2e-tests/test/function.spec.ts
@@ -3,9 +3,21 @@ import { returnSapCloudSdk } from '@sap-cloud-sdk/test-services-e2e/v4/admin-ser
 const url = 'http://localhost:4004/';
 const destination = { url };
 
-describe('Request builder test', () => {
-  xit('should', async () => {
+fdescribe('Request builder test', () => {
+  it('should correctly use function import without params', async () => {
     const generatedUrl = await returnSapCloudSdk({}).url(destination);
+    expect(generatedUrl).toBe(`${url}/returnSapCloudSdk()?$format=json`);
+    const response = await returnSapCloudSdk({}).execute(destination);
+    expect(response).toBe('SapCloudSdk');
+  });
+
+  it('should correctly use function import with params', async () => {
+    const generatedUrl = await returnSapCloudSdk({ Int: 3, Str: 'test' }).url(
+      destination
+    );
+    expect(generatedUrl).toBe(
+      `${url}/returnSapCloudSdk(Int=3,Str=test)?$format=json`
+    );
     const response = await returnSapCloudSdk({}).execute(destination);
     expect(response).toBe('SapCloudSdk');
   });

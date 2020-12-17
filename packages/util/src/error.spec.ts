dbf0b2dd03885b4d304c65d6669dee46fc8d1aa5
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-12-17 16:16:48 +0100

    Add tests

--- packages/util/src/error.spec.ts
+++ packages/util/src/error.spec.ts
@@ -1,11 +1,34 @@
-import { errorWithCause } from './error';
+import { ErrorWithCause, errorWithCause } from './error';
 
-describe('SdkError', () => {
+describe('errorWithCause', () => {
   it("creates a new error and appends the cause's stacktrace", () => {
     const cause = new Error('cause');
     const wrapper = errorWithCause('wrapper', cause);
     expect(wrapper instanceof Error).toBeTruthy();
     expect(wrapper.message).toBe('wrapper');
     expect(wrapper.stack).toContain('Caused by:\nError: cause');
+  });
+});
+
+describe('ErrorWithCause', () => {
+  it('creates an error', () => {
+    const err = new ErrorWithCause('message', new Error('rootCause'));
+    expect(err).toBeInstanceOf(Error);
+    expect(err).toBeInstanceOf(ErrorWithCause);
+  });
+
+  it('creates an error with cause', () => {
+    const err = new ErrorWithCause('message', new Error('rootCause'));
+    expect(err.cause.message).toEqual('rootCause');
+    expect(err.cause).toBe(err.rootCause);
+  });
+
+  it('creates an error with nested causes', () => {
+    const err = new ErrorWithCause(
+      'message',
+      new ErrorWithCause('cause', new Error('rootCause'))
+    );
+    expect(err.cause.message).toEqual('cause');
+    expect(err.rootCause.message).toBe('rootCause');
   });
 });
--- packages/util/src/error.ts
+++ packages/util/src/error.ts
@@ -23,7 +23,8 @@ export class ErrorWithCause extends Error {
    * @param message Error message.
    * @param cause Original error, causing this error.
    */
-  constructor(message: string, public cause?: Error) {
+  constructor(message: string, public cause: Error) {
+    // There is an issue with the prototype chain when extending from Error: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html#support-for-newtarget
     super(message); // 'Error' breaks prototype chain here
     Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
     this.name = 'ErrorWithCause';
@@ -39,7 +40,7 @@ export class ErrorWithCause extends Error {
    * If there are multiple errors caused one by another, the root cause is the first error that occurred.
    * In case there is no root cause.
    */
-  get rootCause(): Error | undefined {
+  get rootCause(): Error {
     return isErrorWithCause(this.cause) ? this.cause.rootCause : this.cause;
   }
 }
@@ -49,8 +50,6 @@ export class ErrorWithCause extends Error {
  * @param err An error.
  * @returns Whether the given error is of type ErrorWithCause.
  */
-export function isErrorWithCause(
-  err: Error | undefined
-): err is ErrorWithCause {
+export function isErrorWithCause(err: Error): err is ErrorWithCause {
   return err?.name === 'ErrorWithCause';
 }

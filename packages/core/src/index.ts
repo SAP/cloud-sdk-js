65dde526b0e97976c40c74344d914115ef15f3d0
tomfrenken <tom.frenken@sap.com>
2020-12-29 15:38:17 +0100

    update index.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,7 @@ export * from './connectivity';
 export * from './header-util';
 export * from './http-client';
 export * from './odata-common';
+export { entityDeserializer } from './odata-common';
 export * from './odata-v2';
 // The explicit export is needed to guarantee backwards compatibility, it overrides the same named functions in odata-common and odata-v4.
 export {
@@ -13,7 +14,6 @@ export {
   ODataFunctionImportRequestConfig,
   Entity,
   CustomField,
-  entityDeserializer,
   filterFunctions
 } from './odata-v2';
 export * from './odata-v4';

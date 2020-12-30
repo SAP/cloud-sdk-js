baf879cd408d9b5f46eaa4099380b7f19b71dc98
tomfrenken <tom.frenken@sap.com>
2020-12-30 11:48:33 +0100

    order index.ts alphabetically

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,47 +3,47 @@ export * from './header-util';
 export * from './http-client';
 export * from './odata-common';
 // The explicit export is needed to guarantee backwards compatibility, it overrides the same named function in odata-v4.
-export { filterFunction, entityDeserializer } from './odata-common';
+export { entityDeserializer, filterFunction } from './odata-common';
 export * from './odata-v2';
 // The explicit export is needed to guarantee backwards compatibility, it overrides the same named functions in odata-common and odata-v4.
 export {
   CreateRequestBuilder,
   CustomField,
   DeleteRequestBuilder,
-  Entity,
-  GetAllRequestBuilder,
-  GetByKeyRequestBuilder,
-  UpdateRequestBuilder,
-  ODataFunctionImportRequestConfig,
-  filterFunctions,
-  EdmToPrimitive,
-  EdmType,
-  edmToTs,
-  tsToEdm,
-  deserializeComplexType,
   deserializeEntity,
+  deserializeComplexType,
+  Entity,
+  edmToTs,
+  EdmType,
+  EdmToPrimitive,
   entitySerializer,
   extractODataEtag,
+  filterFunctions,
+  FunctionImportRequestBuilder,
+  GetAllRequestBuilder,
+  GetByKeyRequestBuilder,
   getExpand,
   getSelect,
+  ODataBatchChangeSet,
+  ODataBatchRequestBuilder,
+  ODataFunctionImportRequestConfig,
   oDataUri,
   serializeComplexType,
   serializeEntity,
   serializeEntityNonCustomFields,
   toBatchChangeSet,
   toBatchRetrieveBody,
-  uriConverter,
-  uriConverters,
   transformReturnValueForEntity,
   transformReturnValueForEdmType,
-  FunctionImportRequestBuilder,
-  ODataBatchChangeSet,
-  ODataBatchRequestBuilder,
   transformReturnValueForComplexType,
   transformReturnValueForComplexTypeList,
   transformReturnValueForEdmTypeList,
   transformReturnValueForEntityList,
-  transformReturnValueForUndefined
+  transformReturnValueForUndefined,
+  tsToEdm,
+  UpdateRequestBuilder,
+  uriConverter,
+  uriConverters
 } from './odata-v2';
 export * from './odata-v4';
 export * from './openapi';

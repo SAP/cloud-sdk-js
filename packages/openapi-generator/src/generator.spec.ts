fc200fec91f1e3c2c17b9c9f307c76fb798ce2da
Frank Essenberger <frank.essenberger@sap.com>
2021-01-15 16:00:24 +0100

    try log

--- packages/openapi-generator/src/generator.spec.ts
+++ packages/openapi-generator/src/generator.spec.ts
@@ -16,7 +16,7 @@ describe('generator', () => {
       generateJs: true,
       clearOutputDir: true,
       generatePackageJson: true
-    })
+    }).then(()=>console.log('Finished generation')).catch(err=>console.log('Error generation'+err.message))
   , 120000);
 
   afterAll(() =>  deleteDirectory(outputDir),120000);

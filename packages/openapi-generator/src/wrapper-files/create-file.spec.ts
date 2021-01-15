523bce829285da599cd215458edea11d1278602c
Junjie Tang <junjie.tang@sap.com>
2021-01-15 16:40:15 +0100

    remove copyright from json

--- packages/openapi-generator/src/wrapper-files/create-file.spec.ts
+++ packages/openapi-generator/src/wrapper-files/create-file.spec.ts
@@ -16,4 +16,22 @@ describe('create-file', () => {
       expect.anything()
     );
   });
+
+  it('creates content with copyright', async () => {
+    await createFile('directory', 'filename', 'content', true, true);
+    expect(fs.promises.writeFile).toHaveBeenCalledWith(
+      'directory/filename',
+      expect.stringContaining('Copyright'),
+      expect.anything()
+    );
+  });
+
+  it('creates content without copyright', async () => {
+    await createFile('directory', 'filename', 'content', true, false);
+    expect(fs.promises.writeFile).toHaveBeenCalledWith(
+      'directory/filename',
+      expect.not.stringContaining('Copyright'),
+      expect.anything()
+    );
+  });
 });

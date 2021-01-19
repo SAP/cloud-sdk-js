17b777d0bb71353c9cfe2f76e48a9f34524b6fed
Frank Essenberger <frank.essenberger@sap.com>
2021-01-19 10:43:14 +0100

    remove dist

--- test-packages/test-services/openapi/test-service/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './openapi/model';
-export * from './api';
-//# sourceMappingURL=index.d.ts.map
--- test-packages/test-services/openapi/test-service/dist/openapi/model/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './simple-test-entity';
-export * from './test-entity';
-//# sourceMappingURL=index.d.ts.map

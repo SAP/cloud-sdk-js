795843ff50383d25d3a2965851a5e221eff48ad4
tomfrenken <tom.frenken@sap.com>
2021-01-20 15:59:26 +0100

    remove redundant Dirent and include useless test

--- packages/openapi-generator/src/generator.spec.ts
+++ packages/openapi-generator/src/generator.spec.ts
@@ -1,7 +1,11 @@
-import { getSdkVersion } from './generator';
+import { getSdkVersion, getInputFilePaths } from './generator';
 
 describe('generator', () => {
   it('getSdkVersion returns a valid stable version', async () => {
     expect((await getSdkVersion()).split('.').length).toBe(3);
+  });
+
+  it('getInputPaths returns an array of all file paths, including subdirectories', async () => {
+    expect((await getInputFilePaths('this')).length).toBe(1);
   });
 });

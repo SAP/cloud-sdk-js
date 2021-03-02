d0a25af2931edfb60e7f7c7db9f49b41f285154b
Junjie Tang <33489572+jjtang1985@users.noreply.github.com>
2021-03-02 14:22:38 +0100

    feat: support custom api names and operation names (#1038)

--- test-packages/test-services/openapi/test-service/index.d.ts
+++ test-packages/test-services/openapi/test-service/index.d.ts
@@ -4,4 +4,5 @@ export * from './test-case-api';
 export * from './tag-dot-api';
 export * from './tag-space-api';
 export * from './default-api';
+export * from './extension-api';
 //# sourceMappingURL=index.d.ts.map

90abf300ad3789ab49b7bc9a669d1edc03b396a0
Marika Marszalkowski <marika.marszalkowski@sap.com>
2021-03-04 10:32:02 +0100

    chore: Remove more files

---
+++ test-packages/test-services/openapi/test-service/tag-dot-api.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * This is a generated file powered by the SAP Cloud SDK for JavaScript.
+ */
+import { OpenApiRequestBuilder } from '@sap-cloud-sdk/core';
+import { TagDotApi } from './openapi/api';
+
+
+export const TestServiceTagDotApi = {
+  tagWithDot: () => new OpenApiRequestBuilder<TagDotApi, 'tagWithDot'>(
+    TagDotApi,
+    'tagWithDot'
+  )
+};

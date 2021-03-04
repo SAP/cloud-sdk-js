921592a42941c600d50b627a143ec9894888c316
Marika Marszalkowski <marika.marszalkowski@sap.com>
2021-03-04 10:12:47 +0100

    chore: Remove transpiled sources from unit tests

--- test-packages/test-services/openapi/test-service/extension-api.d.ts
+++
@@ -1,7 +0,0 @@
-import { OpenApiRequestBuilder } from '@sap-cloud-sdk/core';
-import { ExtensionApi } from './openapi/api';
-export declare const TestServiceExtensionApi: {
-    niceGetFunction: () => OpenApiRequestBuilder<ExtensionApi, "niceGetFunction">;
-    nicePostFunction: () => OpenApiRequestBuilder<ExtensionApi, "nicePostFunction">;
-};
-//# sourceMappingURL=extension-api.d.ts.map
--- test-packages/test-services/openapi/test-service/tag-dot-api.d.ts
+++
@@ -1,6 +0,0 @@
-import { OpenApiRequestBuilder } from '@sap-cloud-sdk/core';
-import { TagDotApi } from './openapi/api';
-export declare const TestServiceTagDotApi: {
-    tagWithDot: () => OpenApiRequestBuilder<TagDotApi, "tagWithDot">;
-};
-//# sourceMappingURL=tag-dot-api.d.ts.map

aba3fe7452af2fbddf7059dcde2d1cc8bf72bbef
Junjie Tang <junjie.tang@sap.com>
2021-03-05 12:59:25 +0100

    fix tests

--- packages/generator/src/edmx-parser/v4/edmx-parser.spec.ts
+++ packages/generator/src/edmx-parser/v4/edmx-parser.spec.ts
@@ -18,11 +18,11 @@ describe('edmx-edmx-parser', () => {
     );
 
     expect(parseEntitySets(metadataEdmx.root).length).toBe(13);
-    expect(parseEntityType(metadataEdmx.root).length).toBe(12);
-    expect(parseFunctionImports(metadataEdmx.root).length).toBe(8);
-    expect(parseFunctions(metadataEdmx.root).length).toBe(8);
-    expect(parseActionImport(metadataEdmx.root).length).toBe(6);
-    expect(parseActions(metadataEdmx.root).length).toBe(5);
+    expect(parseEntityType(metadataEdmx.root).length).toBe(13);
+    expect(parseFunctionImports(metadataEdmx.root).length).toBe(10);
+    expect(parseFunctions(metadataEdmx.root).length).toBe(10);
+    expect(parseActionImport(metadataEdmx.root).length).toBe(8);
+    expect(parseActions(metadataEdmx.root).length).toBe(7);
     expect(parseComplexTypesBase(metadataEdmx.root).length).toBe(4);
     expect(parseEnumTypes(metadataEdmx.root).length).toBe(2);
 

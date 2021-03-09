0a4a4e781cd308a71966ac1be8460d91acc59e06
Junjie Tang <junjie.tang@sap.com>
2021-03-09 15:14:36 +0100

    fix tests

--- test-packages/type-tests/test/http-client.spec.ts
+++ test-packages/type-tests/test/http-client.spec.ts
@@ -6,5 +6,5 @@ import {
 // $ExpectType Promise<HttpResponse>
 executeHttpRequest({ url: 'https://example.com' }, { method: 'get' });
 
-// $ExpectType Promise<AxiosResponse>
+// $ExpectType Promise<AxiosResponse<any>>
 executeHttpRequestReturnAxiosResponse({ url: 'https://example.com' }, { method: 'get' });

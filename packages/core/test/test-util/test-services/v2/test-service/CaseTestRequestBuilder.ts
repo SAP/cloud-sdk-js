065d37c29615a43133714025aa12ebe9e9d19581
Junjie Tang <junjie.tang@sap.com>
2021-03-11 10:04:48 +0100

    test service

---
+++ packages/core/test/test-util/test-services/v2/test-service/CaseTestRequestBuilder.ts
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * This is a generated file powered by the SAP Cloud SDK for JavaScript.
+ */
+import { RequestBuilder, GetAllRequestBuilderV2, GetByKeyRequestBuilderV2, CreateRequestBuilderV2, UpdateRequestBuilderV2, DeleteRequestBuilderV2 } from '../../../../../src';
+import { CaseTest } from './CaseTest';
+
+/**
+ * Request builder class for operations supported on the [[CaseTest]] entity.
+ */
+export class CaseTestRequestBuilder extends RequestBuilder<CaseTest> {
+  /**
+   * Returns a request builder for retrieving one `CaseTest` entity based on its keys.
+   * @param keyPropertyString Key property. See [[CaseTest.keyPropertyString]].
+   * @returns A request builder for creating requests to retrieve one `CaseTest` entity based on its keys.
+   */
+  getByKey(keyPropertyString: string): GetByKeyRequestBuilderV2<CaseTest> {
+    return new GetByKeyRequestBuilderV2(CaseTest, { KeyPropertyString: keyPropertyString });
+  }
+
+  /**
+   * Returns a request builder for querying all `CaseTest` entities.
+   * @returns A request builder for creating requests to retrieve all `CaseTest` entities.
+   */
+  getAll(): GetAllRequestBuilderV2<CaseTest> {
+    return new GetAllRequestBuilderV2(CaseTest);
+  }
+
+  /**
+   * Returns a request builder for creating a `CaseTest` entity.
+   * @param entity The entity to be created
+   * @returns A request builder for creating requests that create an entity of type `CaseTest`.
+   */
+  create(entity: CaseTest): CreateRequestBuilderV2<CaseTest> {
+    return new CreateRequestBuilderV2(CaseTest, entity);
+  }
+
+  /**
+   * Returns a request builder for updating an entity of type `CaseTest`.
+   * @param entity The entity to be updated
+   * @returns A request builder for creating requests that update an entity of type `CaseTest`.
+   */
+  update(entity: CaseTest): UpdateRequestBuilderV2<CaseTest> {
+    return new UpdateRequestBuilderV2(CaseTest, entity);
+  }
+
+  /**
+   * Returns a request builder for deleting an entity of type `CaseTest`.
+   * @param keyPropertyString Key property. See [[CaseTest.keyPropertyString]].
+   * @returns A request builder for creating requests that delete an entity of type `CaseTest`.
+   */
+  delete(keyPropertyString: string): DeleteRequestBuilderV2<CaseTest>;
+  /**
+   * Returns a request builder for deleting an entity of type `CaseTest`.
+   * @param entity Pass the entity to be deleted.
+   * @returns A request builder for creating requests that delete an entity of type `CaseTest` by taking the entity as a parameter.
+   */
+  delete(entity: CaseTest): DeleteRequestBuilderV2<CaseTest>;
+  delete(keyPropertyStringOrEntity: any): DeleteRequestBuilderV2<CaseTest> {
+    return new DeleteRequestBuilderV2(CaseTest, keyPropertyStringOrEntity instanceof CaseTest ? keyPropertyStringOrEntity : { KeyPropertyString: keyPropertyStringOrEntity! });
+  }
+}
---
+++ test-packages/test-services/v2/test-service/CaseTest.d.ts
@@ -0,0 +1,71 @@
+import { CaseTestRequestBuilder } from './CaseTestRequestBuilder';
+import { AllFields, CustomFieldV2, EntityBuilderType, EntityV2, Field, StringField } from '@sap-cloud-sdk/core';
+/**
+ * This class represents the entity "A_CaseTest" of service "API_TEST_SRV".
+ */
+export declare class CaseTest extends EntityV2 implements CaseTestType {
+    /**
+     * Technical entity name for CaseTest.
+     */
+    static _entityName: string;
+    /**
+     * Default url path for the according service.
+     */
+    static _defaultServicePath: string;
+    /**
+     * Key Property String.
+     */
+    keyPropertyString: string;
+    /**
+     * Returns an entity builder to construct instances of `CaseTest`.
+     * @returns A builder that constructs instances of entity type `CaseTest`.
+     */
+    static builder(): EntityBuilderType<CaseTest, CaseTestType>;
+    /**
+     * Returns a request builder to construct requests for operations on the `CaseTest` entity type.
+     * @returns A `CaseTest` request builder.
+     */
+    static requestBuilder(): CaseTestRequestBuilder;
+    /**
+     * Returns a selectable object that allows the selection of custom field in a get request for the entity `CaseTest`.
+     * @param fieldName Name of the custom field to select
+     * @returns A builder that constructs instances of entity type `CaseTest`.
+     */
+    static customField(fieldName: string): CustomFieldV2<CaseTest>;
+    /**
+     * Overwrites the default toJSON method so that all instance variables as well as all custom fields of the entity are returned.
+     * @returns An object containing all instance variables + custom fields.
+     */
+    toJSON(): {
+        [key: string]: any;
+    };
+}
+export interface CaseTestType {
+    keyPropertyString: string;
+}
+export declare namespace CaseTest {
+    /**
+     * Static representation of the [[keyPropertyString]] property for query construction.
+     * Use to reference this property in query operations such as 'select' in the fluent request API.
+     */
+    const KEY_PROPERTY_STRING: StringField<CaseTest>;
+    /**
+     * All fields of the CaseTest entity.
+     */
+    const _allFields: Array<StringField<CaseTest>>;
+    /**
+     * All fields selector.
+     */
+    const ALL_FIELDS: AllFields<CaseTest>;
+    /**
+     * All key fields of the CaseTest entity.
+     */
+    const _keyFields: Array<Field<CaseTest>>;
+    /**
+     * Mapping of all key field names to the respective static field property CaseTest.
+     */
+    const _keys: {
+        [keys: string]: Field<CaseTest>;
+    };
+}
+//# sourceMappingURL=CaseTest.d.ts.map

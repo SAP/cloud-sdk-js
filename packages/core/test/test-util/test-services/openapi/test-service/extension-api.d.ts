4e754c04b9826c48f31203d0c7f9961eb796bba4
Marika Marszalkowski <marika.marszalkowski@sap.com>
2021-03-11 16:44:55 +0100

    chore: Remove transpliation artifacts from unit tests (#1069)
    * chore: Remove transpiled sources from unit tests
    
    * chore: Remove more files
    
    * Fix weird tests

--- packages/core/test/test-util/test-services/openapi/test-service/extension-api.d.ts
+++
@@ -1,7 +0,0 @@
-import { OpenApiRequestBuilder } from '../../../../../src';
-import { ExtensionApi } from './openapi/api';
-export declare const TestServiceExtensionApi: {
-    niceGetFunction: () => OpenApiRequestBuilder<ExtensionApi, "niceGetFunction">;
-    nicePostFunction: () => OpenApiRequestBuilder<ExtensionApi, "nicePostFunction">;
-};
-//# sourceMappingURL=extension-api.d.ts.map

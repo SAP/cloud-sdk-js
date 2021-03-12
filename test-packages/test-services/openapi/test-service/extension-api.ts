f8da71ad149f875a6a0ce01d221e7824bf788eef
Marika Marszalkowski <marika.marszalkowski@sap.com>
2021-03-12 14:18:35 +0100

    chore: Remove more files

---
+++ test-packages/test-services/openapi/test-service/extension-api.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * This is a generated file powered by the SAP Cloud SDK for JavaScript.
+ */
+import { OpenApiRequestBuilder } from '@sap-cloud-sdk/core';
+import { ExtensionApi } from './openapi/api';
+
+
+export const TestServiceExtensionApi = {
+  niceGetFunction: () => new OpenApiRequestBuilder<ExtensionApi, 'niceGetFunction'>(
+    ExtensionApi,
+    'niceGetFunction'
+  ),
+  nicePostFunction: () => new OpenApiRequestBuilder<ExtensionApi, 'nicePostFunction'>(
+    ExtensionApi,
+    'nicePostFunction'
+  )
+};
---
+++ test-packages/test-services/openapi/test-service/index.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * This is a generated file powered by the SAP Cloud SDK for JavaScript.
+ */
+export * from './openapi/model';
+export * from './entity-api';
+export * from './test-case-api';
+export * from './tag-dot-api';
+export * from './tag-space-api';
+export * from './default-api';
+export * from './extension-api';

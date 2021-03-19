1393d9e3cf07da0ebba88ffe271e90a965d77ef9
Frank Essenberger <frank.essenberger@sap.com>
2021-03-19 10:24:48 +0100

    use fixed version

--- packages/generator/src/sdk-metadata/sdk-metadata.spec.ts
+++ packages/generator/src/sdk-metadata/sdk-metadata.spec.ts
@@ -7,11 +7,11 @@ describe('sdk-metadata',()=>{
   const service = getTestService();
 
  it('generates the header content',async ()=>{
-   expect(sdkMetaDataHeader(service,createOptions())).toMatchSnapshot();
+   expect(sdkMetaDataHeader(service,createOptions({versionInPackageJson:"1.0.0"}))).toMatchSnapshot();
   });
 
   it('[E2E] generates the JS metadata content',async ()=>{
-    await expect(sdkMetaDataJS(service,createOptions())).resolves.toMatchSnapshot();
+    await expect(sdkMetaDataJS(service,createOptions({versionInPackageJson:"1.0.0"}))).resolves.toMatchSnapshot();
   });
 
   it('generates the File names',()=>{

e155b4d2363b12b406f04990b6b6de787ef67f3c
Marika Marszalkowski <marika.marszalkowski@sap.com>
2021-03-22 17:15:23 +0100

    Fix E2E tests

--- test-packages/e2e-tests/test/openapi.spec.ts
+++ test-packages/e2e-tests/test/openapi.spec.ts
@@ -1,7 +1,7 @@
 import {
   TestEntity,
-  TestServiceEntityApi
-} from '../../../packages/core/test/test-util/test-services/openapi/test-service';
+  EntityApi
+} from '@sap-cloud-sdk/test-services/openapi/test-service';
 import { destination } from './test-util';
 
 // TODO: How do I handle paths in rest requests?
@@ -9,14 +9,14 @@ import { destination } from './test-util';
 const restDestination = { url: destination.url + 'openapi' };
 describe('openapi request builder', () => {
   it('executes getAll request', async () => {
-    const request = TestServiceEntityApi.getAllEntities();
+    const request = EntityApi.getAllEntities();
     expect(
       (await request.execute(restDestination)).length
     ).toBeGreaterThanOrEqual(4);
   });
 
   it('executes getAll request', async () => {
-    const request = TestServiceEntityApi.getEntityByKey(
+    const request = EntityApi.getEntityByKey(
       'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
     );
     expect(await request.execute(restDestination)).toBeDefined();
@@ -28,14 +28,12 @@ describe('openapi request builder', () => {
       keyProperty: 'a13e7a92-cb9c-8f4a-b1e1-6b5c8458a9fb',
       stringProperty: 'string'
     };
-    await TestServiceEntityApi.createEntity(testEntity).execute(
-      restDestination
-    );
+    await EntityApi.createEntity(testEntity).execute(restDestination);
     const postCreateCount = await countEntities();
     expect(postCreateCount).toEqual(preCreateCount + 1);
   });
 });
 
 function countEntities(): Promise<number> {
-  return TestServiceEntityApi.countEntities().execute(restDestination);
+  return EntityApi.countEntities().execute(restDestination);
 }

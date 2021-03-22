04f3b74bd1e6cb631eed8a954e4288f6d5d4a3a9
Frank Essenberger <56544999+FrankEssenberger@users.noreply.github.com>
2021-03-22 17:22:49 +0100

    use a fixed version in tests (#1132)

--- packages/generator/src/sdk-metadata/sdk-metadata.spec.ts
+++ packages/generator/src/sdk-metadata/sdk-metadata.spec.ts
@@ -2,22 +2,16 @@ import { createOptions } from '../../test/test-util/create-generator-options';
 import { VdmServiceMetadata } from '../vdm-types';
 import { getTestService } from './pregenerated-lib.spec';
 import { getSdkMetadataFileNames, sdkMetaDataHeader, sdkMetaDataJS } from './sdk-metadata';
-import * as pregeneratedLib from './pregenerated-lib';
 
 describe('sdk-metadata',()=>{
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
   const service = getTestService();
 
  it('generates the header content',async ()=>{
-   jest.spyOn(pregeneratedLib, 'getVersionForClient').mockImplementationOnce(() => '1.0.0');
-   expect(sdkMetaDataHeader(service,createOptions())).toMatchSnapshot();
+   expect(sdkMetaDataHeader(service,createOptions({ versionInPackageJson:'1.0.0' }))).toMatchSnapshot();
   });
 
   it('[E2E] generates the JS metadata content',async ()=>{
-    await expect(sdkMetaDataJS(service,createOptions())).resolves.toMatchSnapshot();
+    await expect(sdkMetaDataJS(service,createOptions({ versionInPackageJson:'1.0.0' }))).resolves.toMatchSnapshot();
   });
 
   it('generates the File names',()=>{

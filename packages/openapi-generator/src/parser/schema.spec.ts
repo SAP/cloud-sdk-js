61d26a0714d20c2230435c8163e2ea9799bdb72a
Marika Marszalkowski <marika.marszalkowski@sap.com>
2021-03-31 19:59:00 +0200

    Fix tests

--- packages/openapi-generator/src/parser/schema.spec.ts
+++ packages/openapi-generator/src/parser/schema.spec.ts
@@ -126,7 +126,7 @@ describe('parseSchema', () => {
     const schema: OpenAPIV3.SchemaObject = {
       enum: ['one', 'two', 'three']
     };
-    expect(parseSchema(schema)).toEqual({
+    expect(parseSchema(schema, {})).toEqual({
       type: 'string',
       enum: ["'one'", "'two'", "'three'"]
     });

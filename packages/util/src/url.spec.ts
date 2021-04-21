46cfa5418a7991c8299dd50d5608ab4866806ab2
Frank Essenberger <frank.essenberger@sap.com>
2021-04-21 17:22:05 +0200

    checkin snapshot 2

--- packages/util/src/url.spec.ts
+++ packages/util/src/url.spec.ts
@@ -2,7 +2,7 @@ import { checkUrlExists } from '@sap-cloud-sdk/util/dist/url';
 import nock from 'nock';
 
 describe('url', () => {
-  it('fails for non existing link', async () => {
+  it('throws for non existing link', async () => {
     nock('https://non.existing.com').head(/.*/).reply(404);
 
     await expect(
@@ -10,7 +10,7 @@ describe('url', () => {
     ).rejects.toThrowErrorMatchingSnapshot();
   });
 
-  it('does not fail for existing link', async () => {
+  it('does not throw for existing link', async () => {
     nock('https://existing.com').head(/.*/).reply(200);
     await expect(
       checkUrlExists('https://existing.com')

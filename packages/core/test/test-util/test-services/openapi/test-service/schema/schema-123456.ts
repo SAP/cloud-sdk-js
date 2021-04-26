4c50df205d8d88a77f43bde1789e5282750d03de
Frank Essenberger <frank.essenberger@sap.com>
2021-04-26 18:17:15 +0200

    checked in also in core

---
+++ packages/core/test/test-util/test-services/openapi/test-service/schema/schema-123456.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * This is a generated file powered by the SAP Cloud SDK for JavaScript.
+ */
+
+/**
+ * Representation of the 'Schema123456' schema.
+ */
+export type Schema123456 =
+  | {
+      someProperty?: string;
+    }
+  | Record<string, any>;

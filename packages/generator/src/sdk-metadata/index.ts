74ce36ec1f1023b83b0e1d3e2c287406491b12fc
Frank Essenberger <frank.essenberger@sap.com>
2021-04-30 09:12:30 +0200

    fix index

--- packages/generator/src/sdk-metadata/index.ts
+++ packages/generator/src/sdk-metadata/index.ts
@@ -1,6 +1,4 @@
 export * from './generation-and-usage';
 export * from './pregenerated-lib';
 export * from './sdk-metadata';
-export * from './sdk-metadata-types';
 export * from './generation-and-usage';
-export * from './links';

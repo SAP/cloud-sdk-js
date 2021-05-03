6fbb7e34c6df31b13aa47e2297aca7320f9ef9a7
Marika Marszalkowski <marika.marszalkowski@sap.com>
2021-05-03 16:39:54 +0200

    chore: Fix metadata test to be sdk version independent (#1261)

--- packages/openapi-generator/src/sdk-metadata/generation-and-usage.spec.ts
+++ packages/openapi-generator/src/sdk-metadata/generation-and-usage.spec.ts
@@ -1,8 +1,15 @@
+import { getSdkVersion } from '@sap-cloud-sdk/generator-common';
 import { dummyOpenApiDocument } from '../../test/test-util';
 import { getGenerationAndUsage } from './generation-and-usage';
 
 describe('generation-and-usage', () => {
   it('creates GenerationAndUsage from openApiDocument', async () => {
-    expect(await getGenerationAndUsage(dummyOpenApiDocument)).toMatchSnapshot();
+    const generationAndUsage = await getGenerationAndUsage(
+      dummyOpenApiDocument
+    );
+    expect(generationAndUsage).toMatchSnapshot({
+      generatorVersion: expect.any(String)
+    });
+    expect(generationAndUsage.generatorVersion).toEqual(await getSdkVersion());
   });
 });

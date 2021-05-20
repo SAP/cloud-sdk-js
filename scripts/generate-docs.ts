1537eb90cbf339bf53881c64948eba650027c0f0
Frank Essenberger <frank.essenberger@sap.com>
2021-05-20 14:25:46 +0200

    formatter

--- scripts/generate-docs.ts
+++ scripts/generate-docs.ts
@@ -8,9 +8,9 @@ import {
 } from 'fs';
 import { resolve, basename, extname } from 'path';
 import execa = require('execa');
-import { unixEOL } from '@sap-cloud-sdk/util';
+import { jsonStringify, unixEOL } from '@sap-cloud-sdk/util';
 import compareVersions from 'compare-versions';
-import { jsonStringify, transformFile } from './util';
+import { transformFile } from './util';
 const apiDocPath = resolve('docs', 'api');
 
 const isDirectory = entryPath => lstatSync(entryPath).isDirectory();

db902711747b54c9ba03556144a6a671a33bc91f
Junjie Tang <33489572+jjtang1985@users.noreply.github.com>
2021-05-26 17:52:29 +0200

    Fix: opossum compilation error (#1318)

--- packages/core/src/connectivity/scp-cf/resilience-options.ts
+++ packages/core/src/connectivity/scp-cf/resilience-options.ts
@@ -1,5 +1,3 @@
-import CircuitBreaker from 'opossum';
-
 export interface ResilienceOptions {
   /**
    * A boolean value that indicates whether to execute request to SCP-CF services using circuit breaker.
@@ -7,7 +5,7 @@ export interface ResilienceOptions {
   enableCircuitBreaker?: boolean;
 }
 
-export const circuitBreakerDefaultOptions: CircuitBreaker.Options = {
+export const circuitBreakerDefaultOptions = {
   timeout: 10000,
   errorThresholdPercentage: 50,
   volumeThreshold: 10,

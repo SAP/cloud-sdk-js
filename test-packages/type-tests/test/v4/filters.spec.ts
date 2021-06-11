889d2f49f2bc73c39464e45aa47b90f77ba7c70b
Marika Marszalkowski <marika.marszalkowski@sap.com>
2021-06-11 16:13:49 +0200

    Fix lint

--- test-packages/type-tests/test/v4/filters.spec.ts
+++ test-packages/type-tests/test/v4/filters.spec.ts
@@ -1,5 +1,12 @@
 import { and, any } from '@sap-cloud-sdk/core';
-import { TestEntity, TestEntityMultiLink } from '@sap-cloud-sdk/test-services/v4/test-service';
+import {
+  TestEntity,
+  TestEntityMultiLink
+} from '@sap-cloud-sdk/test-services/v4/test-service';
 
 // $ExpectType FilterList<TestEntity>
-and(TestEntity.TO_MULTI_LINK.filter(any(TestEntityMultiLink.STRING_PROPERTY.equals('test'))));
+and(
+  TestEntity.TO_MULTI_LINK.filter(
+    any(TestEntityMultiLink.STRING_PROPERTY.equals('test'))
+  )
+);

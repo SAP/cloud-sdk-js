93055eb0a8462559217a1a0ea55706d6f9aef197
Marika Marszalkowski <marika.marszalkowski@sap.com>
2021-07-09 14:29:28 +0200

    Move selectable

--- packages/core/src/odata-common/selectable/legacy/index.ts
+++ packages/core/src/odata-common/selectable/legacy/index.ts
@@ -4,6 +4,7 @@ export * from './boolean-field';
 export * from './date-field';
 export * from './duration-field';
 export * from './number-field';
+export * from './selectable';
 export * from './string-field';
 export * from './time-field';
 export * from './any-field';
--- packages/core/src/odata-common/selectable/selectable.ts
+++ packages/core/src/odata-common/selectable/selectable.ts
@@ -23,14 +23,3 @@ export type Selectable<EntityT extends Entity> =
         | CollectionField<EntityT, any, boolean, boolean>
         | AllFields<EntityT>
     : never;
-
-/**
- * @deprecated Since v1.27.0. This will not be replaced. Use the generic `SelectableT` parameter of Fields instead.
- * Interface denoting a selectable [[EdmTypeField]].
- */
-export interface SelectableEdmTypeField {
-  /**
-   * This property denotes that this is a selectable field.
-   */
-  selectable: true;
-}

c300f621d44ea09dfb8fe592bd34f1df36ce6ed0
Marika Marszalkowski <marika.marszalkowski@sap.com>
2021-07-28 10:30:49 +0200

    Remove options

--- packages/openapi-generator/src/sdk-metadata/sdk-metadata.ts
+++ packages/openapi-generator/src/sdk-metadata/sdk-metadata.ts
@@ -4,13 +4,11 @@ import {
 } from '@sap-cloud-sdk/generator-common';
 import type { Client } from '@sap-cloud-sdk/generator-common';
 import { OpenApiDocument } from '../openapi-types';
-import { GeneratorOptions } from '../options';
 import { packageDescription } from './package-description';
 import { getGenerationAndUsage } from './generation-and-usage';
 
 export async function sdkMetadata(
-  openApiDocument: OpenApiDocument,
-  options: GeneratorOptions
+  openApiDocument: OpenApiDocument
 ): Promise<Client> {
   const [pregeneratedLibrary, generationAndUsage] = await Promise.all([
     getPregeneratedLibrary(

7b16e1f2140b8543c6f50e54c3068a0661c95178
Frank Essenberger <56544999+FrankEssenberger@users.noreply.github.com>
2021-08-13 13:46:43 +0200

    Update packages/core/src/connectivity/scp-cf/destination/destination-from-service.ts
    Co-authored-by: Marika Marszalkowski <marika.marszalkowski@sap.com>

--- packages/core/src/connectivity/scp-cf/destination/destination-from-service.ts
+++ packages/core/src/connectivity/scp-cf/destination/destination-from-service.ts
@@ -139,23 +139,23 @@ class DestinationFromServiceRetriever {
   private static async getDecodedUserJwt(
     options: DestinationOptions
   ): Promise<Jwt | IssuerJwt | undefined> {
-    if (options.userJwt && options.iss) {
-      logger.warn(
-        'You have provided the userJwt and iss option to fetch the destination. This is most likely unintentional. The userJwt will be used.'
-      );
-    }
-    if (!options.userJwt && options.iss) {
-      logger.info(
-        'You use the iss option to fetch a destination instead of a full JWT. No validation is performed.'
-      );
-    }
     if (options.userJwt) {
+      if (options.iss) {
+        logger.warn(
+          'You have provided the `userJwt` and `iss` options to fetch the destination. This is most likely unintentional. Ignoring `iss`.'
+        );
+      }
+      
       return {
         encoded: options.userJwt,
         decoded: await verifyJwt(options.userJwt, options)
       };
     }
+    
     if (options.iss) {
+      logger.info(
+        'Using `iss` option to fetch a destination instead of a full JWT. No validation is performed.'
+      );
       return { decoded: { iss: options.iss } };
     }
   }

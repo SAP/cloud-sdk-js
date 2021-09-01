2bf7968e47e5174fb5365fb51a089648718c319c
deekshas8 <deeksha.sinha@sap.com>
2021-09-01 17:23:31 +0200

    code changes

--- packages/generator/src/sdk-metadata/generation-and-usage.spec.ts
+++ packages/generator/src/sdk-metadata/generation-and-usage.spec.ts
@@ -3,7 +3,7 @@ import { writeFile, readFile, removeSync } from 'fs-extra';
 import execa = require('execa');
 import { VdmServiceMetadata } from '../vdm-types';
 import { getApiSpecificUsage } from './generation-and-usage';
-import { genericCodeSample } from './code-samples';
+import { genericEntityCodeSample } from './code-samples';
 
 describe('generation-and-usage', () => {
   const service = {
@@ -63,7 +63,7 @@ describe('generation-and-usage', () => {
   } as VdmServiceMetadata;
 
   it('creates generic usage example', () => {
-    expect(genericCodeSample()).toMatchSnapshot();
+    expect(genericEntityCodeSample()).toMatchSnapshot();
   });
 
   it('creates api specific usage for entity', () => {
@@ -83,7 +83,7 @@ describe('generation-and-usage', () => {
   });
 
   it('creates compiling generic usage', async () => {
-    const codeSnippet = genericCodeSample().instructions;
+    const codeSnippet = genericEntityCodeSample().instructions;
     const tsFile = 'generic-get-all-code-sample.ts';
     const jsFile = tsFile.replace('.ts', '.js');
     await writeFile(resolve(__dirname, tsFile), codeSnippet);

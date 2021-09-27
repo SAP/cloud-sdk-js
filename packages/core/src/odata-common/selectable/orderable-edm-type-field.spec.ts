1c084deccf1c4c3790ce2683e3e0cab56c37dd54
deekshas8 <deeksha.sinha@sap.com>
2021-09-27 18:48:27 +0200

    add tests

--- packages/core/src/odata-common/selectable/orderable-edm-type-field.spec.ts
+++ packages/core/src/odata-common/selectable/orderable-edm-type-field.spec.ts
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import {
   TestComplexTypeField,
   TestEntity
@@ -53,6 +54,35 @@ describe('Number Field', () => {
     it('should create filter for "greaterOrEqual"', () => {
       const filter = field.greaterOrEqual(filterValue);
       checkFilter(filter, fieldName, 'ge', filterValue);
+    });
+  });
+
+  describe('DateTime and DateTimeOffset fields', () => {
+    const dateFilterValue = moment(1425427200000);
+    const datetimefieldName = 'DateTimeProperty';
+    const datetimeOffsetfieldName = 'DateTimeOffSetProperty';
+
+    it('should create filter for type DateTimeOffset by passing moment() ', () => {
+      const filter = TestEntity.DATE_TIME_OFF_SET_PROPERTY.equals(moment());
+      expect(moment.isMoment(filter.value)).toBe(true);
+    });
+
+    it('should create filter for equals for type Edm.DateTime', () => {
+      const filter = TestEntity.DATE_TIME_PROPERTY.equals(
+        moment(1425427200000)
+      );
+      expect(filter.field).toBe(datetimefieldName);
+      expect(filter.operator).toBe('eq');
+      expect(filter.value).toEqual(dateFilterValue);
+    });
+
+    it('should create filter for equals for type Edm.DateTimeOffset', () => {
+      const filter = TestEntity.DATE_TIME_OFF_SET_PROPERTY.equals(
+        moment(1425427200000)
+      );
+      expect(filter.field).toBe(datetimeOffsetfieldName);
+      expect(filter.operator).toBe('eq');
+      expect(filter.value).toEqual(dateFilterValue);
     });
   });
 

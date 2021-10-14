a97a017ef370f3a3c6d7457d5278b6496624cd27
Junjie Tang <33489572+jjtang1985@users.noreply.github.com>
2021-10-14 12:06:06 +0200

    feat: update read me of generated clients (#1658) (#1685)

--- packages/openapi-generator/src/file-serializer/readme.spec.ts
+++ packages/openapi-generator/src/file-serializer/readme.spec.ts
@@ -10,8 +10,46 @@ describe('readme', () => {
           directoryName: ''
         },
         serviceName: 'WorkflowAPI',
+        serviceDescription:
+          'With the API, you can, for example, start new workflow instances and work with tasks.',
         originalFileName: '',
-        operations: []
+        operations: [],
+        apis: []
+      } as unknown as OpenApiDocument)
+    ).toMatchSnapshot();
+  });
+
+  it('returns the readme content with usage example', () => {
+    expect(
+      readme({
+        serviceOptions: {
+          packageName: '@sap-cloud-sdk/workflow-service',
+          directoryName: ''
+        },
+        serviceName: 'WorkflowAPI',
+        serviceDescription:
+          'With the API, you can, for example, start new workflow instances and work with tasks.',
+        originalFileName: '',
+        operations: [],
+        apis: [
+          {
+            name: 'testApi',
+            operations: [
+              {
+                operationId: 'getAll',
+                method: 'get',
+                responses: {
+                  200: { description: 'some response description' }
+                },
+                tags: [],
+                pathParameters: [],
+                queryParameters: [],
+                response: { type: 'any' },
+                pathPattern: ''
+              }
+            ]
+          }
+        ]
       } as unknown as OpenApiDocument)
     ).toMatchSnapshot();
   });

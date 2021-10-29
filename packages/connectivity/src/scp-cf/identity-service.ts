a0c67fd77a2b034257614dc2d7b517813fb2d4b4
Junjie Tang <junjie.tang@sap.com>
2021-10-29 11:53:36 +0200

    fix merge conflict

---
+++ packages/connectivity/src/scp-cf/identity-service.ts
@@ -0,0 +1,33 @@
+import * as xssec from '@sap/xssec';
+import { first } from '@sap-cloud-sdk/util';
+import { getServiceCredentialsList } from './environment-accessor';
+import { DestinationOptions } from './destination/destination-accessor';
+
+/**
+ * Make a token exchange from IAS token to XSUAA token.
+ * @param options - Configuration for how to retrieve destinations from the destination service.
+ * @returns Exchanged token.
+ */
+export async function exchangeToken(
+  options: DestinationOptions
+): Promise<string> {
+  const xsuaaServiceCredentials = first(getServiceCredentialsList('xsuaa'));
+  return new Promise((resolve: (p: string) => void, reject) => {
+    xssec.createSecurityContext(
+      options.userJwt,
+      xsuaaServiceCredentials,
+      (err: Error, context, tokenInfo) =>
+        err ? reject(err) : resolve(tokenInfo.getTokenValue())
+    );
+  });
+}
+
+/**
+ * Checks whether the IAS token to XSUAA token exchange should be applied.
+ * @param options - Configuration for how to retrieve destinations from the destination service.
+ * @returns A boolean value, that indicates whether the token exchange should be applied.
+ */
+export function isTokenExchangeEnabled(options: DestinationOptions): boolean {
+  // note: the option is optional and by default we enable the token exchange.
+  return options.iasToXsuaaTokenExchange !== false && !!options.userJwt;
+}

64f76e2e0628f4e3f44cb32643d3b8cb5b9faadd
d068544 <frank.essenberger@sap.com>
2021-11-01 16:46:00 +0100

    linter

--- test-packages/test-services-e2e/v4/test-service/index.ts
+++ test-packages/test-services-e2e/v4/test-service/index.ts
@@ -8,5 +8,4 @@ export * from './TestEntityLink';
 export * from './TestEntityRequestBuilder';
 export * from './TestEntityLinkRequestBuilder';
 export * from './function-imports';
-export * from './action-imports';
 export * from './BatchRequest';

2a5ccc71053bc9d5b4d2b55c950e3d18075b5a22
I519840 <tom.frenken@sap.com>
2021-11-17 08:34:54 +0100

    use index.ts approach

--- packages/odata-common/src/internal.ts
+++ packages/odata-common/src/internal.ts
@@ -1,163 +1,19 @@
-export { createODataUri } from './uri-conversion/odata-uri';
-
-export { createUriConverter } from './uri-conversion/uri-value-converter';
-
-export { createEdmToTs, createTsToEdm } from './payload-value-converter';
-
-export { ODataBatchRequestConfig } from './request/odata-batch-request-config';
-
-export { Filter } from './filter/filter';
-
-export { ODataGetByKeyRequestConfig } from './request/odata-get-by-key-request-config';
-
-export { EntityBuilder } from './entity-builder';
-
-export { EnumField } from './selectable/enum-field';
-
-export { throwErrorWhenReturnTypeIsUnionType } from './response-transformer';
-
-export { ComplexTypeField } from './selectable/complex-type-field';
-export { ConstructorOrField } from './selectable/constructor-or-field';
-export { PropertyMetadata } from './selectable/complex-type-namespace';
-
-export { RequestBuilder } from './request-builder/request-builder';
-
-export { EdmTypeField } from './selectable/edm-type-field';
-export { FieldBuilder } from './selectable/field-builder';
-export { FieldOptions } from './selectable/field';
-
-export { ODataCreateRequestConfig } from './request/odata-create-request-config';
-export { ODataDeleteRequestConfig } from './request/odata-delete-request-config';
-
-export { asc, desc } from './order/orderable';
-
-export { OrderableFilterFunction } from './filter/orderable-filter-function';
-export { CollectionField } from './selectable/collection-field';
-
-export { GetByKeyRequestBuilderBase } from './request-builder/get-by-key-request-builder-base';
-
-export { ODataRequestConfig } from './request/odata-request-config';
-export {
-  serializeChangeSet,
-  serializeRequest,
-  serializeBatchRequest
-} from './request-builder/batch/batch-request-serializer';
-export { FilterLambdaExpression } from './filter/filter-lambda-expression';
-
-export { isNavigationProperty } from './properties-util';
-export { removePropertyOnCondition } from './request-builder/update-request-builder-base';
-
-export { NumberFilterFunction } from './filter/number-filter-function';
-export { CollectionFilterFunction } from './filter/collection-filter-function';
-export { BatchChangeSet } from './request-builder/batch/batch-change-set';
-export { BatchResponse } from './batch-response';
-
-export { StringFilterFunction } from './filter/string-filter-function';
-export { BooleanFilterFunction } from './filter/boolean-filter-function';
-
-export { filterFunction } from './filter/filter-function';
-export {
-  filterFunctions,
-  isOf,
-  trim,
-  concat,
-  round,
-  floor,
-  ceiling,
-  day,
-  hour,
-  minute,
-  month,
-  second,
-  year,
-  indexOf,
-  substring,
-  toLower,
-  toUpper,
-  endsWith,
-  startsWith,
-  length
-} from './filter/filter-functions';
-export { BatchRequestBuilder } from './request-builder/batch/batch-request-builder';
-export {
-  BatchResponseDeserializer,
-  deserializeBatchResponse
-} from './request-builder/batch/batch-response-deserializer';
-export { parseBatchResponse } from './request-builder/batch/batch-response-parser';
-export { ODataFunctionImportRequestConfig } from './request/odata-function-import-request-config';
-
-export { ResponseDataAccessor } from './response-data-accessor';
-export { FunctionImportParameters } from './request/function-import-parameter';
-export { ODataUpdateRequestConfig } from './request/odata-update-request-config';
-export { UpdateRequestBuilderBase } from './request-builder/update-request-builder-base';
-export { ActionFunctionImportRequestBuilderBase } from './request-builder/action-function-import-request-builder-base';
-export { GetAllRequestBuilderBase } from './request-builder/get-all-request-builder-base';
-export { ODataGetAllRequestConfig } from './request/odata-get-all-request-config';
-
-export { CreateRequestBuilderBase } from './request-builder/create-request-builder-base';
-export { DeleteRequestBuilderBase } from './request-builder/delete-request-builder-base';
-export { FunctionImportParameter } from './request/function-import-parameter';
-export { ODataRequest } from './request/odata-request';
-export { RequestMethodType } from './request/odata-request-config';
-
-export { getOrderBy } from './uri-conversion/get-orderby';
-
-export { createGetFilter } from './uri-conversion/get-filter';
-export { AllFields } from './selectable/all-fields';
-
-export {
-  EdmTypeShared,
-  EdmTypeCommon,
-  ExclusiveEdmTypeV4,
-  ExclusiveEdmTypeV2
-} from './edm-types';
-
-export { Field, FieldType } from './selectable/field';
-export { Filterable, and, or, not } from './filter/filterable';
-
-export { CustomField } from './selectable/custom-field';
-export { OrderableEdmTypeField } from './selectable/orderable-edm-type-field';
-export {
-  EntityBase,
-  Constructable,
-  EntityIdentifiable,
-  EntityBuilderType
-} from './entity-base';
-export { Time } from './time';
-export {
-  deserializersCommon,
-  serializersCommon
-} from './payload-value-converter';
-export {
-  uriConvertersCommon,
-  UriConverter,
-  convertToUriForEdmString
-} from './uri-conversion/uri-value-converter';
-export { createGetResourcePathForKeys } from './uri-conversion/get-resource-path';
-export { ODataUri } from './uri-conversion/odata-uri';
-export { Expandable } from './expandable';
-export { OneToManyLink } from './selectable/one-to-many-link';
-export { OneToOneLink } from './selectable/one-to-one-link';
-export { Link } from './selectable/link';
-export { getEntityKeys } from './uri-conversion/get-keys';
-
-export { EntitySerializer, entitySerializer } from './entity-serializer';
-export { EntityDeserializer, entityDeserializer } from './entity-deserializer';
-export { Selectable } from './selectable/selectable';
-export { Orderable } from './order/orderable';
-
-export {
-  FilterFunctionReturnType,
-  createFilterFunction,
-  numberReturnTypeMapping
-} from './filter/filter-function';
-export {
-  FilterFunctionParameterType,
-  FilterFunctionPrimitiveParameterType
-} from './filter/filter-function-base';
-export { toFilterableList } from './selectable/one-to-many-link';
-export { fromEdmToNumber, fromNumberToEdm } from './payload-value-converter';
-export { extractCustomFields } from './entity-deserializer';
-export { buildHeaders } from './header-builder';
-export { getEntityConstructor } from './selectable/complex-type-field';
-export { ErrorResponse } from './batch-response';
+export * from './filter';
+export * from './order';
+export * from './request';
+export * from './request-builder';
+export * from './selectable';
+export * from './uri-conversion';
+export * from './payload-value-converter';
+export * from './entity-builder';
+export * from './response-transformer';
+export * from './properties-util';
+export * from './batch-response';
+export * from './response-data-accessor';
+export * from './edm-types';
+export * from './entity-base';
+export * from './time';
+export * from './expandable';
+export * from './entity-serializer';
+export * from './entity-deserializer';
+export * from './header-builder';

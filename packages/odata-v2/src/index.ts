34c41060ef307d9ba51fe126c3bc81566c7f7ea6
Marika Marszalkowski <868536+marikaner@users.noreply.github.com>
2021-11-26 13:10:24 +0100

    Apply suggestions from code review

--- packages/odata-v2/src/index.ts
+++ packages/odata-v2/src/index.ts
@@ -13,15 +13,16 @@ export {
   transformReturnValueForEdmTypeList,
   transformReturnValueForEntity,
   transformReturnValueForEntityList,
-  transformReturnValueForUndefined,
-  CreateRequestBuilder,
-  DeleteRequestBuilder,
-  GetAllRequestBuilder,
-  GetByKeyRequestBuilder,
-  ODataBatchRequestBuilder,
-  UpdateRequestBuilder,
-  FunctionImportRequestBuilder
-} from './request-builder';
+transformReturnValueForUndefined
+} from './request-builder/response-transformers';
+export { CreateRequestBuilder } from './request-builder/create-request-builder';
+export { DeleteRequestBuilder } from './request-builder/delete-request-builder';
+export { GetAllRequestBuilder } from './request-builder/get-all-request-builder';
+export { GetByKeyRequestBuilder } from './request-builder/get-by-key-request-builder';
+export { ODataBatchRequestBuilder } from './request-builder/batch-request-builder';
+export { UpdateRequestBuilder } from './request-builder/update-request-builder';
+export { FunctionImportRequestBuilder } from './request-builder/function-import-request-builder';
+export { CustomField } from './selectable/custom-field';
 
 export { Entity } from './entity';
 export { CustomField } from './selectable';

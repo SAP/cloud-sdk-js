a9b9bd4ba1bd41928104373b5d79ee5298fd8cd0
Marika Marszalkowski <868536+marikaner@users.noreply.github.com>
2021-11-26 13:22:24 +0100

    Update packages/http-client/src/csrf-token-header.ts

--- packages/http-client/src/csrf-token-header.ts
+++ packages/http-client/src/csrf-token-header.ts
@@ -12,7 +12,7 @@ import {
   DestinationFetchOptions
 } from '@sap-cloud-sdk/connectivity';
 import { HttpRequestConfig } from './http-client-types';
-import { executeHttpRequest } from './http-client';
+import { executeHttpRequest } from '.';
 
 const logger = createLogger({
   package: 'http-client',

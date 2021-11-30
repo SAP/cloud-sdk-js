aecaf710040825fb95c95e3ef590b7619403025e
d068544 <frank.essenberger@sap.com>
2021-11-30 16:34:05 +0100

    remove checked in sources

--- test-packages/test-services/v4/common-service/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from './CommonEntity';
-export * from './CommonEntitySingleLink';
-export * from './CommonEntityRequestBuilder';
-export * from './CommonEntitySingleLinkRequestBuilder';
-export * from './CommonComplexType';
-export * from './NestedComplexType';
-export * from './BatchRequest';
-//# sourceMappingURL=index.d.ts.map

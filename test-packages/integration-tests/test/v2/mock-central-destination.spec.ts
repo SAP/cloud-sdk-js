f2faae09d3424964ebfda2c2aff1644253e3e361
d068544 <frank.essenberger@sap.com>
2021-12-01 17:02:31 +0100

    linter

--- test-packages/integration-tests/test/v2/mock-central-destination.spec.ts
+++ test-packages/integration-tests/test/v2/mock-central-destination.spec.ts
@@ -1,7 +1,11 @@
 import { unlinkSync, writeFileSync } from 'fs';
 import { TestEntity } from '@sap-cloud-sdk/test-services/v2/test-service';
 import nock from 'nock';
-import {basicHeader, mockAllTestDestinations, unmockAllTestDestinations} from '@sap-cloud-sdk/connectivity';
+import {
+  basicHeader,
+  mockAllTestDestinations,
+  unmockAllTestDestinations
+} from '@sap-cloud-sdk/connectivity';
 import { testEntityCollectionResponse } from '../test-data/test-entity-collection-response';
 
 describe('mockAllTestDestinations', () => {

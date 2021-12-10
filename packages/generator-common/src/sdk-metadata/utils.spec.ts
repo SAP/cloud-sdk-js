bb8883c69c06773504d0f535a8d5efb90b4e2d13
Junjie Tang <junjie.tang@sap.com>
2021-12-10 12:27:36 +0100

    adjust pipeline

--- packages/generator-common/src/sdk-metadata/utils.spec.ts
+++ packages/generator-common/src/sdk-metadata/utils.spec.ts
@@ -2,11 +2,6 @@ import { getSdkVersion } from './util';
 
 describe('utils', () => {
   it('getSdkVersion returns a valid stable version', async () => {
-    const version = await getSdkVersion();
-    if (!version.includes('beta')) {
-      expect(version.split('.').length).toBe(3);
-    } else {
-      expect(version.split('.').length).toBe(4);
-    }
+    expect((await getSdkVersion()).split('.').length).toBe(3);
   });
 });

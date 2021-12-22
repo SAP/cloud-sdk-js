24dff0c4c5cba39e949cec7820066328c7a948f6
I519840 <tom.frenken@sap.com>
2021-12-22 10:05:47 +0100

    add test case

--- packages/connectivity/src/scp-cf/client-credentials-token-cache.spec.ts
+++ packages/connectivity/src/scp-cf/client-credentials-token-cache.spec.ts
@@ -1,3 +1,4 @@
+import { createLogger } from '@sap-cloud-sdk/util';
 import { clientCredentialsTokenCache } from './client-credentials-token-cache';
 
 const oneHourInSeconds = 60 * 60;
@@ -54,5 +55,36 @@ describe('ClientCredentialsTokenCache', () => {
     );
 
     expect(expired).toBeUndefined();
+  });
+
+  it('should return undefined if the cache key is at least partly undefined', () => {
+    const logger = createLogger('client-credentials-token-cache');
+    const warn = jest.spyOn(logger, 'warn');
+
+    const validToken = {
+      access_token: '1234567890',
+      token_type: 'UserToken',
+      expires_in: oneHourInSeconds * 3,
+      jti: '',
+      scope: ''
+    };
+
+    clientCredentialsTokenCache.cacheToken(
+      'https://url_valid',
+      'clientid',
+      validToken
+    );
+
+    jest.advanceTimersByTime(oneHourInSeconds * 2 * 1000);
+
+    const invalid = clientCredentialsTokenCache.getToken(
+      'https://url_valid',
+      undefined as any
+    );
+
+    expect(invalid).toBeUndefined();
+    expect(warn).toBeCalledWith(
+      'Cannot get cache key. The ClientId was undefined.'
+    );
   });
 });

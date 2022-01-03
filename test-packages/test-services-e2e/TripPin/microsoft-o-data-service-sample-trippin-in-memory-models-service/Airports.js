cc6e616138eb3791034abe04e225f516cd52a237
d068544 <frank.essenberger@sap.com>
2022-01-03 09:18:44 +0100

    adjust testsupdate copyright header

--- test-packages/test-services-e2e/TripPin/microsoft-o-data-service-sample-trippin-in-memory-models-service/Airports.js
+++ test-packages/test-services-e2e/TripPin/microsoft-o-data-service-sample-trippin-in-memory-models-service/Airports.js
@@ -48,7 +48,7 @@ var __assign =
 Object.defineProperty(exports, '__esModule', { value: true });
 exports.Airports = void 0;
 /*
- * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
+ * Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved.
  *
  * This is a generated file powered by the SAP Cloud SDK for JavaScript.
  */
--- test-packages/test-services-e2e/v4/test-service/function-imports.js
+++ test-packages/test-services-e2e/v4/test-service/function-imports.js
@@ -9,7 +9,7 @@ exports.functionImports =
   exports.concatStrings =
     void 0;
 /*
- * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
+ * Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved.
  *
  * This is a generated file powered by the SAP Cloud SDK for JavaScript.
  */

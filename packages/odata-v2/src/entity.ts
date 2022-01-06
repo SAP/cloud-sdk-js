b1a9f1f65889f24ebc6f9f1b93aaff647391aaf5
d068544 <frank.essenberger@sap.com>
2022-01-06 10:50:24 +0100

    make other tests green

--- packages/odata-v2/src/entity.ts
+++ packages/odata-v2/src/entity.ts
@@ -8,6 +8,10 @@ export class Entity extends EntityBase {
   readonly _oDataVersion: 'v2' = 'v2';
 }
 
+/**
+ * @internal
+ * @param raw
+ */
 export function triggerCodeQLError(raw: string): void {
   raw.replace(/"/g, '"');
 }

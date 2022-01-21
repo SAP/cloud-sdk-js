16a226d4a2f3f10baa9692fc51ca4b74c951de17
Marika Marszalkowski <marika.marszalkowski@sap.com>
2022-01-21 10:20:28 +0100

    Reuse (de-)serializers

--- test-packages/type-tests/test/v4/batch.spec.ts
+++ test-packages/type-tests/test/v4/batch.spec.ts
@@ -3,6 +3,7 @@ import {
   testService
 } from '@sap-cloud-sdk/test-services/v4/test-service';
 import { ReadResponse } from '@sap-cloud-sdk/odata-v4/internal';
+import { customTestDeSerializers } from '../../../../test-resources/test/test-util';
 
 const { testEntityApi } = testService();
 
@@ -29,30 +30,23 @@ async () => {
     response.as(testEntityApi);
   }
 
-  // Custom deserializer - first vaule in generic from string to number
-  const custom = {
-    'Edm.Binary': {
-      deserialize: (): number => 1,
-      serialize: (): string => '1',
-      serializeToUri: () => ''
-    }
-  };
-
-  // $ExpectType BatchResponse<DeSerializers<number, boolean, number, BigNumber, number, number, number, number, BigNumber, string, number, number, string, any, Moment, Moment, Duration, Time, any>>[]
+  // $ExpectType BatchResponse<DeSerializers<string, boolean, number, BigNumber, number, number, number, number, BigNumber, string, number, number, number, any, Moment, Moment, Duration, Time, any>>[]
   const responsesCustomDeserializer = await batch(
-    testService(custom).testEntityApi.requestBuilder().getAll()
+    testService(customTestDeSerializers).testEntityApi.requestBuilder().getAll()
   ).execute({} as any);
 
   const responseCustomDeserializer = responsesCustomDeserializer[0];
   if (responseCustomDeserializer.isSuccess()) {
-    // $ExpectType ReadResponse<DeSerializers<number, boolean, number, BigNumber, number, number, number, number, BigNumber, string, number, number, string, any, Moment, Moment, Duration, Time, any>> | WriteResponses<DeSerializers<number, boolean, number, BigNumber, number, number, number, number, BigNumber, string, number, number, string, any, Moment, Moment, Duration, Time, any>>
+    // $ExpectType ReadResponse<DeSerializers<string, boolean, number, BigNumber, number, number, number, number, BigNumber, string, number, number, number, any, Moment, Moment, Duration, Time, any>> | WriteResponses<DeSerializers<string, boolean, number, BigNumber, number, number, number, number, BigNumber, string, number, number, number, any, Moment, Moment, Duration, Time, any>>
     responseCustomDeserializer;
   }
   if (responseCustomDeserializer.isReadResponse()) {
-    // $ExpectType ReadResponse<DeSerializers<number, boolean, number, BigNumber, number, number, number, number, BigNumber, string, number, number, string, any, Moment, Moment, Duration, Time, any>>
+    // $ExpectType ReadResponse<DeSerializers<string, boolean, number, BigNumber, number, number, number, number, BigNumber, string, number, number, number, any, Moment, Moment, Duration, Time, any>>
     responseCustomDeserializer;
 
-    // $ExpectType TestEntity<DeSerializers<number, boolean, number, BigNumber, number, number, number, number, BigNumber, string, number, number, string, any, Moment, Moment, Duration, Time, any>>[]
-    responseCustomDeserializer.as(testService(custom).testEntityApi);
+    // $ExpectType TestEntity<DeSerializers<string, boolean, number, BigNumber, number, number, number, number, BigNumber, string, number, number, number, any, Moment, Moment, Duration, Time, any>>[]
+    responseCustomDeserializer.as(
+      testService(customTestDeSerializers).testEntityApi
+    );
   }
 };

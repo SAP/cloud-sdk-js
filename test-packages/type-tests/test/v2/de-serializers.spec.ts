9d5a493e30fb6be029d62b2679fcd166c1b5f8ad
Marika Marszalkowski <marika.marszalkowski@sap.com>
2022-01-21 10:21:53 +0100

    Reuse more

--- test-packages/type-tests/test/v2/de-serializers.spec.ts
+++ test-packages/type-tests/test/v2/de-serializers.spec.ts
@@ -1,15 +1,9 @@
 import { mergeDefaultDeSerializersWith } from '@sap-cloud-sdk/odata-v2/internal';
 import { temporalDeSerializersV2 } from '@sap-cloud-sdk/temporal-de-serializers';
-
-const customDeSerializers = {
-  'Edm.String': {
-    serialize: () => '3',
-    deserialize: () => 3
-  }
-};
+import { customTestDeSerializers } from '../../../../test-resources/test/test-util';
 
 // $ExpectType CustomDeSerializers<Partial<DeSerializers<string, boolean, number, BigNumber, number, number, number, number, BigNumber, string, number, number, number, any, Moment, Moment, Time>>>
-mergeDefaultDeSerializersWith(customDeSerializers);
+mergeDefaultDeSerializersWith(customTestDeSerializers);
 
 // $ExpectType CustomDeSerializers<Partial<DeSerializers<string, boolean, number, BigNumber, number, number, number, number, BigNumber, string, number, number, string, any, PlainDateTime, ZonedDateTime, PlainTime>>>
 mergeDefaultDeSerializersWith(temporalDeSerializersV2);

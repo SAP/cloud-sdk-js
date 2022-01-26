9a492806e747538d2cd7f5ca69245ce1665f5250
d068544 <frank.essenberger@sap.com>
2022-01-26 13:16:52 +0100

    add rught type to base

--- packages/odata-common/src/request-builder/get-all-request-builder-base.ts
+++ packages/odata-common/src/request-builder/get-all-request-builder-base.ts
@@ -1,8 +1,5 @@
 import { variadicArgumentToArray } from '@sap-cloud-sdk/util';
-import {
-  Destination,
-  DestinationFetchOptions
-} from '@sap-cloud-sdk/connectivity';
+import { DestinationOrFetchOptions } from '@sap-cloud-sdk/connectivity';
 import { EntityBase } from '../entity-base';
 import { Selectable } from '../selectable';
 import { Orderable } from '../order';
@@ -109,9 +106,7 @@ export abstract class GetAllRequestBuilderBase<
    * @param options - Options to employ when fetching destinations
    * @returns A promise resolving to the requested entities
    */
-  async execute(
-    destination: Destination | DestinationFetchOptions
-  ): Promise<EntityT[]> {
+  async execute(destination: DestinationOrFetchOptions): Promise<EntityT[]> {
     return this.executeRaw(destination).then(response =>
       this.dataAccessor
         .getCollectionResult(response.data)

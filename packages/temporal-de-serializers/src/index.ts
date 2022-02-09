b01674c345bda38a8e3fa53ad730dff0e57fc45c
Junjie Tang <junjie.tang@sap.com>
2022-02-09 14:25:17 +0100

    fix missing links to new packages

--- packages/temporal-de-serializers/src/index.ts
+++ packages/temporal-de-serializers/src/index.ts
@@ -1,2 +1,9 @@
+/* eslint-disable tsdoc/syntax */
+/**
+ * [[include:temporal-de-serializers/README.md]]
+ * @packageDocumentation
+ * @module @sap-cloud-sdk/temporal-de-serializers
+ */
+
 export { temporalDeSerializersV2 } from './temporal-de-serializers-v2';
 export { temporalDeSerializersV4 } from './temporal-de-serializers-v4';

d65e4fd95e71150bc1f4602386bf26c279f23053
Junjie Tang <junjie.tang@sap.com>
2022-03-24 09:56:55 +0100

    typo

--- packages/connectivity/src/scp-cf/resilience-options.spec.ts
+++ packages/connectivity/src/scp-cf/resilience-options.spec.ts
@@ -2,7 +2,7 @@ import CircuitBreaker from 'opossum';
 import { CircuitBreakerOptions } from './resilience-options';
 
 describe('resilience-options', () => {
-  // when compiling a generated client the compiler fails, because the CircuitBreaker.Options from @types/opssum are not present (long import chain)
+  // when compiling a generated client the compiler fails, because the CircuitBreaker.Options from @types/opossum are not present (long import chain)
   // since these types contain the @types/node they are 1.8MB in size and we do not want to include this as a prod dependency of the connectivity module.
   // Hence we copied the CircuitBreaker options and added this test to ensure type compatibility.
   it('ensure type compatibility to opossum', () => {

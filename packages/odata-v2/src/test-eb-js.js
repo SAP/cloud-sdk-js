ca2cf53e21adecbd864390e03ac6dbdef69e7085
Junjie Tang <junjie.tang@sap.com>
2022-04-05 16:34:08 +0200

    fix schema

--- packages/odata-v2/src/test-eb-js.js
+++ packages/odata-v2/src/test-eb-js.js
@@ -12,24 +12,27 @@ const sdkCode = async () => {
   const testProp = new TestProp();
   const col = [];
   // const t = new TestEntity(testService().testEntityApi.schema);
-  var hd0 = new memwatch.HeapDiff();
-  for(let i=0;i<10000;i++) {
-    col[i] = api.schema;
-    // col[i] = testSchema.schema;
-    // col[i] = TestNS;
-    // col[i] = testProp.schema;
-  }
-  col[0].k0 = undefined;
-  col[1].KEY_PROPERTY_STRING = 'ss';
-  var diff0 = hd0.end();
-  console.log(diff0);
-  // var hd1 = new memwatch.HeapDiff();
-  // // entityBuilder => 12 * 1000 EdmTypeField + 24 * 1000 OrderableEdmTypeField
-  // for(let i=0;i<1000;i++) {
-  //   col[i] = testService().testEntityApi.entityBuilder();
+
+  // var hd0 = new memwatch.HeapDiff();
+  // for(let i=0;i<10000;i++) {
+  //   col[i] = api.schema;
+  //   // col[i] = testSchema.schema;
+  //   // col[i] = TestNS;
+  //   // col[i] = testProp.schema;
   // }
-  // var diff1 = hd1.end();
-  // console.log(diff1);
+  // col[0].k0 = undefined;
+  // col[1].KEY_PROPERTY_STRING = 'ss';
+  // var diff0 = hd0.end();
+  // console.log(diff0);
+
+  var hd1 = new memwatch.HeapDiff();
+  // entityBuilder => 12 * 1000 EdmTypeField + 24 * 1000 OrderableEdmTypeField
+  for(let i=0;i<1000;i++) {
+    col[i] = api.entityBuilder();
+  }
+  var diff1 = hd1.end();
+  console.log(diff1);
+
   // var hd2 = new memwatch.HeapDiff();
   // // entityBuilder => 12 EdmTypeField + 24 OrderableEdmTypeField
   // //TODO fix testService singleton
@@ -39,9 +42,11 @@ const sdkCode = async () => {
   // col[1000] = testService().testEntityApi.entityBuilder();
   // var diff2 = hd2.end();
   // console.log(diff2);
+
   // var hd3 = new memwatch.HeapDiff();
   // const api = testService().testEntityApi;
   // for(let i=0;i<1000;i++) {
+  // //singleton can help here, which cannot be applied unfortunately
   //   col[i] = api.entityBuilderNew();
   // }
   // var diff3 = hd3.end();

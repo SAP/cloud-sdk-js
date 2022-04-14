2186a130e027b56d7e46a8c22d2f844be6f64733
Junjie Tang <junjie.tang@sap.com>
2022-04-14 14:49:12 +0200

    all

--- test-packages/mermory-tests/sdk-v1/test-script.js
+++ test-packages/mermory-tests/sdk-v1/test-script.js
@@ -11,30 +11,30 @@ const sdkCode = async () => {
   const hd = new memwatch.HeapDiff();
 
   const testEntities101 = await TestEntity.requestBuilder().getAll().execute(dest);
-  // const testEntities102 = await TestEntity.requestBuilder().getAll().execute(dest);
-  // const testEntities103 = await TestEntity.requestBuilder().getAll().execute(dest);
-  // const testEntities104 = await TestEntity.requestBuilder().getAll().execute(dest);
-  // const testEntities105 = await TestEntity.requestBuilder().getAll().execute(dest);
-  // const testEntities106 = await TestEntity.requestBuilder().getAll().execute(dest);
-  // const testEntities107 = await TestEntity.requestBuilder().getAll().execute(dest);
-  // const testEntities108 = await TestEntity.requestBuilder().getAll().execute(dest);
-  // const testEntities109 = await TestEntity.requestBuilder().getAll().execute(dest);
-  // const testEntities110 = await TestEntity.requestBuilder().getAll().execute(dest);
-  //
-  // const testEntities201 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
-  // const testEntities202 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
-  // const testEntities203 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
-  // const testEntities204 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
-  // const testEntities205 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
-  // const testEntities206 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
-  // const testEntities207 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
-  // const testEntities208 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
-  // const testEntities209 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
-  // const testEntities210 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  const testEntities102 = await TestEntity.requestBuilder().getAll().execute(dest);
+  const testEntities103 = await TestEntity.requestBuilder().getAll().execute(dest);
+  const testEntities104 = await TestEntity.requestBuilder().getAll().execute(dest);
+  const testEntities105 = await TestEntity.requestBuilder().getAll().execute(dest);
+  const testEntities106 = await TestEntity.requestBuilder().getAll().execute(dest);
+  const testEntities107 = await TestEntity.requestBuilder().getAll().execute(dest);
+  const testEntities108 = await TestEntity.requestBuilder().getAll().execute(dest);
+  const testEntities109 = await TestEntity.requestBuilder().getAll().execute(dest);
+  const testEntities110 = await TestEntity.requestBuilder().getAll().execute(dest);
+
+  const testEntities201 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  const testEntities202 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  const testEntities203 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  const testEntities204 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  const testEntities205 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  const testEntities206 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  const testEntities207 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  const testEntities208 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  const testEntities209 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  const testEntities210 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
 
   const diff = hd.end();
   console.log("Additional heap memory  used: " + diff.change.size);
-  console.log("Major changes: " + JSON.stringify(diff.change.details.filter(e => e.size.includes('mb'))));
+  console.log("Major changes: " + JSON.stringify(diff.change.details.filter(e => e.size.includes('kb' || e.size.includes('mb'))));
 };
 
 async function main(){

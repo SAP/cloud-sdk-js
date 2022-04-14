dda8c3ddc20b086524e52ef374f10ad1a50e563e
Junjie Tang <junjie.tang@sap.com>
2022-04-14 11:32:16 +0200

    inspect

---
+++ test-packages/mermory-tests/sdk-v1/test-script.js
@@ -0,0 +1,48 @@
+const memwatch = require('node-memwatch-new');
+const { TestEntity, TestEntity50Col } = require('./test-service');
+
+const dest = {url: 'http://localhost:4004/' };
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+const sdkCode = async () => {
+  const hd = new memwatch.HeapDiff();
+
+  // const testEntities101 = await TestEntity.requestBuilder().getAll().execute(dest);
+  // const testEntities102 = await TestEntity.requestBuilder().getAll().execute(dest);
+  // const testEntities103 = await TestEntity.requestBuilder().getAll().execute(dest);
+  // const testEntities104 = await TestEntity.requestBuilder().getAll().execute(dest);
+  // const testEntities105 = await TestEntity.requestBuilder().getAll().execute(dest);
+  // const testEntities106 = await TestEntity.requestBuilder().getAll().execute(dest);
+  // const testEntities107 = await TestEntity.requestBuilder().getAll().execute(dest);
+  // const testEntities108 = await TestEntity.requestBuilder().getAll().execute(dest);
+  // const testEntities109 = await TestEntity.requestBuilder().getAll().execute(dest);
+  // const testEntities110 = await TestEntity.requestBuilder().getAll().execute(dest);
+  //
+  // const testEntities201 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  // const testEntities202 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  // const testEntities203 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  // const testEntities204 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  // const testEntities205 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  // const testEntities206 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  // const testEntities207 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  // const testEntities208 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  // const testEntities209 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+  // const testEntities210 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
+
+  const diff = hd.end();
+  console.log("Additional heap memory  used: " + diff.change.size);
+  console.log("Major changes: " + JSON.stringify(diff.change.details.filter(e => e.size.includes('mb'))));
+};
+
+async function main(){
+  console.log('***** test starts *****');
+
+  await sdkCode();
+
+  console.log('***** test ends *****');
+}
+
+main();

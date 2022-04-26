826d05898c7a58ae4f4bfd49070fcc7955b95984
Junjie Tang <junjie.tang@sap.com>
2022-04-26 10:42:17 +0200

    test positive/negative results

--- scripts/compare-memory-usage.ts
+++ scripts/compare-memory-usage.ts
@@ -1,11 +1,13 @@
-const threshold = 0.9;
+const threshold = 90;
 
 const main = () => {
   const v1MemoryUsage = parseInt(process.argv[2]);
   const canaryMemoryUsage = parseInt(process.argv[3]);
-  if(v1MemoryUsage * threshold < canaryMemoryUsage) {
+  const percentageBasedOnV1 = canaryMemoryUsage/v1MemoryUsage * 100;
+  if(percentageBasedOnV1 > threshold) {
     throw new Error(`Test failed: v1 memory usage is ${v1MemoryUsage} but canary memory usage is ${canaryMemoryUsage}. Consider change the threshold or fix it.`);
   }
+  console.log(`Test passed: v1 memory usage is ${v1MemoryUsage} and canary memory usage is ${canaryMemoryUsage} (${percentageBasedOnV1} %).`)
 }
 
 main()

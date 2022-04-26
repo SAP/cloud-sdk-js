a81446b6460b960d9df29b8e5dd5d987b42a036d
Junjie Tang <junjie.tang@sap.com>
2022-04-26 10:12:53 +0200

    test

--- test-packages/mermory-tests/sdk-v1/test-script.js
+++ test-packages/mermory-tests/sdk-v1/test-script.js
@@ -1,9 +1,11 @@
 const memwatch = require('node-memwatch-new');
+const { writeFileSync } = require('fs');
+const { resolve } = require('path');
 const { TestEntity, TestEntity50Col } = require('./test-service');
 
 const dest = {url: 'http://localhost:4004/' };
 
-const main = async () => {
+const test = async () => {
   // The script is written in js (not ts) and executed with --inspect flag because of this lib.
   const hd = new memwatch.HeapDiff();
 
@@ -30,9 +32,19 @@ const main = async () => {
   // const testEntities210 = await TestEntity50Col.requestBuilder().getAll().execute(dest);
 
   const diff = hd.end();
+  console.log("Additional heap memory  used: " + diff.change.size);
+  console.log("Major changes: " + JSON.stringify(diff.change.details.filter(e => e.size.includes('kb') || e.size.includes('mb'))));
   console.log(diff.change.size_bytes);
-  // console.log("Additional heap memory  used: " + diff.change.size);
-  // console.log("Major changes: " + JSON.stringify(diff.change.details.filter(e => e.size.includes('kb') || e.size.includes('mb'))));
+  return diff.change.size_bytes;
 };
+
+const main = async () => {
+  const result = await test();
+  writeFileSync(
+    resolve(__dirname, './result.txt'),
+    result,
+    'utf8'
+  );
+}
 
 main();

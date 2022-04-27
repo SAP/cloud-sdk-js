00e8c93f50c22e7effc54898097a99b70e2f44c4
d068544 <frank.essenberger@sap.com>
2022-04-27 13:06:10 +0200

    remakrs marika

--- packages/util/src/error-with-cause.ts
+++ packages/util/src/error-with-cause.ts
@@ -1,13 +1,9 @@
 import { AxiosError } from 'axios';
 import { unixEOL } from './string-formatter';
+
 /**
  * Represents an error that was caused by another error.
  */
-
-function isAxiosError(err: Error | AxiosError): err is AxiosError {
-  return err['isAxiosError'] === true;
-}
-
 export class ErrorWithCause extends Error {
   /**
    * Create an instance of ErrorWithCause.
@@ -20,17 +16,27 @@ export class ErrorWithCause extends Error {
     Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
     this.name = 'ErrorWithCause';
 
+    this.addStack(cause);
+  }
+
+  private isAxiosError(err: Error | AxiosError): err is AxiosError {
+    return err['isAxiosError'] === true;
+  }
+
+  private addStack(cause: Error) {
     // Axios removed the stack property in version 0.27 which gave no useful information anyway. This adds the http cause.
-    if (isAxiosError(cause)) {
-      this.stack = `${this.stack}${unixEOL}Caused by:${unixEOL}Http Response: ${cause.message} - ${cause?.response?.data}`;
+    if (this.isAxiosError(cause)) {
+      const response = cause.response?.data ? ` - ${cause.response?.data}` : '';
+      this.stack = `${this.stack}${unixEOL}Caused by:${unixEOL}HTTP Response: ${cause.message}${response}`;
+      return;
     }
 
     // Stack is a non-standard property according to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#Custom_Error_Types
     if (this.stack && cause?.stack) {
       this.stack = `${this.stack}${unixEOL}Caused by:${unixEOL}${cause.stack}`;
+      return;
     }
   }
-
   /**
    * Root cause of the error.
    * If there are multiple errors caused one by another, the root cause is the first error that occurred.

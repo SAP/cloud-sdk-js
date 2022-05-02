3ea4a0514d474f48c0abd4e9509ca90efe7a2c19
Zhongpin Wang <zhongpin.wang@sap.com>
2022-05-02 18:53:26 +0200

    fix: wrong package name in type test

--- test-packages/type-tests/test/v4/filters.spec.ts
+++ test-packages/type-tests/test/v4/filters.spec.ts
@@ -1,7 +1,7 @@
 import {
   TestEnumType,
   testService
-} from '@sap-cloud-sdk/test-services/v4/test-service';
+} from '@sap-cloud-sdk/test-services-odata-v4/test-service';
 
 const { testEntityApi } = testService();
 

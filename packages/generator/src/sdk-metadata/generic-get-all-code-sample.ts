d362bbdb054a9e86d19b1f6cf3ee4075be7b51c0
deekshas8 <deeksha.sinha@sap.com>
2022-05-04 14:19:16 +0200

    Revert "revert -- fix"
    This reverts commit 962bc720dc51029a62c0285248eb5c041a118c56.

--- packages/generator/src/sdk-metadata/generic-get-all-code-sample.ts
+++
@@ -1,4 +0,0 @@
-import { testService } from '@sap-cloud-sdk/test-services-odata-v2/test-service';
-
-const { testEntityApi } = testService();
-const resultPromise = testEntityApi.requestBuilder().getAll().top(5).execute({ destinationName: 'myDestinationName' });

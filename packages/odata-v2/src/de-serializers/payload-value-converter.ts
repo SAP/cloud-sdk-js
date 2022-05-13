07641897c4bac9aecd77ed288f876462627d7447
Junjie Tang <33489572+jjtang1985@users.noreply.github.com>
2022-05-13 09:24:09 +0200

    Update packages/odata-v2/src/de-serializers/payload-value-converter.ts
    Co-authored-by: Marika Marszalkowski <868536+marikaner@users.noreply.github.com>

--- packages/odata-v2/src/de-serializers/payload-value-converter.ts
+++ packages/odata-v2/src/de-serializers/payload-value-converter.ts
@@ -2,9 +2,9 @@ import { EdmTypeShared } from '@sap-cloud-sdk/odata-common/internal';
 import { DeSerializers } from './de-serializers';
 
 /**
- * Convert a value with edm format to one with typescript format. This function is typically used when deserializing an OData response.
- * @param value - The original value with edm format.
- * @param edmType - The edm type of that value.
+ * Convert a value from EDM format to TypeScript. This function is typically used when deserializing an OData response.
+ * @param value - The original value with EDM format.
+ * @param edmType - The EDM type of that value.
  * @param deSerializers - (De-)serializers used for transformation.
  * @returns Deserialized value.
  */

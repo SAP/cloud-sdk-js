e59a74deeaca0a812fcf60c9d55bd3bee69df2e9
Junjie Tang <33489572+jjtang1985@users.noreply.github.com>
2022-06-27 10:04:50 +0200

    Update test-data.ts

--- packages/odata-v2/test/test-util/test-data.ts
+++ packages/odata-v2/test/test-util/test-data.ts
@@ -11,11 +11,11 @@ import {
 
 const testService = testService();
 
-export testEntityApi = testService.testEntityApi;
-export testEntityMultiLinkApi = testService.testEntityMultiLinkApi;
-export testEntitySingleLinkApi = testService.testEntitySingleLinkApi;
-export testEntityLvl2MultiLinkApi = testService.testEntityLvl2MultiLinkApi;
-export testEntityLvl2SingleLinkApi = testService.testEntityLvl2SingleLinkApi;
+export const testEntityApi = testService.testEntityApi;
+export const testEntityMultiLinkApi = testService.testEntityMultiLinkApi;
+export const testEntitySingleLinkApi = testService.testEntitySingleLinkApi;
+export const testEntityLvl2MultiLinkApi = testService.testEntityLvl2MultiLinkApi;
+export const testEntityLvl2SingleLinkApi = testService.testEntityLvl2SingleLinkApi;
 
 export const { testEntityApi: testEntityApiCustom } = testService(
   customTestDeSerializers

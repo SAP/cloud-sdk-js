60f23da8120a5da02a91ef5049e940fafae8b3d3
Junjie Tang <33489572+jjtang1985@users.noreply.github.com>
2022-06-27 10:34:53 +0200

    revert test data

--- packages/odata-v2/test/test-util/test-data.ts
+++ packages/odata-v2/test/test-util/test-data.ts
@@ -9,13 +9,13 @@ import {
   customTestDeSerializers
 } from '../../../../test-resources/test/test-util';
 
-const service = testService();
-
-export const testEntityApi = service.testEntityApi;
-export const testEntityMultiLinkApi = service.testEntityMultiLinkApi;
-export const testEntitySingleLinkApi = service.testEntitySingleLinkApi;
-export const testEntityLvl2MultiLinkApi = service.testEntityLvl2MultiLinkApi;
-export const testEntityLvl2SingleLinkApi = service.testEntityLvl2SingleLinkApi;
+export const {
+   testEntityApi,
+   testEntityMultiLinkApi,
+   testEntitySingleLinkApi,
+   testEntityLvl2MultiLinkApi,
+   testEntityLvl2SingleLinkApi
+ } = testService();
 
 export const { testEntityApi: testEntityApiCustom } = testService(
   customTestDeSerializers

63d13bde07f3bc7ae8f59f2497c971d4bbf112f2
d068544 <frank.essenberger@sap.com>
2022-07-08 13:57:51 +0200

    move xsuaa only when cache is needed

--- packages/connectivity/src/scp-cf/token-accessor.ts
+++ packages/connectivity/src/scp-cf/token-accessor.ts
@@ -38,10 +38,11 @@ export async function serviceToken(
 
   service = resolveService(service);
   const serviceCredentials = service.credentials;
-  // TODO 2.0 Once the xssec supports caching remove all xsuaa related content here and use their cache.
-  const xsuaa = multiTenantXsuaaCredentials(options);
 
+  // TODO 2.0 Once the xssec supports caching remove all xsuaa related content here and use their cache.
   if (opts.useCache) {
+    const xsuaa = multiTenantXsuaaCredentials(options);
+
     const cachedToken = clientCredentialsTokenCache.getToken(
       xsuaa.url,
       serviceCredentials.clientid

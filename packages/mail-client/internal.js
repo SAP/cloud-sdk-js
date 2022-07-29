05d82aa9bb7baef15530b281bb98425db00c62d5
Junjie Tang <33489572+jjtang1985@users.noreply.github.com>
2022-07-29 11:14:04 +0200

    feat: support send email with proxy type: internet (#2641)

---
+++ packages/mail-client/internal.js
@@ -0,0 +1,11 @@
+'use strict';
+function __export(m) {
+  for (const p in m) {
+    if (!exports.hasOwnProperty(p)) {
+      exports[p] = m[p];
+    }
+  }
+}
+Object.defineProperty(exports, '__esModule', { value: true });
+__export(require('./dist/internal'));
+// # sourceMappingURL=internal.js.map

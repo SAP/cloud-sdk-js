1c6d6e642bccc1c523b4b867665ae5a3899e0366
Shoji Soma <shoji.soma@sap.com>
2022-08-03 17:34:26 +0900

    remove unnecessaries

--- packages/odata-common/src/request-builder/action-function-import-request-builder-base.ts
+++ packages/odata-common/src/request-builder/action-function-import-request-builder-base.ts
@@ -24,8 +24,6 @@ export abstract class ActionFunctionImportRequestBuilderBase<
     super(requestConfig);
   }
 
-
-  //Zongping made a change a beloow to allow a deserialization
   /**
    * Execute request.
    * @param destination - Destination or DestinationFetchOptions to execute the request against.

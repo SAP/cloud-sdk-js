c8d90b3f71bed7198b07a721e5fa9e08b4c67925
Shoji Soma <shoji.soma@sap.com>
2022-08-10 19:46:52 +0900

    add function

--- packages/generator/src/generator-options.ts
+++ packages/generator/src/generator-options.ts
@@ -1,5 +1,5 @@
-import { PathLike } from 'fs';
-import { resolve } from 'path';
+import { PathLike, readFileSync } from 'fs';
+import { resolve, dirname } from 'path';
 import { Options } from 'yargs';
 import { createLogger } from '@sap-cloud-sdk/util';
 
@@ -182,3 +182,21 @@ export const generatorOptionsCli: KeysToOptions = {
     default: false
   }
 };
+
+/**
+ * @internal
+ */
+export function createOptionsFromConfig(configPath: string) {
+  const file = readFileSync(configPath, 'utf-8');
+  const pathLikeKeys = ['inputDir', 'outputDir', 'serviceMapping'];
+  return pathLikeKeys.reduce(
+    (json, pathLikeKey) =>
+      typeof json[pathLikeKey] === 'undefined'
+        ? json
+        : {
+          ...json,
+          [pathLikeKey]: resolve(dirname(configPath), json[pathLikeKey])
+        },
+    JSON.parse(file)
+  );
+}

f8ceeb8f2d5e08dfb606dd66283a66a0be47f951
Shoji Soma <shoji.soma@sap.com>
2022-08-15 20:56:30 +0900

    update test

--- test-packages/e2e-tests/test/generator-cli.spec.ts
+++ test-packages/e2e-tests/test/generator-cli.spec.ts
@@ -158,7 +158,7 @@ describe('generator-cli', () => {
       ]);
     } catch (err) {
       expect(err.stdout).toMatch(
-        /Error: No types found for API_TEST_SRV.A_TestComplexTypeMISTAKE | ErrorWithCause: Generation of services failed./
+        /Caused by: Error: No types found for API_TEST_SRV.A_TestComplexTypeMISTAKE | \(generator-cli\): ErrorWithCause: Generation of services failed\./
       );
     }
   });

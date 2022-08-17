aba4e76014d7e4762e9f4e9d9660fbb4c33fafcb
Shoji Soma <shoji.soma@sap.com>
2022-08-17 21:52:31 +0900

    remove formatter

--- packages/generator-common/src/package-description.ts
+++ packages/generator-common/src/package-description.ts
@@ -2,6 +2,4 @@
  * @internal
  */
 export const packageDescription = (packageName: string): string =>
-  `SAP Cloud SDK for JavaScript: Generated client for service ${packageName
-    .split('-')
-    .join(' ')}`;
+  `SAP Cloud SDK for JavaScript: Generated client for service ${packageName}`;

29bc6562be12a5aac6d46ec57d7d55da784b8b26
Shoji Soma <shoji.soma@sap.com>
2022-08-17 20:02:55 +0900

    remove duplicated parts

--- packages/generator/src/sdk-metadata/pregenerated-lib.ts
+++ packages/generator/src/sdk-metadata/pregenerated-lib.ts
@@ -1,18 +1,9 @@
+import { packageDescription } from '@sap-cloud-sdk/generator-common/internal';
 import { VdmServiceMetadata } from '../vdm-types';
-import { GeneratorOptions } from '../generator-options';
-import {
-  genericDescription,
-  s4hanaCloudDescription
-} from '../package-description';
 
 /**
  * @internal
  */
-export function getServiceDescription(
-  service: VdmServiceMetadata,
-  options: GeneratorOptions
-): string {
-  return options.s4hanaCloud
-    ? s4hanaCloudDescription(service.directoryName)
-    : genericDescription(service.directoryName);
+export function getServiceDescription(service: VdmServiceMetadata): string {
+  return packageDescription(service.directoryName);
 }

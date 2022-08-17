aac4d5109f5cb4c747d62ce9451f09b7cc637e11
Shoji Soma <shoji.soma@sap.com>
2022-08-17 20:04:22 +0900

    add new unified package-generator

---
+++ packages/generator-common/src/package-description.ts
@@ -0,0 +1,7 @@
+/**
+ * @internal
+ */
+export const packageDescription = (packageName: string): string =>
+  `SAP Cloud SDK for JavaScript: Generated client for service ${packageName
+    .split('-')
+    .join(' ')}`;

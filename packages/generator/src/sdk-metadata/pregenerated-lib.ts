b81ae1c1742d3b7cb50720185723d4cf112ccd6c
Shoji Soma <shoji.soma@sap.com>
2022-08-18 13:55:07 +0900

    add formatter option

--- packages/generator/src/sdk-metadata/pregenerated-lib.ts
+++ packages/generator/src/sdk-metadata/pregenerated-lib.ts
@@ -1,9 +1,17 @@
 import { packageDescription } from '@sap-cloud-sdk/generator-common/internal';
+import { GeneratorOptions } from '../generator-options';
 import { VdmServiceMetadata } from '../vdm-types';
 
 /**
  * @internal
  */
-export function getServiceDescription(service: VdmServiceMetadata): string {
-  return packageDescription(service.directoryName);
+export function getServiceDescription(
+  service: VdmServiceMetadata,
+  options: GeneratorOptions
+): string {
+  return options.s4hanaCloud
+    ? packageDescription(service.directoryName, (packageName: string) =>
+        packageName.split('-').join(' ')
+      )
+    : packageDescription(service.directoryName);
 }

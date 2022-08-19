8e400381efdf61fc4d54c0589d4d5a8a1de7c459
d068544 <frank.essenberger@sap.com>
2022-08-19 11:46:57 +0200

    linter

--- packages/odata-common/src/request-builder/batch/batch-change-set.ts
+++ packages/odata-common/src/request-builder/batch/batch-change-set.ts
@@ -24,8 +24,8 @@ export class BatchChangeSet<
 }
 
 /**
- *  Some function imports contain not serializable entities and the execute() method is removed from them.
- *  Since the execute method is not needed in batch the execute it is removed from all function imports.
+ * Some function imports contain not serializable entities and the execute() method is removed from them.
+ * Since the execute method is not needed in batch the execute it is removed from all function imports.
  */
 export type ChangesetBuilderTypes<DeSerializersT extends DeSerializers> =
   | CreateRequestBuilderBase<EntityBase, DeSerializersT>

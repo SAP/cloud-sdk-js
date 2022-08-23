364065ba936eabbec5caf9118d7a981d5611ec4a
Zhongpin Wang <zhongpin.wang@sap.com>
2022-08-23 13:22:43 +0200

    Fix lint rule for unused variables

--- test-packages/integration-tests/test/v2/service.spec.ts
+++ test-packages/integration-tests/test/v2/service.spec.ts
@@ -2,7 +2,8 @@ import { testService } from '@sap-cloud-sdk/test-services-odata-v2/test-service'
 
 describe('OData Client Service', () => {
   it('should contain schemas in nested apis', () => {
-    const { testEntityApi } = testService();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { testEntityApi, testEntityMultiLinkApi } = testService();
     expect(
       testEntityApi.schema?.TO_MULTI_LINK._linkedEntityApi.schema.TO_MULTI_LINK
         ._linkedEntityApi.schema.STRING_PROPERTY
--- test-packages/integration-tests/test/v4/service.spec.ts
+++ test-packages/integration-tests/test/v4/service.spec.ts
@@ -2,7 +2,9 @@ import { testService } from '@sap-cloud-sdk/test-services-odata-v4/test-service'
 
 describe('OData Client Service', () => {
   it('should contain schemas when having circular navigation properties', () => {
-    const { testEntityCircularLinkParentApi } = testService();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { testEntityCircularLinkParentApi, testEntityCircularLinkChildApi } =
+      testService();
     expect(
       testEntityCircularLinkParentApi.schema.TO_CHILDREN._linkedEntityApi.schema
         .TO_PARENT._linkedEntityApi.schema

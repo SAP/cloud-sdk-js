12d87c8d37a297e4409e10e9f754d31a84b60e81
Deeksha Sinha <88374536+deekshas8@users.noreply.github.com>
2022-09-14 09:34:42 +0200

    chore: clean up sdk metadata code (#2857)
    * remove metadata code
    
    * fix test
    
    * lint
    
    * fix test
    
    * revert

--- packages/generator/src/sdk-metadata/code-samples.ts
+++ packages/generator/src/sdk-metadata/code-samples.ts
@@ -1,16 +1,9 @@
 import { codeBlock } from '@sap-cloud-sdk/util';
-import {
-  InstructionWithText,
-  InstructionWithTextAndHeader,
-  usageHeaderText
-} from '@sap-cloud-sdk/generator-common/internal';
+import { MultiLineText } from '@sap-cloud-sdk/generator-common/internal';
 import voca from 'voca';
 import { VdmActionImport, VdmFunctionImport } from '../vdm-types';
 import { getApiName } from '../generator-without-ts-morph/service';
 import { getActionFunctionParams } from './code-sample-util';
-
-const instructionsText =
-  'To consume the service via the pregenerated typed client library run the code snippet below. For more details about OData client libraries chose "OData Consumption Manual" from the "Helpful Links" menu.';
 
 /**
  * @internal
@@ -18,41 +11,28 @@ const instructionsText =
 export function entityCodeSample(
   entityName: string,
   serviceName: string,
-  packageName: string
-): InstructionWithText {
-  return {
-    text: instructionsText,
-    instructions: codeBlock`
-import { ${voca.decapitalize(serviceName)} } from '${packageName}';
+  directoryName: string
+): MultiLineText {
+  return codeBlock`
+import { ${voca.decapitalize(
+    serviceName
+  )} } from './generated/${directoryName}';
 
 const { ${getApiName(entityName)} } = ${voca.decapitalize(serviceName)}();
 const resultPromise = ${getApiName(
-      entityName
-    )}.requestBuilder().getAll().top(5).execute({ destinationName: 'myDestinationName' });
+    entityName
+  )}.requestBuilder().getAll().top(5).execute({ destinationName: 'myDestinationName' });
 
-`
-  };
+`;
 }
-/**
- * @internal
- */
-export function genericEntityCodeSample(): InstructionWithTextAndHeader {
-  return {
-    ...entityCodeSample(
-      'BusinessPartner',
-      'BusinessPartnerService',
-      '@sap/cloud-sdk-vdm-business-partner-service'
-    ),
-    header: usageHeaderText
-  };
-}
+
 /**
  * @internal
  */
 export function functionImportCodeSample(
   functionImport: VdmFunctionImport,
   packageName: string
-): InstructionWithText {
+): MultiLineText {
   return importsCodeSample(functionImport, packageName);
 }
 /**
@@ -61,29 +41,26 @@ export function functionImportCodeSample(
 export function actionImportCodeSample(
   actionImport: VdmActionImport,
   packageName: string
-): InstructionWithText {
+): MultiLineText {
   return importsCodeSample(actionImport, packageName);
 }
 
 function importsCodeSample(
   actionFunctionImport: VdmFunctionImport | VdmActionImport,
   packageName: string
-): InstructionWithText {
-  return {
-    text: instructionsText,
-    instructions: codeBlock`
+): MultiLineText {
+  return codeBlock`
 import { ${actionFunctionImport.name}${
-      actionFunctionImport.parametersTypeName
-        ? `, ${actionFunctionImport.parametersTypeName}`
-        : ''
-    } } from '${packageName}';
+    actionFunctionImport.parametersTypeName
+      ? `, ${actionFunctionImport.parametersTypeName}`
+      : ''
+  } } from './generated/${packageName}';
 
 ${getParameterCodeSample(actionFunctionImport)}
 const resultPromise = ${actionFunctionImport.name}(${
-      actionFunctionImport.parametersTypeName ? 'parameter' : ''
-    }).execute({ destinationName: 'myDestinationName' });
-`
-  };
+    actionFunctionImport.parametersTypeName ? 'parameter' : ''
+  }).execute({ destinationName: 'myDestinationName' });
+`;
 }
 
 function getParameterCodeSample(functionImport: VdmFunctionImport): string {

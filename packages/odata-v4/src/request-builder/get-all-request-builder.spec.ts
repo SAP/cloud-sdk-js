1bc46d134740335c61ed3d33d27db32ba5f9593b
Shoji Soma <shoji.soma@sap.com>
2022-09-16 16:06:10 +0900

    move the tests from common

--- packages/odata-v4/src/request-builder/get-all-request-builder.spec.ts
+++ packages/odata-v4/src/request-builder/get-all-request-builder.spec.ts
@@ -1,4 +1,5 @@
 import { TestEntity } from '@sap-cloud-sdk/test-services-odata-v4/test-service';
+import { asc, desc } from '@sap-cloud-sdk/odata-common';
 import {
   defaultDestination,
   mockCountRequest,
@@ -31,6 +32,41 @@ describe('GetAllRequestBuilder', () => {
   });
 
   describe('url', () => {
+    it('should set ascending order', async () => {
+      const expected =
+        '/testination/sap/opu/odata/sap/API_TEST_SRV/A_TestEntity?$orderby=ComplexTypeProperty/StringProperty%20asc';
+      const request = await testEntityApi
+        .requestBuilder()
+        .getAll()
+        .orderBy(asc(testEntityApi.schema.COMPLEX_TYPE_PROPERTY.stringProperty))
+        .url(defaultDestination);
+      expect(request).toBe(expected);
+    });
+
+    it('should set descending order', async () => {
+      const expected =
+        '/testination/sap/opu/odata/sap/API_TEST_SRV/A_TestEntity?$orderby=ComplexTypeProperty/StringProperty%20desc';
+      const request = await testEntityApi
+        .requestBuilder()
+        .getAll()
+        .orderBy(
+          desc(testEntityApi.schema.COMPLEX_TYPE_PROPERTY.stringProperty)
+        )
+        .url(defaultDestination);
+      expect(request).toBe(expected);
+    });
+
+    it('should set ascending order when any order not specified', async () => {
+      const expected =
+        '/testination/sap/opu/odata/sap/API_TEST_SRV/A_TestEntity?$orderby=ComplexTypeProperty/StringProperty%20asc';
+      const request = await testEntityApi
+        .requestBuilder()
+        .getAll()
+        .orderBy(testEntityApi.schema.COMPLEX_TYPE_PROPERTY.stringProperty)
+        .url(defaultDestination);
+      expect(request).toBe(expected);
+    });
+    
     it('is built correctly', async () => {
       const expected =
         '/testination/sap/opu/odata/sap/API_TEST_SRV/A_TestEntity';

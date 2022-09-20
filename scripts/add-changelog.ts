856c079a4ce723c929dbe8700c3de7c7434c59c0
Frank Essenberger <56544999+FrankEssenberger@users.noreply.github.com>
2022-09-20 16:41:42 +0200

    chore: Add API docs link to release docs (#2890)

--- scripts/add-changelog.ts
+++ scripts/add-changelog.ts
@@ -20,7 +20,7 @@ function getChangelogWithVersion(v = currentSdkVersion): string {
   const year = date.getFullYear();
 
   const headerWithVersion = `${unixEOL}## ${v} [Core Modules] - ${month} ${day}, ${year}`
-  const apiReferenceLink= `**API Reference:** [${v}](https://sap.github.io/cloud-sdk/api/${v})`;
+  const apiReferenceLink= `${unixEOL}**API Reference:** [${v}](https://sap.github.io/cloud-sdk/api/${v})`;
 
   return [headerWithVersion,apiReferenceLink,logs].join(unixEOL);
 }
@@ -29,12 +29,12 @@ export function addCurrentChangelog(): void {
   const changelog = getChangelogWithVersion();
   const releaseNotes = openFile('./cloud-sdk/docs-js/release-notes.mdx');
   let releaseNotesArray = releaseNotes.split(
-    `<!-- This line is used for our release notes automation -->${unixEOL}`
+      `<!-- This line is used for our release notes automation -->${unixEOL}`
   );
   const newContent = changelog + releaseNotesArray[1];
   releaseNotesArray[1] = newContent;
   const newReleaseNotes = releaseNotesArray.join(
-    `<!-- This line is used for our release notes automation -->${unixEOL}`
+      `<!-- This line is used for our release notes automation -->${unixEOL}`
   );
   writeFileSync('./cloud-sdk/docs-js/release-notes.mdx', newReleaseNotes);
 }

6409a22b6a9c58d92913c9eb91a81a1523699bfd
Florian Wilhelm <florian.wilhelm02@sap.com>
2022-11-07 17:26:22 +0100

    unit tests

--- packages/generator/src/service-name-formatter.spec.ts
+++ packages/generator/src/service-name-formatter.spec.ts
@@ -280,5 +280,17 @@ describe('name-formatter', () => {
         ].includes('MY_PROPERTY')
       ).toBe(true);
     });
+
+    it('should generate bound parameter name', () => {
+      const formatter = getFreshNameFormatter();
+      formatter.originalToBoundParameterName('A_SomeEntity', 'MyFn', 'MyParam');
+      expect(formatter['parameterNameGenerators']['A_SomeEntity.MyFn']['usedNames'].pop()).toEqual('myParam');
+    });
+
+    it('should generate bound operation name', () => {
+      const formatter = getFreshNameFormatter();
+      formatter.originalToBoundOperationName('A_SomeEntity', 'MyFn');
+      expect(formatter['instancePropertyNameGenerators']['A_SomeEntity']['usedNames'].pop()).toEqual('myFn');
+    });
   });
 });

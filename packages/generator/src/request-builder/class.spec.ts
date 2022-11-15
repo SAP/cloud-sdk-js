0c19228eb709ad11e7012a67138d100ab716ff19
Florian Wilhelm <florian.wilhelm02@sap.com>
2022-11-15 16:26:51 +0100

    tests

--- packages/generator/src/request-builder/class.spec.ts
+++ packages/generator/src/request-builder/class.spec.ts
@@ -1,4 +1,3 @@
-import { unixEOL } from '@sap-cloud-sdk/util';
 import { breakfastEntity } from '../../test/test-util/data-model';
 import { VdmProperty } from '../vdm-types';
 import { requestBuilderClass } from './class';
@@ -14,7 +13,7 @@ describe('request builder class', () => {
     const getByKey = {
       name: 'getByKey',
       returnType: 'GetByKeyRequestBuilder<Breakfast<T>, T>',
-      statements: `return new GetByKeyRequestBuilder<Breakfast<T>, T>(this.entityApi, {EntityName: entityName,${unixEOL}            BreakfastTime: breakfastTime});`,
+      statements: ['return new GetByKeyRequestBuilder<Breakfast<T>, T>(this.entityApi, {EntityName: entityName,', 'BreakfastTime: breakfastTime});'],
       parameters: [
         { name: 'entityName', type: "DeserializedType<T, 'Edm.String'>" },
         { name: 'breakfastTime', type: "DeserializedType<T, 'Edm.DateTime'>" }
@@ -24,28 +23,29 @@ describe('request builder class', () => {
       name: 'getAll',
       returnType: 'GetAllRequestBuilder<Breakfast<T>, T>',
       statements:
-        'return new GetAllRequestBuilder<Breakfast<T>, T>(this.entityApi);',
+        ['return new GetAllRequestBuilder<Breakfast<T>, T>(this.entityApi);'],
       parameters: undefined
     };
     const create = {
       name: 'create',
       returnType: 'CreateRequestBuilder<Breakfast<T>, T>',
       statements:
-        'return new CreateRequestBuilder<Breakfast<T>, T>(this.entityApi, entity);',
+        ['return new CreateRequestBuilder<Breakfast<T>, T>(this.entityApi, entity);'],
       parameters: [{ name: 'entity', type: 'Breakfast<T>' }]
     };
     const update = {
       name: 'update',
       returnType: 'UpdateRequestBuilder<Breakfast<T>, T>',
       statements:
-        'return new UpdateRequestBuilder<Breakfast<T>, T>(this.entityApi, entity);',
+        ['return new UpdateRequestBuilder<Breakfast<T>, T>(this.entityApi, entity);'],
       parameters: [{ name: 'entity', type: 'Breakfast<T>' }]
     };
 
     [getByKey, getAll, create, update].forEach(method => {
       expect(requestBuilder).toContain(method.name);
       expect(requestBuilder).toContain(method.returnType);
-      expect(requestBuilder).toContain(method.statements);
+      method.statements.forEach(statement => expect(requestBuilder).toContain(statement));
+      method.parameters?.forEach(parameter => expect(requestBuilder).toContain(`${parameter.name}: ${parameter.type}`));
     });
   });
 
@@ -76,7 +76,7 @@ describe('request builder class', () => {
     const getByKeyRequestBuilder = {
       name: 'getByKey',
       returnType: 'GetByKeyRequestBuilder<Breakfast<T>, T>',
-      statements: `return new GetByKeyRequestBuilder<Breakfast<T>, T>(this.entityApi, {EntityName: entityName,${unixEOL}            BreakfastTime: breakfastTime,${unixEOL}            With: pWith});`,
+      statements: ['return new GetByKeyRequestBuilder<Breakfast<T>, T>(this.entityApi, {EntityName: entityName,', 'BreakfastTime: breakfastTime,', 'With: pWith});'],
       parameters: params
     };
 
@@ -88,14 +88,15 @@ describe('request builder class', () => {
     const deleteRequestBuilder = {
       name: 'delete',
       returnType: 'DeleteRequestBuilder<Breakfast<T>, T>',
-      statements: `return new DeleteRequestBuilder<Breakfast<T>, T>(this.entityApi, entityNameOrEntity instanceof Breakfast ? entityNameOrEntity : {EntityName: entityNameOrEntity!,${unixEOL}        BreakfastTime: breakfastTime!,${unixEOL}        With: pWith!});`,
+      statements: ['return new DeleteRequestBuilder<Breakfast<T>, T>(this.entityApi, entityNameOrEntity instanceof Breakfast ? entityNameOrEntity : {EntityName: entityNameOrEntity!,', 'BreakfastTime: breakfastTime!,', 'With: pWith!});'],
       parameters: deleteParams
     };
 
     [getByKeyRequestBuilder, deleteRequestBuilder].forEach(method => {
       expect(requestBuilder).toContain(method.name);
       expect(requestBuilder).toContain(method.returnType);
-      expect(requestBuilder).toContain(method.statements);
+      method.statements.forEach(statement => expect(requestBuilder).toContain(statement));
+      method.parameters.forEach(parameter => expect(requestBuilder).toContain(`${parameter.name}: ${parameter.type}`));
     });
   });
 });

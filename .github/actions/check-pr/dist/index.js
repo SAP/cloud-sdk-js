9a9e578a98227076f4659611b3b0686cc078b4f2
Marika Marszalkowski <marika.marszalkowski@sap.com>
2022-12-05 19:41:59 +0100

    i

--- .github/actions/check-pr/dist/index.js
+++ .github/actions/check-pr/dist/index.js
@@ -112,7 +112,7 @@ function validateChangelog(allowedBumps) {
 }
 function validateBody() {
     return __awaiter(this, void 0, void 0, function () {
-        var body, prTemplate;
+        var body, prTemplate, i;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
@@ -123,9 +123,16 @@ function validateBody() {
                     if (!body || body === prTemplate) {
                         (0, core_1.setFailed)('PR should have a description');
                     }
+                    i = 0;
+                    while (i < prTemplate.length) {
+                        if (prTemplate[i] !== body[i]) {
+                            break;
+                        }
+                    }
                     console.log(prTemplate.trim() === body.trim());
                     console.log(prTemplate.length);
                     console.log(body.length);
+                    console.log(i);
                     return [2 /*return*/];
             }
         });

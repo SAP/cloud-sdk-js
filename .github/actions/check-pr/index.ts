b3ea347b3dbeb160644dd08f2b5fd6b76915c2bc
Marika Marszalkowski <marika.marszalkowski@sap.com>
2022-12-05 16:45:32 +0100

    Try extra action

--- .github/actions/check-pr/index.ts
+++ .github/actions/check-pr/index.ts
@@ -1,7 +1,9 @@
-import { setOutput, setFailed } from '@actions/core';
+import { setOutput, getInput, setFailed } from '@actions/core';
 import { context } from '@actions/github';
 
-console.log(JSON.stringify(context.payload.pull_request, null, 2));
+const changedFiles = getInput('changed-files').split(' ');
+console.log(changedFiles);
+// console.log(JSON.stringify(context.payload.pull_request, null, 2));
 
 function checkChangeLogExists(title: string) {
   if (!title.startsWith('chore:')) {

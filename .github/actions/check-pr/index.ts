3f561a6e1df532f9622ddfb44e117cfbda588f1f
Marika Marszalkowski <marika.marszalkowski@sap.com>
2022-12-05 16:48:40 +0100

    where is the log?

--- .github/actions/check-pr/index.ts
+++ .github/actions/check-pr/index.ts
@@ -2,7 +2,7 @@ import { setOutput, getInput, setFailed } from '@actions/core';
 import { context } from '@actions/github';
 
 const changedFiles = getInput('changed-files').split(' ');
-console.log(changedFiles);
+console.log('here', changedFiles);
 // console.log(JSON.stringify(context.payload.pull_request, null, 2));
 
 function checkChangeLogExists(title: string) {

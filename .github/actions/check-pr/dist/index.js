d289b574f3b81c695dd63e7cc6da8a947eef54bb
Marika Marszalkowski <marika.marszalkowski@sap.com>
2022-12-05 16:49:40 +0100

    Rerun tsc

--- .github/actions/check-pr/dist/index.js
+++ .github/actions/check-pr/dist/index.js
@@ -1,8 +1,9 @@
 "use strict";
 exports.__esModule = true;
 var core_1 = require("@actions/core");
-var github_1 = require("@actions/github");
-console.log(JSON.stringify(github_1.context.payload.pull_request, null, 2));
+var changedFiles = (0, core_1.getInput)('changed-files').split(' ');
+console.log('here', changedFiles);
+// console.log(JSON.stringify(context.payload.pull_request, null, 2));
 function checkChangeLogExists(title) {
     if (!title.startsWith('chore:')) {
         (0, core_1.setFailed)('No chore:');

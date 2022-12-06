e557b3a27d24123c794162497abe25dfa7fe700e
Marika Marszalkowski <marika.marszalkowski@sap.com>
2022-12-06 15:04:17 +0100

    Refactor

--- .github/actions/check-pr/index.ts
+++ .github/actions/check-pr/index.ts
@@ -1,53 +1,70 @@
-import { setOutput, getInput, setFailed, info, notice } from '@actions/core';
+import { getInput, setFailed, info } from '@actions/core';
 import { context } from '@actions/github';
 import { readFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
 
-const validPreambles = ['feat', 'fix', 'chore'];
-interface ParsedTitle {
-  preamble?: string;
-  topic?: string;
-  isBreaking: boolean;
-  description?: string;
+const validCommitTypes = ['feat', 'fix', 'chore'];
+
+// Expected format: preamble(topic)!: Title text
+async function validateTitle(): Promise<void> {
+  const title = context.payload.pull_request.title;
+  if (!title.includes(':')) {
+    throw new Error(
+      `PR title does not adhere to conventional commit guidelines. No preamble found.`
+    );
+  }
+
+  const [preamble, postamble] = title.split(':');
+
+  await validatePreamble(preamble);
+  validatePostamble(postamble);
 }
 
-function parseTitle(title: string): ParsedTitle {
-  const groups = title.match(
-    /(?<preamble>\w+)(\((?<topic>\w+)\))?(?<isBreaking>!)?: (?<description>.*)/
-  ).groups;
+async function validatePreamble(preamble: string): Promise<void> {
+  const groups = preamble.match(
+    /(?<commitType>\w+)?(\((?<topic>\w+)\))?(?<isBreaking>!)?/
+  )?.groups;
+
   if (groups) {
-    return { ...groups, isBreaking: !!groups.isBreaking };
+    const { commitType, isBreaking } = groups;
+
+    validateCommitType(commitType);
+    validateChangelog(commitType, !!isBreaking);
   } else {
-    throw new Error('Could not parse PR title');
+    throw new Error(
+      'Could not parse preamble. Ensure it follows the conventional commit guidelines.'
+    );
   }
 }
 
-function validatePreamble(preamble: string | undefined): void {
-  if (!preamble || !validPreambles.includes(preamble)) {
-    setFailed(
-      `PR title does not adhere to conventional commit guidelines. Commit type found: ${preamble}. Should be one of ${validPreambles.join(
+function validateCommitType(commitType) {
+  if (!commitType || !validCommitTypes.includes(commitType)) {
+    throw new Error(
+      `PR title does not adhere to conventional commit guidelines. Commit type found: ${commitType}. Must be one of ${validCommitTypes.join(
         ', '
       )}`
     );
-  } else {
-    info('✓ Preamble: OK');
   }
+  info('✓ Commit type: OK');
 }
 
-function validateTitle(title: string | undefined): void {
-  if (!title) {
-    setFailed(
-      `PR title does not have a title (after conventional commit preamble).`
+function validatePostamble(title: string | undefined): void {
+  if (!title || !title.trim().length) {
+    throw new Error(
+      `PR title does not have a title after conventional commit preamble.`
     );
+  }
+
+  if (title[0] !== ' ') {
+    throw new Error(`Space missing after conventional commit preamble.`);
   }
 
   if (title[0] === title[0].toLowerCase()) {
-    setFailed(
+    throw new Error(
       `PR title title must be capitalized (after conventional commit preamble).`
     );
-  } else {
-    info('✓ Title: OK');
   }
+  info('✓ Title: OK');
 }
 
 function getAllowedBumps(preamble: string, isBreaking: boolean): string[] {
@@ -63,43 +80,45 @@ function getAllowedBumps(preamble: string, isBreaking: boolean): string[] {
   return [];
 }
 
-async function validateChangelog(
-  preamble: string,
-  isBreaking: boolean
-): Promise<void> {
-  let ok = true;
-  const allowedBumps = getAllowedBumps(preamble, isBreaking);
+async function hasMatchingChangelog(allowedBumps: string[]): Promise<boolean> {
   if (allowedBumps.length) {
     const changedFiles = getInput('changed-files').split(' ');
     const fileContents = await Promise.all(
       changedFiles.map(file => readFile(file, 'utf-8'))
     );
-    ok = fileContents.some(fileContent =>
+    return fileContents.some(fileContent =>
       allowedBumps.some(bump =>
         new RegExp(`'@sap-cloud-sdk\/\w+': ${bump}/`).test(fileContent)
       )
     );
   }
 
-  if (!ok) {
-    setFailed(
-      `Preamble '${preamble}' requires a changelog file with bump ${allowedBumps.join(
+  return true;
+}
+
+async function validateChangelog(
+  commitType: string,
+  isBreaking: boolean
+): Promise<void> {
+  const allowedBumps = getAllowedBumps(commitType, isBreaking);
+  if (!(await hasMatchingChangelog(allowedBumps))) {
+    throw new Error(
+      `Preamble '${commitType}' requires a changelog file with bump ${allowedBumps.join(
         ' or '
       )}.`
     );
-  } else {
-    info('✓ Changelog: OK');
   }
+  info('✓ Changelog: OK');
 }
 
 async function validateBody() {
   const body = context.payload.pull_request.body.replace(/\r\n/g, '\n');
-  const prTemplate = await readFile(
+  const template = await readFile(
     resolve('.github', 'PULL_REQUEST_TEMPLATE.md'),
     'utf-8'
   );
 
-  if (!body || body === prTemplate) {
+  if (!body || body === template) {
     setFailed('PR must have a description');
   } else {
     info('✓ Body: OK');
@@ -107,13 +126,7 @@ async function validateBody() {
 }
 
 try {
-  const { preamble, isBreaking, description } = parseTitle(
-    context.payload.pull_request.title
-  );
-
-  validatePreamble(preamble);
-  validateTitle(description);
-  validateChangelog(preamble, isBreaking);
+  validateTitle();
   validateBody();
 } catch (err) {
   setFailed(err);

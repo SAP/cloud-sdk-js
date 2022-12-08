8e3f37aab18b8cbe1f160932601075d4eb7fe021
Marika Marszalkowski <868536+marikaner@users.noreply.github.com>
2022-12-08 11:14:49 +0100

    chore: Add PR checks (#3157)
    * Add PR check
    
    * Trigger run
    
    * Add separate wf
    
    * Add PR config
    
    * remove dashes
    
    * Ups
    
    * fix
    
    * Use custom action
    
    * Fix action
    
    * Add action checkout
    
    * Use node16
    
    * Fix action for no root
    
    * -.-
    
    * Log payload
    
    * Try extra action
    
    * where is the log?
    
    * Rerun tsc
    
    * Remove unused step
    
    * .
    
    * test
    
    * .
    
    * gf
    
    * Implement custom checks
    
    * no json
    
    * Add test file
    
    * Leave error
    
    * Split by “ “
    
    * Add PR template test
    
    * go
    
    * Add body check
    
    * log body
    
    * trim
    
    * i
    
    * go
    
    * frew
    
    * ge
    
    * fd
    
    * jgofi
    
    * Remove wf name
    
    * better name
    
    * Update .github/actions/check-pr/index.ts
    
    Co-authored-by: Florian Wilhelm <florian.wilhelm02@sap.com>
    
    * Update .github/actions/check-pr/index.ts
    
    Co-authored-by: Florian Wilhelm <florian.wilhelm02@sap.com>
    
    * Refactor
    
    * Remove test files
    
    * transpile
    
    * Imrove checks
    
    * Check letter after space
    
    Co-authored-by: Florian Wilhelm <florian.wilhelm02@sap.com>

---
+++ .github/actions/check-pr/index.ts
@@ -0,0 +1,133 @@
+import { getInput, setFailed, info } from '@actions/core';
+import { context } from '@actions/github';
+import { readFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+
+const validCommitTypes = ['feat', 'fix', 'chore'];
+
+// Expected format: preamble(topic)!: Title text
+async function validateTitle(): Promise<void> {
+  const title = context.payload.pull_request.title;
+  if (!title.includes(':')) {
+    return setFailed(
+      `PR title does not adhere to conventional commit guidelines. No preamble found.`
+    );
+  }
+  const [preamble, postamble] = title.split(':');
+
+  await validatePreamble(preamble);
+  validatePostamble(postamble);
+}
+
+async function validatePreamble(preamble: string): Promise<void> {
+  const groups = preamble.match(
+    /(?<commitType>\w+)?(\((?<topic>\w+)\))?(?<isBreaking>!)?/
+  )?.groups;
+
+  if (!groups) {
+    return setFailed(
+      'Could not parse preamble. Ensure it follows the conventional commit guidelines.'
+    );
+  }
+
+  const { commitType, isBreaking } = groups;
+
+  validateCommitType(commitType);
+  validateChangelog(commitType, !!isBreaking);
+}
+
+function validateCommitType(commitType) {
+  if (!commitType || !validCommitTypes.includes(commitType)) {
+    return setFailed(
+      `PR title does not adhere to conventional commit guidelines. Commit type found: ${commitType}. Must be one of ${validCommitTypes.join(
+        ', '
+      )}`
+    );
+  }
+  info('✓ Commit type: OK');
+}
+
+function validatePostamble(title: string | undefined): void {
+  if (!title || !title.trim().length) {
+    return setFailed(
+      `PR title does not have a title after conventional commit preamble.`
+    );
+  }
+
+  if (title[0] !== ' ') {
+    return setFailed(`Space missing after conventional commit preamble.`);
+  }
+
+  if (title[1] === title[1].toLowerCase()) {
+    return setFailed(
+      `PR title title must be capitalized (after conventional commit preamble).`
+    );
+  }
+
+  info('✓ Title: OK');
+}
+
+function getAllowedBumps(preamble: string, isBreaking: boolean): string[] {
+  if (isBreaking) {
+    return ['major'];
+  }
+  if (preamble === 'feat') {
+    return ['minor'];
+  }
+  if (preamble === 'fix') {
+    return ['minor', 'patch'];
+  }
+  return [];
+}
+
+async function hasMatchingChangelog(allowedBumps: string[]): Promise<boolean> {
+  if (allowedBumps.length) {
+    const changedFiles = getInput('changed-files').split(' ');
+    const fileContents = await Promise.all(
+      changedFiles.map(file => readFile(file, 'utf-8'))
+    );
+    return fileContents.some(fileContent =>
+      allowedBumps.some(bump =>
+        new RegExp(`'@sap-cloud-sdk\/\w+': ${bump}/`).test(fileContent)
+      )
+    );
+  }
+
+  return true;
+}
+
+async function validateChangelog(
+  commitType: string,
+  isBreaking: boolean
+): Promise<void> {
+  const allowedBumps = getAllowedBumps(commitType, isBreaking);
+  if (!(await hasMatchingChangelog(allowedBumps))) {
+    return setFailed(
+      `Preamble '${commitType}' requires a changelog file with bump ${allowedBumps.join(
+        ' or '
+      )}.`
+    );
+  }
+
+  info('✓ Changelog: OK');
+}
+
+async function validateBody() {
+  const body = context.payload.pull_request.body.replace(/\r\n/g, '\n');
+  const template = await readFile(
+    resolve('.github', 'PULL_REQUEST_TEMPLATE.md'),
+    'utf-8'
+  );
+
+  if (!body || body === template) {
+    return setFailed('PR must have a description');
+  }
+  info('✓ Body: OK');
+}
+
+try {
+  validateTitle();
+  validateBody();
+} catch (err) {
+  setFailed(err);
+}

9140f3071f13a2f086f7b9bb5aeac25bb789af92
d068544 <frank.essenberger@sap.com>
2023-01-13 13:40:59 +0100

    disable integration tests

--- test-packages/integration-tests/test/auth-flows/auth-flow.spec.ts
+++ test-packages/integration-tests/test/auth-flows/auth-flow.spec.ts
@@ -159,7 +159,7 @@ describe('OAuth flows', () => {
     expect(result.length).toBe(1);
   }, 60000);
 
-  it('Basic Auth: iss as token ', async () => {
+  xit('Basic Auth: iss as token ', async () => {
     const destination = await getDestination({
       destinationName: systems.s4.providerBasic,
       iss: 'http://s4sdk.localhost:8080/uaa/oauth/token'
@@ -167,7 +167,7 @@ describe('OAuth flows', () => {
     expect(destination?.password).toBeDefined();
   }, 60000);
 
-  it('Basic Auth OP: iss as token provider', async () => {
+  xit('Basic Auth OP: iss as token provider', async () => {
     const destination = await getDestination({
       destinationName: systems.s4onPrem.providerBasic,
       iss: 'http://s4sdk.localhost:8080/uaa/oauth/token'
@@ -178,7 +178,7 @@ describe('OAuth flows', () => {
     ).toBeDefined();
   }, 60000);
 
-  it('Basic Auth OP: iss as token subscriber', async () => {
+  xit('Basic Auth OP: iss as token subscriber', async () => {
     const destination = await getDestination({
       destinationName: systems.s4onPrem.subscriberBasic,
       iss: 'http://s4sdk-cf-subscriber.localhost:8080/uaa/oauth/token'

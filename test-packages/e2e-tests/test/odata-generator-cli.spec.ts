a0225fd97340ebadf973a7f5e834b2b3c17ba1de
d068544 <frank.essenberger@sap.com>
2023-01-30 16:03:18 +0100

    fix generator test

--- test-packages/e2e-tests/test/odata-generator-cli.spec.ts
+++ test-packages/e2e-tests/test/odata-generator-cli.spec.ts
@@ -44,7 +44,7 @@ describe('OData generator CLI', () => {
   it('should fail if mandatory parameters are not there', async () => {
     await expect(() =>
       execa.command(`npx ts-node ${pathToGenerator}`)
-    ).rejects.toThrowError(/Missing required arguments: inputDir, outputDir/);
+    ).rejects.toThrowError(/Missing required arguments: outputDir, inputDir/);
   }, 60000);
 
   it('should read config', async () => {

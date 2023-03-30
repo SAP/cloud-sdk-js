685e30714d44b6003742845e8edcf09a685d8a4e
Florian Wilhelm <florian.wilhelm02@sap.com>
2023-03-30 15:21:42 +0200

    threshold

--- scripts/compare-memory-usage.ts
+++ scripts/compare-memory-usage.ts
@@ -1,17 +1,17 @@
-const threshold = 90;
+const threshold = 105;
 
 const main = () => {
-  const v1MemoryUsage = parseInt(process.argv[2]);
+  const v2MemoryUsage = parseInt(process.argv[2]);
   const canaryMemoryUsage = parseInt(process.argv[3]);
-  const percentageBasedOnV1 = (canaryMemoryUsage / v1MemoryUsage) * 100;
-  if (percentageBasedOnV1 > threshold) {
+  const percentageBasedOnv2 = (canaryMemoryUsage / v2MemoryUsage) * 100;
+  if (percentageBasedOnv2 > threshold) {
     throw new Error(
-      `Test failed: v1 memory usage is ${v1MemoryUsage} and canary memory usage is ${canaryMemoryUsage} (${percentageBasedOnV1} %). Consider changing the threshold (${threshold} %) or fix it.`
+      `Test failed: v2 memory usage is ${v2MemoryUsage} and canary memory usage is ${canaryMemoryUsage} (${percentageBasedOnv2} %). Consider changing the threshold (${threshold} %) or fix it.`
     );
   }
   /* eslint-disable-next-line no-console */
   console.log(
-    `Test passed: v1 memory usage is ${v1MemoryUsage} and canary memory usage is ${canaryMemoryUsage} (${percentageBasedOnV1} %).`
+    `Test passed: v2 memory usage is ${v2MemoryUsage} and canary memory usage is ${canaryMemoryUsage} (${percentageBasedOnv2} %).`
   );
 };
 

afd09854c5305df73284f4f0bfd0dcf5417788b4
Marika Marszalkowski <marika.marszalkowski@sap.com>
2023-06-09 15:11:41 +0200

    Only import needed requestbuilders

--- packages/generator/src/generator-without-ts-morph/request-builder/imports.ts
+++ packages/generator/src/generator-without-ts-morph/request-builder/imports.ts
@@ -31,20 +31,23 @@ function requestBuilderImports(entity: VdmEntity) {
   const imports = [
     'DefaultDeSerializers',
     'DeSerializers',
-    'GetAllRequestBuilder',
-    'GetByKeyRequestBuilder'
+    'GetAllRequestBuilder'
   ];
 
   if (entity.creatable) {
     imports.push('CreateRequestBuilder');
   }
 
-  if (entity.updatable) {
-    imports.push('UpdateRequestBuilder');
-  }
+  if (entity.keys.length) {
+    imports.push('GetByKeyRequestBuilder');
 
-  if (entity.deletable) {
-    imports.push('DeleteRequestBuilder');
+    if (entity.updatable) {
+      imports.push('UpdateRequestBuilder');
+    }
+
+    if (entity.deletable) {
+      imports.push('DeleteRequestBuilder');
+    }
   }
 
   return imports;

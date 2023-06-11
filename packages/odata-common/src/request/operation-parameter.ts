5b502ee7e44abc2a0f16c5c59c7afb7ca5c8a6e8
Yuan Zhuang <yuan.zhuang@sap.com>
2023-06-11 19:13:33 +0200

    Update specs and create operation request builder

---
+++ packages/odata-common/src/request/operation-parameter.ts
@@ -0,0 +1,27 @@
+import { EdmTypeShared } from '../edm-types';
+
+/**
+ * Internal representation of operation parameters. It adds metadata to the value.
+ * @typeParam ValueT - Type of the value.
+ */
+export class OperationParameter<ValueT> {
+  /**
+   * Creates an instance of OperationParameter.
+   * @param originalName - The original name of the parameter in the OData service.
+   * @param edmType - Original EDM type.
+   * @param value - Value to be used as parameter.
+   */
+  constructor(
+    public originalName: string,
+    public edmType: EdmTypeShared<'any'>,
+    public value: ValueT
+  ) {}
+}
+
+/**
+ * Internal representation of all parameters of an operation as a map.
+ * @typeParam ParametersT - External representation of all parameters
+ */
+export type OperationParameters<ParametersT> = {
+  [K in keyof ParametersT]: OperationParameter<ParametersT[K]>;
+};
---
+++ packages/odata-v4/src/request-builder/operation-request-builder.ts
@@ -0,0 +1,61 @@
+import {
+  ActionFunctionImportRequestBuilderBase,
+  ODataRequestConfig,
+  OperationParameters
+} from '@sap-cloud-sdk/odata-common/internal';
+import { DeSerializers } from '../de-serializers/index';
+import { ODataFunctionImportRequestConfig, ODataActionImportRequestConfig } from '../request/index';
+import { createODataUri } from '../uri-conversion/index';
+
+/**
+ * Create OData request to execute an operation.
+ * @typeParam ParametersT - Type of the operation parameters.
+ * @typeParam ReturnT - Type of the operation return value.
+ */
+export class OperationRequestBuilder<
+  DeSerializersT extends DeSerializers,
+  ParametersT,
+  ReturnT
+> extends ActionFunctionImportRequestBuilderBase<
+  DeSerializersT,
+  ReturnT,
+  ODataRequestConfig
+> {
+  /**
+   * Creates an instance of OperationRequestBuilder.
+   * @param defaultBasePath - Default base path for the service the operation belongs to.
+   * @param operationName - The name of the operation.
+   * @param responseTransformer - Transformation function for the response.
+   * @param parameters - Parameters to be set in the operation.
+   * @param deSerializers - (De-)serializers used for transformation.
+   * @param type - Type of operation, whether it is a function or an action.
+   */
+  constructor(
+    defaultBasePath: string,
+    operationName: string,
+    readonly responseTransformer: (data: any) => ReturnT,
+    parameters: OperationParameters<ParametersT>,
+    deSerializers: DeSerializersT,
+    type: 'function' | 'action'
+  ) {
+   const requestConfig = type === 'function' ?
+    new ODataFunctionImportRequestConfig(
+      'get',
+      defaultBasePath,
+      operationName,
+      parameters,
+      createODataUri(deSerializers)
+    ) :
+    new ODataActionImportRequestConfig(
+      defaultBasePath,
+      operationName,
+      parameters,
+      createODataUri(deSerializers)
+    );
+    super(
+      responseTransformer,
+      requestConfig,
+      deSerializers
+    );
+  }
+}

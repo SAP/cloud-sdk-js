9b82d156425983a8974c4d05a44dcd7fe0a9ec0e
cloud-sdk-js <cloud-sdk-js@github.com>
2024-08-26 13:51:24 +0000

    Changes from lint:fix

--- .github/actions/check-pr/index.js
+++ .github/actions/check-pr/index.js
@@ -92,7 +92,7 @@ async function validateChangesets(preamble, commitType, isBreaking, fileContents
         const matches = content.match(/\[([^\]]+)\]/g);
         return matches ? matches.map(match => match.slice(1, -1)) : [];
     });
-    if (changeTypes.length === 0) {
+    if (preamble !== 'chore' && !changeTypes.length) {
         return (0, core_1.setFailed)('Missing change type in changeset.');
     }
     const allChangeTypesMatch = changeTypes.every(type => allowedChangeTypes.includes(type));

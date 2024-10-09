d0ae1bc4168a743d284d1d76ec71c641201e575a
Shibesh Duwadi <shibesh.duwadi@sap.com>
2024-10-09 10:00:11 +0200

    init

---
+++ build-packages/check-license/index.ts
@@ -0,0 +1,76 @@
+import { resolve } from 'path';
+import { ModuleInfos, init } from 'license-checker';
+import { createLogger } from '@sap-cloud-sdk/util';
+
+const logger = createLogger('check-licenses');
+
+// Here all permissive FLOSS licenses are ok, see https://en.wikipedia.org/wiki/Permissive_software_license
+// We just added the most common ones here. If one is in the wiki list and not here add it.
+const allowedLicenses = [
+  'MIT',
+  'Apache',
+  'ISC',
+  'BSD',
+  'WTFPL',
+  'CC-BY',
+  'CC0',
+  'Unlicense',
+  'Public Domain'
+];
+
+async function getLicenses(): Promise<ModuleInfos> {
+  return new Promise((resolvePromise, reject) => {
+    init(
+      {
+        start: resolve(__dirname, '..'),
+        direct: true,
+        summary: true,
+        json: true,
+        production: true
+      },
+      function (err, packages: ModuleInfos) {
+        if (err) {
+          reject(`Could not check licenses. Error: ${err}`);
+        }
+        resolvePromise(packages);
+      }
+    );
+  });
+}
+
+function isAllowedLicense(licenses) {
+  return Array.isArray(licenses)
+    ? isAllowedLicense(licenses.join(','))
+    : allowedLicenses.some(allowedLicense => licenses.includes(allowedLicense));
+}
+
+function isSapDependency(dependency) {
+  // Exclude root package from license check
+  if (dependency.startsWith('sap-cloud-sdk')) {
+    return true;
+  }
+  const [scope] = dependency.split('/');
+  return scope === '@sap' || scope === '@sap-cloud-sdk';
+}
+
+async function checkLicenses() {
+  const licenses = await getLicenses();
+  const notAllowedDependencies = Object.entries(licenses)
+    .filter(([, licenseInfo]) => !isAllowedLicense(licenseInfo.licenses))
+    .filter(([packageName]) => !isSapDependency(packageName));
+
+  if (notAllowedDependencies.length) {
+    notAllowedDependencies.forEach(notAllowedDependency => {
+      logger.error(
+        `Not allowed license ${notAllowedDependency[1].licenses} found for dependency: ${notAllowedDependency[0]}.`
+      );
+    });
+    logger.error(
+      'Check if the faulty licenses are in the FLOSS list: https://en.wikipedia.org/wiki/Category:Free_and_open-source_software_licenses and update the check script accordingly.'
+    );
+
+    process.exit(1);
+  }
+}
+
+checkLicenses();

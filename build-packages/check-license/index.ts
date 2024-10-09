a5198fbc187cb9de3fc96a13f4bd95368200a0bc
Shibesh Duwadi <shibesh.duwadi@sap.com>
2024-10-09 12:35:56 +0200

    using actions/core

--- build-packages/check-license/index.ts
+++ build-packages/check-license/index.ts
@@ -1,8 +1,5 @@
-import { resolve } from 'path';
+import { info, setFailed } from '@actions/core';
 import { ModuleInfos, init } from 'license-checker';
-import { createLogger } from '@sap-cloud-sdk/util';
-
-const logger = createLogger('check-licenses');
 
 // Here all permissive FLOSS licenses are ok, see https://en.wikipedia.org/wiki/Permissive_software_license
 // We just added the most common ones here. If one is in the wiki list and not here add it.
@@ -22,7 +19,7 @@ async function getLicenses(): Promise<ModuleInfos> {
   return new Promise((resolvePromise, reject) => {
     init(
       {
-        start: resolve(__dirname, '..'),
+        start: process.cwd(),
         direct: true,
         summary: true,
         json: true,
@@ -30,7 +27,7 @@ async function getLicenses(): Promise<ModuleInfos> {
       },
       function (err, packages: ModuleInfos) {
         if (err) {
-          reject(`Could not check licenses. Error: ${err}`);
+          reject(`Error: ${err}`);
         }
         resolvePromise(packages);
       }
@@ -54,23 +51,28 @@ function isSapDependency(dependency) {
 }
 
 async function checkLicenses() {
-  const licenses = await getLicenses();
-  const notAllowedDependencies = Object.entries(licenses)
-    .filter(([, licenseInfo]) => !isAllowedLicense(licenseInfo.licenses))
-    .filter(([packageName]) => !isSapDependency(packageName));
+    try {
+        const licenses = await getLicenses();
+        const notAllowedDependencies = Object.entries(licenses)
+            .filter(([, licenseInfo]) => !isAllowedLicense(licenseInfo.licenses))
+            .filter(([packageName]) => !isSapDependency(packageName));
 
-  if (notAllowedDependencies.length) {
-    notAllowedDependencies.forEach(notAllowedDependency => {
-      logger.error(
-        `Not allowed license ${notAllowedDependency[1].licenses} found for dependency: ${notAllowedDependency[0]}.`
-      );
-    });
-    logger.error(
-      'Check if the faulty licenses are in the FLOSS list: https://en.wikipedia.org/wiki/Category:Free_and_open-source_software_licenses and update the check script accordingly.'
-    );
-
-    process.exit(1);
-  }
+        if (notAllowedDependencies.length) {
+            notAllowedDependencies.forEach(notAllowedDependency => {
+                setFailed(
+                    `Not allowed license ${notAllowedDependency[1].licenses} found for dependency: ${notAllowedDependency[0]}.`
+                );
+            });
+            setFailed(
+            'Check if the faulty licenses are in the FLOSS list: https://en.wikipedia.org/wiki/Category:Free_and_open-source_software_licenses and update the check script accordingly.'
+            );
+            process.exit(1);
+        }
+        info('License check complete.');
+    } catch (error) {
+        setFailed(`Could not check licenses. ${error}`);
+      }
+  
 }
 
 checkLicenses();

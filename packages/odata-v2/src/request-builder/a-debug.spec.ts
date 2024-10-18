64d3f0d9bfcc7a0717ef6bcfc5d2222ff06a9a08
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-10-18 13:26:37 +0200

    remove getall from batch

--- packages/odata-v2/src/request-builder/a-debug.spec.ts
+++ packages/odata-v2/src/request-builder/a-debug.spec.ts
@@ -10,47 +10,9 @@ import {
   defaultHost
 } from '../../../../test-resources/test/test-util';
 import { CreateRequestBuilder } from './create-request-builder';
-const responseBoundary = 'responseBoundary';
 
 describe('debug', () => {
-  const { batch, testEntityApi } = testService();
-
-  const getHeader = contentType => `Content-Type: ${contentType}
-Content-Length: 3886
-content-transfer-encoding: binary
-
-HTTP/1.1 200 OK
-Content-Type: application/json; charset=utf-8
-Content-Length: 3679
-sap-metadata-last-modified: Wed, 22 Dec 2021 22:29:24 GMT
-cache-control: no-store, no-cache
-dataserviceversion: 2.0`;
-
-  const getAllResponse = `--${responseBoundary}
-${getHeader('application/http')}
-
-{"d":{"results":[{"StringProperty":"4711"}]}}
---${responseBoundary}--
-`;
-
-  const baseUrl = 'https://some.sdk.test.url.com';
-
-  it('executes a getAll request', async () => {
-    nock(baseUrl)
-      .post('/sap/opu/odata/sap/API_TEST_SRV/$batch')
-      .reply(202, getAllResponse, {
-        'content-type': `multipart/mixed; boundary=${responseBoundary}`
-      });
-
-    const response = await batch(
-      testEntityApi.requestBuilder().getAll()
-    ).execute({ url: baseUrl });
-    expect(response[0].isReadResponse()).toBeTruthy();
-    if (response[0].isReadResponse()) {
-      const casted = response[0].as(testEntityApi);
-      expect(casted[0].stringProperty).toEqual('4711');
-    }
-  });
+  const { testEntityApi } = testService();
 
   it('create', async () => {
     const requestConfig = new ODataCreateRequestConfig(

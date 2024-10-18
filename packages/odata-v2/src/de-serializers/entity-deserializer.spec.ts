d60e8378967199e52706d8b1a0050e21e0b1607f
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-10-18 13:40:17 +0200

    Revert "fsd"
    This reverts commit c83c9e76aefb82ab4f037369e6faae0ec3ee62bf.

--- packages/odata-v2/src/de-serializers/entity-deserializer.spec.ts
+++ packages/odata-v2/src/de-serializers/entity-deserializer.spec.ts
@@ -1,4 +1,9 @@
-import { testEntityApi, testEntityMultiLinkApi } from '../../test/test-util';
+import { extractCustomFields } from '@sap-cloud-sdk/odata-common/internal';
+import {
+  testEntityApi,
+  testEntityMultiLinkApi,
+  testEntitySingleLinkApi
+} from '../../test/test-util';
 import { defaultDeSerializers } from './default-de-serializers';
 import { entityDeserializer } from './entity-deserializer';
 
@@ -33,5 +38,121 @@ describe('with default (de-)serializers', () => {
       }
     };
     expect(deserializeEntity(response, testEntityApi)).toEqual(testEntity);
+  });
+
+  it('should build an entity with multi link from response without results object (C4C)', () => {
+    const prop = 'test';
+    const multiLinkEntity = testEntityMultiLinkApi
+      .entityBuilder()
+      .stringProperty(prop)
+      .build();
+    const testEntity = testEntityApi
+      .entityBuilder()
+      .toMultiLink([multiLinkEntity])
+      .build();
+
+    const response = {
+      to_MultiLink: [{ StringProperty: prop }]
+    };
+    expect(deserializeEntity(response, testEntityApi)).toEqual(testEntity);
+  });
+
+  it('should build an entity with one to one link', () => {
+    const prop = 'test';
+
+    const singleLinkEntity = testEntitySingleLinkApi
+      .entityBuilder()
+      .stringProperty(prop)
+      .build();
+
+    const testEntity = testEntityApi
+      .entityBuilder()
+      .toSingleLink(singleLinkEntity)
+      .build();
+
+    const response = {
+      to_SingleLink: {
+        StringProperty: prop
+      }
+    };
+
+    expect(deserializeEntity(response, testEntityApi)).toEqual(testEntity);
+  });
+
+  it('should build an entity with custom fields', () => {
+    const withCustomFields = {
+      KeyPropertyGuid: '123456789',
+      to_SingleLink: { StringProperty: 'testSingle' },
+      to_MultiLink: [{ StringProperty: 'testMulti' }],
+      FirstCustomField: 'ACustomField',
+      SecondCustomField: 12345
+    };
+
+    const expected = {
+      FirstCustomField: 'ACustomField',
+      SecondCustomField: 12345
+    };
+
+    expect(extractCustomFields(withCustomFields, testEntityApi.schema)).toEqual(
+      expected
+    );
+  });
+
+  describe('entity with complex type field', () => {
+    const stringProperty = 'prop';
+    const int16Property = 16;
+    const booleanProperty = false;
+
+    const expected = testEntityApi
+      .entityBuilder()
+      .complexTypeProperty({
+        stringProperty,
+        int16Property,
+        booleanProperty,
+        complexTypeProperty: {
+          stringProperty
+        }
+      })
+      .stringProperty('test')
+      .build();
+
+    it('should deserialize', () => {
+      const actual = deserializeEntity(
+        {
+          ComplexTypeProperty: {
+            StringProperty: stringProperty,
+            Int16Property: int16Property,
+            BooleanProperty: booleanProperty,
+            ComplexTypeProperty: {
+              StringProperty: stringProperty
+            }
+          },
+          StringProperty: expected.stringProperty
+        },
+        testEntityApi
+      );
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('should deserialize with unknown keys', () => {
+      const actual = deserializeEntity(
+        {
+          ComplexTypeProperty: {
+            StringProperty: stringProperty,
+            Int16Property: int16Property,
+            BooleanProperty: booleanProperty,
+            ComplexTypeProperty: {
+              StringProperty: stringProperty
+            },
+            UnknownKey: ''
+          },
+          StringProperty: expected.stringProperty
+        },
+        testEntityApi
+      );
+
+      expect(actual).toEqual(expected);
+    });
   });
 });

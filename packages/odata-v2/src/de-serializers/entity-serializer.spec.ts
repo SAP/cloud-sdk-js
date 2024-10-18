9f60560076484e6f32bcf406bdf429f57679f8a8
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-10-18 17:18:14 +0200

    gf

--- packages/odata-v2/src/de-serializers/entity-serializer.spec.ts
+++ packages/odata-v2/src/de-serializers/entity-serializer.spec.ts
@@ -8,18 +8,6 @@ import { testEntityApi } from '../../test/test-util';
 describe('with default (de-)serializers', () => {
   const { serializeEntity } = entitySerializer(defaultDeSerializers);
   const tsToEdm = createValueDeserializer(defaultDeSerializers);
-  it('should serialize simple entity', () => {
-    const testEntity = testEntityApi
-      .entityBuilder()
-      .stringProperty('test')
-      .int16Property(100)
-      .build();
-
-    expect(serializeEntity(testEntity, testEntityApi)).toEqual({
-      StringProperty: testEntity.stringProperty,
-      Int16Property: testEntity.int16Property
-    });
-  });
 
   it('should serialize entity with complex type fields', () => {
     const stringProperty1 = 'test';

c8725bfa19278477739e94d8b53083bf7fca7ef0
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-10-18 17:11:45 +0200

    Revert "fgs"
    This reverts commit 707913b3e69c57bc6868c4db9ca2ae67df4e72bb.

--- packages/odata-v2/src/de-serializers/entity-serializer.spec.ts
+++ packages/odata-v2/src/de-serializers/entity-serializer.spec.ts
@@ -1,4 +1,5 @@
 import {
+  createValueDeserializer,
   defaultDeSerializers,
   entitySerializer
 } from '@sap-cloud-sdk/odata-common/internal';
@@ -6,6 +7,7 @@ import { testEntityApi } from '../../test/test-util';
 
 describe('with default (de-)serializers', () => {
   const { serializeEntity } = entitySerializer(defaultDeSerializers);
+  const tsToEdm = createValueDeserializer(defaultDeSerializers);
   it('should serialize simple entity', () => {
     const testEntity = testEntityApi
       .entityBuilder()
@@ -16,6 +18,50 @@ describe('with default (de-)serializers', () => {
     expect(serializeEntity(testEntity, testEntityApi)).toEqual({
       StringProperty: testEntity.stringProperty,
       Int16Property: testEntity.int16Property
+    });
+  });
+
+  it('should serialize entity with complex type fields', () => {
+    const stringProperty1 = 'test';
+    const stringProperty2 = 'nest';
+    const testEntity = testEntityApi
+      .entityBuilder()
+      .complexTypeProperty({
+        stringProperty: stringProperty1,
+        complexTypeProperty: {
+          stringProperty: stringProperty2
+        }
+      })
+      .int16Property(100)
+      .build();
+
+    expect(serializeEntity(testEntity, testEntityApi)).toEqual({
+      ComplexTypeProperty: {
+        StringProperty: stringProperty1,
+        ComplexTypeProperty: {
+          StringProperty: stringProperty2
+        }
+      },
+      Int16Property: testEntity.int16Property
+    });
+  });
+
+  it('should serialize entity with custom fields', () => {
+    const testEntity = testEntityApi
+      .entityBuilder()
+      .stringProperty('Peter')
+      .singleProperty(14.5)
+      .withCustomFields({
+        CustomField1: 'abcd',
+        CustomField2: 1234
+      })
+      .build();
+
+    expect(serializeEntity(testEntity, testEntityApi)).toEqual({
+      StringProperty: testEntity.stringProperty,
+      SingleProperty: tsToEdm(testEntity.singleProperty, 'Edm.Single'),
+      CustomField1: 'abcd',
+      CustomField2: 1234
     });
   });
 });

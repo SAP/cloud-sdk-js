6f0d8bc8e20cfa211fdf390e99e717e4b00aee88
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-10-18 13:40:33 +0200

    gdf

--- packages/odata-v2/src/de-serializers/entity-deserializer.spec.ts
+++ packages/odata-v2/src/de-serializers/entity-deserializer.spec.ts
@@ -9,36 +9,6 @@ import { entityDeserializer } from './entity-deserializer';
 
 describe('with default (de-)serializers', () => {
   const { deserializeEntity } = entityDeserializer(defaultDeSerializers);
-  it('should build an entity with properties', () => {
-    const prop = 'test';
-    const testEntity = testEntityApi
-      .entityBuilder()
-      .stringProperty(prop)
-      .build();
-
-    const response = { StringProperty: prop };
-
-    expect(deserializeEntity(response, testEntityApi)).toEqual(testEntity);
-  });
-
-  it('should build an entity with multi link from response with results object (S/4)', () => {
-    const prop = 'test';
-    const multiLinkEntity = testEntityMultiLinkApi
-      .entityBuilder()
-      .stringProperty(prop)
-      .build();
-    const testEntity = testEntityApi
-      .entityBuilder()
-      .toMultiLink([multiLinkEntity])
-      .build();
-
-    const response = {
-      to_MultiLink: {
-        results: [{ StringProperty: prop }]
-      }
-    };
-    expect(deserializeEntity(response, testEntityApi)).toEqual(testEntity);
-  });
 
   it('should build an entity with multi link from response without results object (C4C)', () => {
     const prop = 'test';

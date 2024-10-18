ca2f571a4c1617a6880cb5bb9e13e1824fa8b5c0
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-10-18 13:44:59 +0200

    gdf

--- packages/odata-v2/src/de-serializers/entity-deserializer.spec.ts
+++ packages/odata-v2/src/de-serializers/entity-deserializer.spec.ts
@@ -1,72 +1,9 @@
-import { extractCustomFields } from '@sap-cloud-sdk/odata-common/internal';
-import {
-  testEntityApi,
-  testEntityMultiLinkApi,
-  testEntitySingleLinkApi
-} from '../../test/test-util';
+import { testEntityApi } from '../../test/test-util';
 import { defaultDeSerializers } from './default-de-serializers';
 import { entityDeserializer } from './entity-deserializer';
 
 describe('with default (de-)serializers', () => {
   const { deserializeEntity } = entityDeserializer(defaultDeSerializers);
-
-  it('should build an entity with multi link from response without results object (C4C)', () => {
-    const prop = 'test';
-    const multiLinkEntity = testEntityMultiLinkApi
-      .entityBuilder()
-      .stringProperty(prop)
-      .build();
-    const testEntity = testEntityApi
-      .entityBuilder()
-      .toMultiLink([multiLinkEntity])
-      .build();
-
-    const response = {
-      to_MultiLink: [{ StringProperty: prop }]
-    };
-    expect(deserializeEntity(response, testEntityApi)).toEqual(testEntity);
-  });
-
-  it('should build an entity with one to one link', () => {
-    const prop = 'test';
-
-    const singleLinkEntity = testEntitySingleLinkApi
-      .entityBuilder()
-      .stringProperty(prop)
-      .build();
-
-    const testEntity = testEntityApi
-      .entityBuilder()
-      .toSingleLink(singleLinkEntity)
-      .build();
-
-    const response = {
-      to_SingleLink: {
-        StringProperty: prop
-      }
-    };
-
-    expect(deserializeEntity(response, testEntityApi)).toEqual(testEntity);
-  });
-
-  it('should build an entity with custom fields', () => {
-    const withCustomFields = {
-      KeyPropertyGuid: '123456789',
-      to_SingleLink: { StringProperty: 'testSingle' },
-      to_MultiLink: [{ StringProperty: 'testMulti' }],
-      FirstCustomField: 'ACustomField',
-      SecondCustomField: 12345
-    };
-
-    const expected = {
-      FirstCustomField: 'ACustomField',
-      SecondCustomField: 12345
-    };
-
-    expect(extractCustomFields(withCustomFields, testEntityApi.schema)).toEqual(
-      expected
-    );
-  });
 
   describe('entity with complex type field', () => {
     const stringProperty = 'prop';

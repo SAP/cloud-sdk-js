bf7c4d9fb1524d6f02280df53af9abea9b6e22df
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-10-18 17:11:39 +0200

    Revert "have 2 tests"
    This reverts commit 31e456c89b15a696136b4323639860caad196871.

--- packages/odata-v2/src/de-serializers/entity-serializer.spec.ts
+++ packages/odata-v2/src/de-serializers/entity-serializer.spec.ts
@@ -6,20 +6,7 @@ import { testEntityApi } from '../../test/test-util';
 
 describe('with default (de-)serializers', () => {
   const { serializeEntity } = entitySerializer(defaultDeSerializers);
-  it('should serialize simple entity 1', () => {
-    const testEntity = testEntityApi
-      .entityBuilder()
-      .stringProperty('test')
-      .int16Property(100)
-      .build();
-
-    expect(serializeEntity(testEntity, testEntityApi)).toEqual({
-      StringProperty: testEntity.stringProperty,
-      Int16Property: testEntity.int16Property
-    });
-  });
-
-  it('should serialize simple entity 2', () => {
+  it('should serialize simple entity', () => {
     const testEntity = testEntityApi
       .entityBuilder()
       .stringProperty('test')

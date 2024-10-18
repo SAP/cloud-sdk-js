e7743e3445cdfd22a9ca65faac4244c7da33230e
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-10-18 17:37:22 +0200

    gfds

--- packages/odata-v2/src/de-serializers/entity-serializer.spec.ts
+++ packages/odata-v2/src/de-serializers/entity-serializer.spec.ts
@@ -1,5 +1,4 @@
 import {
-  createValueDeserializer,
   defaultDeSerializers,
   entitySerializer
 } from '@sap-cloud-sdk/odata-common/internal';
@@ -7,10 +6,33 @@ import { testEntityApi } from '../../test/test-util';
 
 describe('with default (de-)serializers', () => {
   const { serializeEntity } = entitySerializer(defaultDeSerializers);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const tsToEdm = createValueDeserializer(defaultDeSerializers);
 
-  it('should serialize entity with complex type fields', () => {
+  it('should serialize entity with complex type fields 1', () => {
+    const stringProperty1 = 'test';
+    const stringProperty2 = 'nest';
+    const testEntity = testEntityApi
+      .entityBuilder()
+      .complexTypeProperty({
+        stringProperty: stringProperty1,
+        complexTypeProperty: {
+          stringProperty: stringProperty2
+        }
+      })
+      .int16Property(100)
+      .build();
+
+    expect(serializeEntity(testEntity, testEntityApi)).toEqual({
+      ComplexTypeProperty: {
+        StringProperty: stringProperty1,
+        ComplexTypeProperty: {
+          StringProperty: stringProperty2
+        }
+      },
+      Int16Property: testEntity.int16Property
+    });
+  });
+
+  it('should serialize entity with complex type fields 2', () => {
     const stringProperty1 = 'test';
     const stringProperty2 = 'nest';
     const testEntity = testEntityApi

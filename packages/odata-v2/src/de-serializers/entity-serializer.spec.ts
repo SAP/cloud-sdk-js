8df7869da47488803d4b51192290f2628ca67a08
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-10-18 14:15:41 +0200

    serialize

--- packages/odata-v2/src/de-serializers/entity-serializer.spec.ts
+++ packages/odata-v2/src/de-serializers/entity-serializer.spec.ts
@@ -3,13 +3,7 @@ import {
   defaultDeSerializers,
   entitySerializer
 } from '@sap-cloud-sdk/odata-common/internal';
-import {
-  testEntityApi,
-  testEntityLvl2MultiLinkApi,
-  testEntityLvl2SingleLinkApi,
-  testEntityMultiLinkApi,
-  testEntitySingleLinkApi
-} from '../../test/test-util';
+import { testEntityApi, testEntitySingleLinkApi } from '../../test/test-util';
 
 describe('with default (de-)serializers', () => {
   const { serializeEntity } = entitySerializer(defaultDeSerializers);
@@ -95,88 +89,6 @@ describe('with default (de-)serializers', () => {
       to_SingleLink: {
         StringProperty: singleLinkEntity.stringProperty
       }
-    });
-  });
-
-  it('should serialize multi linked entities with nested links', () => {
-    const lvl2MultiLinkEntity = testEntityLvl2MultiLinkApi
-      .entityBuilder()
-      .booleanProperty(false)
-      .build();
-
-    const lvl2SingleLinkEntity = testEntityLvl2SingleLinkApi
-      .entityBuilder()
-      .booleanProperty(true)
-      .build();
-
-    const multiLinkEntity = testEntityMultiLinkApi
-      .entityBuilder()
-      .stringProperty('true')
-      .toMultiLink([lvl2MultiLinkEntity])
-      .toSingleLink(lvl2SingleLinkEntity)
-      .build();
-
-    const testEntity = testEntityApi
-      .entityBuilder()
-      .stringProperty('root')
-      .int32Property(6474389)
-      .timeProperty({ hours: 12, minutes: 35, seconds: 54 })
-      .toMultiLink([multiLinkEntity])
-      .build();
-
-    expect(serializeEntity(testEntity, testEntityApi)).toEqual({
-      StringProperty: testEntity.stringProperty,
-      Int32Property: tsToEdm(testEntity.int32Property, 'Edm.Int32'),
-      TimeProperty: tsToEdm(testEntity.timeProperty, 'Edm.Time'),
-      to_MultiLink: [
-        {
-          StringProperty: multiLinkEntity.stringProperty,
-          to_MultiLink: [
-            {
-              BooleanProperty: lvl2MultiLinkEntity.booleanProperty
-            }
-          ],
-          to_SingleLink: {
-            BooleanProperty: lvl2SingleLinkEntity.booleanProperty
-          }
-        }
-      ]
-    });
-  });
-
-  it('should serialize linked entities with custom fields', () => {
-    const testEntity = testEntityApi
-      .entityBuilder()
-      .toSingleLink(
-        testEntitySingleLinkApi
-          .entityBuilder()
-          .withCustomFields({ custom: 'custom' })
-          .build()
-      )
-      .toMultiLink([
-        testEntityMultiLinkApi
-          .entityBuilder()
-          .withCustomFields({ custom: 'custom' })
-          .build()
-      ])
-      .build();
-
-    expect(serializeEntity(testEntity, testEntityApi)).toEqual({
-      to_SingleLink: { custom: 'custom' },
-      to_MultiLink: [{ custom: 'custom' }]
-    });
-  });
-
-  it('should serialize null value', () => {
-    const testEntity = testEntityApi
-      .entityBuilder()
-      .stringProperty(null)
-      .toSingleLink(null)
-      .build();
-
-    expect(serializeEntity(testEntity, testEntityApi)).toEqual({
-      StringProperty: null,
-      to_SingleLink: null
     });
   });
 });

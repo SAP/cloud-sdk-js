f7bbea63bbf764532f6e1accf49cc3e80cae1781
shibeshduw <shibesh.duwadi@sap.com>
2024-10-23 11:26:14 +0200

    v1

---
+++ build-packages/check-public-api/index.ts
@@ -0,0 +1,28 @@
+import yargs from 'yargs';
+import { setFailed } from '@actions/core';
+import { checkApiOfPackage } from './check-public-api.js';
+
+async function runPublicApiCheckScript() {
+  try {
+    const argv = yargs(process.argv.slice(2))
+      .option('strict', {
+        type: 'boolean',
+        default: false,
+        description: 'Enable strict mode'
+      })
+      .option('pathsToIgnore', {
+        type: 'string',
+        description: 'Comma-separated paths to ignore',
+        default: ''
+      }).argv;
+
+    const { strict, pathsToIgnore } = await argv;
+    const pathsToIgnoreOnCheck = pathsToIgnore ? pathsToIgnore.split(',') : [];
+
+    checkApiOfPackage(process.cwd(), strict, pathsToIgnoreOnCheck);
+  } catch (error) {
+    setFailed(error);
+  }
+}
+
+runPublicApiCheckScript();

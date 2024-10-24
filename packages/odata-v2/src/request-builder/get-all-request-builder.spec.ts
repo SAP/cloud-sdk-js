71cb3f936929b958cc2658a342c13f923b27efdf
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-10-24 16:17:51 +0200

    more manual

--- packages/odata-v2/src/request-builder/get-all-request-builder.spec.ts
+++ packages/odata-v2/src/request-builder/get-all-request-builder.spec.ts
@@ -1,11 +1,18 @@
 import {
+  ODataGetAllRequestConfig,
+  ODataRequest
+} from '@sap-cloud-sdk/odata-common';
+import nock from 'nock';
+import {
   defaultDestination,
-  mockGetRequest,
   unmockDestinationsEnv,
   createOriginalTestEntityData1,
-  createOriginalTestEntityData2
+  createOriginalTestEntityData2,
+  defaultHost,
+  defaultRequestHeaders
 } from '../../../../test-resources/test/test-util';
 import { testEntityApi } from '../../test/test-util';
+import { createODataUri } from '../uri-conversion';
 import { GetAllRequestBuilder } from './get-all-request-builder';
 import type { DefaultDeSerializers } from '../de-serializers';
 import type { TestEntity } from '@sap-cloud-sdk/test-services-odata-v2/test-service';
@@ -26,14 +33,40 @@ describe('GetAllRequestBuilder', () => {
       const entityData1 = createOriginalTestEntityData1();
       const entityData2 = createOriginalTestEntityData2();
       const rawResponse = { d: { results: [entityData1, entityData2] } };
+      // ///
 
-      mockGetRequest(
-        {
-          responseBody: rawResponse,
-          path: 'A_TestEntity'
-        },
-        testEntityApi
+      const requestConfig = new ODataGetAllRequestConfig(
+        testEntityApi,
+        createODataUri(testEntityApi.deSerializers)
       );
+      // mockRequest(requestConfig, {
+      //   responseBody: rawResponse,
+      //   path: 'A_TestEntity',
+      //   statusCode: 200,
+      //   method: 'get',
+      //   query: undefined
+      // });
+
+      // const request = new ODataRequest(requestConfig, defaultDestination);
+
+      // mockHeaderRequest({ request, path: 'A_TestEntity' });
+
+      nock('http://example.com')
+        .head('/sap/opu/odata/sap/API_TEST_SRV/A_TestEntity/')
+        .reply(200, undefined, mockedBuildHeaderResponse);
+
+      // console.log(
+      //   'buildNockUrl(request.relativeServiceUrl()/A_TestEntity, false)',
+      //   buildNockUrl(`${request.relativeServiceUrl()}/A_TestEntity`, false)
+      // );
+
+      nock('http://example.com', getRequestHeaders('get', undefined, undefined))
+        .get('/sap/opu/odata/sap/API_TEST_SRV/A_TestEntity', undefined)
+        .query({})
+        .delay(0)
+        .reply(200, rawResponse, undefined);
+
+      // ///
 
       const actual = await requestBuilder.executeRaw(defaultDestination);
       expect(actual.data).toEqual(rawResponse);
@@ -41,3 +74,91 @@ describe('GetAllRequestBuilder', () => {
     });
   });
 });
+
+export function buildNockUrl(
+  relativeServiceUrl: string,
+  endWithSlash = true
+): string {
+  if (relativeServiceUrl.startsWith('/')) {
+    return `${relativeServiceUrl}${
+      !relativeServiceUrl.endsWith('/') && endWithSlash ? '/' : ''
+    }`;
+  }
+
+  return `/${relativeServiceUrl}${
+    !relativeServiceUrl.endsWith('/') && endWithSlash ? '/' : ''
+  }`;
+}
+
+const defaultCsrfToken = 'mocked-x-csrf-token';
+
+const mockedBuildHeaderResponse = {
+  'x-csrf-token': defaultCsrfToken,
+  'set-cookie': ['mocked-cookie-0;mocked-cookie-1', 'mocked-cookie-2']
+};
+
+export function mockHeaderRequest({
+  request,
+  host = defaultHost,
+  responseHeaders = mockedBuildHeaderResponse,
+  path
+}: any) {
+  return nock(host)
+    .head(
+      path
+        ? buildNockUrl(`${request.relativeServiceUrl()}/${path}`)
+        : buildNockUrl(request.relativeServiceUrl())
+    )
+    .reply(200, undefined, responseHeaders);
+}
+
+export function mockRequest(
+  requestConfig,
+  {
+    host = defaultHost,
+    destination = defaultDestination,
+    path = '',
+    statusCode = 200,
+    delay = 0,
+    additionalHeaders,
+    method = 'get',
+    body,
+    query = {},
+    responseBody,
+    responseHeaders,
+    headers
+  }: any
+) {
+  const request = new ODataRequest(requestConfig, destination);
+
+  mockHeaderRequest({ request, path });
+
+  return nock(host, getRequestHeaders(method, additionalHeaders, headers))
+    [method](
+      path
+        ? buildNockUrl(`${request.relativeServiceUrl()}/${path}`, false)
+        : buildNockUrl(request.relativeServiceUrl()),
+      body
+    )
+    .query(query)
+    .delay(delay)
+    .reply(statusCode, responseBody, responseHeaders);
+}
+
+function getRequestHeaders(
+  method: string,
+  additionalHeaders?: Record<string, any>,
+  headers?: Record<string, any>
+) {
+  if (headers) {
+    return { reqheaders: headers };
+  }
+
+  if (additionalHeaders) {
+    const initialHeaders =
+      method === 'get'
+        ? defaultRequestHeaders
+        : { ...defaultRequestHeaders, 'x-csrf-token': defaultCsrfToken };
+    return { reqheaders: { ...initialHeaders, ...additionalHeaders } };
+  }
+}

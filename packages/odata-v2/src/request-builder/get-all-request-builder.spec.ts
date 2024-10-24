032e3ed116a570abdfd492beacf09bc2ffa6c3fd
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-10-24 16:27:02 +0200

    remove sdk

--- packages/odata-v2/src/request-builder/get-all-request-builder.spec.ts
+++ packages/odata-v2/src/request-builder/get-all-request-builder.spec.ts
@@ -1,16 +1,14 @@
 import nock from 'nock';
+import axios from 'axios';
 import {
-  defaultDestination,
   unmockDestinationsEnv,
   createOriginalTestEntityData1,
-  createOriginalTestEntityData2,
-  defaultRequestHeaders
+  createOriginalTestEntityData2
 } from '../../../../test-resources/test/test-util';
 import { testEntityApi } from '../../test/test-util';
 import { GetAllRequestBuilder } from './get-all-request-builder';
 import type { DefaultDeSerializers } from '../de-serializers';
 import type { TestEntity } from '@sap-cloud-sdk/test-services-odata-v2/test-service';
-
 describe('GetAllRequestBuilder', () => {
   let requestBuilder: GetAllRequestBuilder<TestEntity, DefaultDeSerializers>;
 
@@ -37,49 +35,13 @@ describe('GetAllRequestBuilder', () => {
 
       // ///
 
-      const actual = await requestBuilder.executeRaw(defaultDestination);
+      const actual = await axios.get(
+        'http://example.com/sap/opu/odata/sap/API_TEST_SRV/A_TestEntity'
+      );
+
+      // const actual = await requestBuilder.executeRaw(defaultDestination);
       expect(actual.data).toEqual(rawResponse);
       expect(actual.request.method).toBe('GET');
     });
   });
 });
-
-export function buildNockUrl(
-  relativeServiceUrl: string,
-  endWithSlash = true
-): string {
-  if (relativeServiceUrl.startsWith('/')) {
-    return `${relativeServiceUrl}${
-      !relativeServiceUrl.endsWith('/') && endWithSlash ? '/' : ''
-    }`;
-  }
-
-  return `/${relativeServiceUrl}${
-    !relativeServiceUrl.endsWith('/') && endWithSlash ? '/' : ''
-  }`;
-}
-
-const defaultCsrfToken = 'mocked-x-csrf-token';
-
-const mockedBuildHeaderResponse = {
-  'x-csrf-token': defaultCsrfToken,
-  'set-cookie': ['mocked-cookie-0;mocked-cookie-1', 'mocked-cookie-2']
-};
-
-function getRequestHeaders(
-  method: string,
-  additionalHeaders?: Record<string, any>,
-  headers?: Record<string, any>
-) {
-  if (headers) {
-    return { reqheaders: headers };
-  }
-
-  if (additionalHeaders) {
-    const initialHeaders =
-      method === 'get'
-        ? defaultRequestHeaders
-        : { ...defaultRequestHeaders, 'x-csrf-token': defaultCsrfToken };
-    return { reqheaders: { ...initialHeaders, ...additionalHeaders } };
-  }
-}

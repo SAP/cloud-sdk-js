e16856d76bc843b87a918f9fc3b871aefce6084e
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-10-24 16:36:18 +0200

    remove config

--- packages/odata-v2/src/request-builder/a-debug.spec.ts
+++ packages/odata-v2/src/request-builder/a-debug.spec.ts
@@ -1,33 +1,20 @@
 import { testService } from '@sap-cloud-sdk/test-services-odata-v2/test-service';
 import nock from 'nock';
-import {
-  ODataCreateRequestConfig,
-  ODataRequest
-} from '@sap-cloud-sdk/odata-common';
-import { createODataUri as createODataUriV2 } from '@sap-cloud-sdk/odata-v2/internal';
-import {
-  defaultDestination,
-  defaultHost
-} from '../../../../test-resources/test/test-util';
+import { defaultDestination } from '../../../../test-resources/test/test-util';
 import { CreateRequestBuilder } from './create-request-builder';
 
 describe('debug', () => {
   const { testEntityApi } = testService();
 
   it('create', async () => {
-    const requestConfig = new ODataCreateRequestConfig(
-      testEntityApi,
-      createODataUriV2(testEntityApi.deSerializers)
-    );
+    nock('http://example.com')
+      .head('/sap/opu/odata/sap/API_TEST_SRV')
+      .reply(200);
 
-    const request = new ODataRequest(requestConfig, defaultDestination);
-
-    nock(defaultHost).head(`/${request.relativeServiceUrl()}`).reply(200);
-
-    nock(defaultHost)
-      .post(`/${request.relativeServiceUrl()}`)
-      // .query({})
-      // .delay(0)
+    nock('http://example.com')
+      .post('/sap/opu/odata/sap/API_TEST_SRV')
+      .query({})
+      .delay(0)
       .reply(500, { d: undefined });
 
     const someEntity = testEntityApi.entityBuilder().stringProperty('').build();

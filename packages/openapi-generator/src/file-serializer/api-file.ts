6ce5e675b459a64c9974759a0eb1c626e70c53d1
i341658 <kavitha.sivakumar@sap.com>
2024-12-09 15:52:42 +0100

    chore:cleanup

--- packages/openapi-generator/src/file-serializer/api-file.ts
+++ packages/openapi-generator/src/file-serializer/api-file.ts
@@ -36,7 +36,9 @@ export function apiFile(
   const apiDoc = apiDocumentation(api, serviceName);
   const sanitizedBasePath = basePath ? removeSlashes(basePath) : '';
   // Add a leading slash if basePath is not empty, as all Path Items start with a slash according to Swagger 3
-  const defaultBasePath = sanitizedBasePath ? '/' + sanitizedBasePath : '';
+  const defaultBasePath = sanitizedBasePath
+    ? '/' + sanitizedBasePath
+    : undefined;
   const apiContent = codeBlock`
 export const ${api.name} = {
   _defaultBasePath: ${defaultBasePath ? `'${defaultBasePath}'` : undefined},

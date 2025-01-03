814a6c00761d6a86bf9c29abc265937bc839c001
Deeksha Sinha <88374536+deekshas8@users.noreply.github.com>
2025-01-03 15:58:47 +0100

    update (#5333)

--- test-packages/test-services-e2e/TripPin/microsoft-o-data-service-sample-trippin-in-memory-models-service/operations.js
+++ test-packages/test-services-e2e/TripPin/microsoft-o-data-service-sample-trippin-in-memory-models-service/operations.js
@@ -4,7 +4,7 @@ exports.operations = void 0;
 exports.getNearestAirport = getNearestAirport;
 exports.resetDataSource = resetDataSource;
 /*
- * Copyright (c) 2024 SAP SE or an SAP affiliate company. All rights reserved.
+ * Copyright (c) 2025 SAP SE or an SAP affiliate company. All rights reserved.
  *
  * This is a generated file powered by the SAP Cloud SDK for JavaScript.
  */
--- test-packages/test-services-odata-v4/test-service/operations.js
+++ test-packages/test-services-odata-v4/test-service/operations.js
@@ -20,7 +20,7 @@ exports.testActionImportSharedEntityReturnType = testActionImportSharedEntityRet
 exports.testActionImportSharedEntityReturnTypeCollection = testActionImportSharedEntityReturnTypeCollection;
 exports.testActionImportNullableTest = testActionImportNullableTest;
 /*
- * Copyright (c) 2024 SAP SE or an SAP affiliate company. All rights reserved.
+ * Copyright (c) 2025 SAP SE or an SAP affiliate company. All rights reserved.
  *
  * This is a generated file powered by the SAP Cloud SDK for JavaScript.
  */

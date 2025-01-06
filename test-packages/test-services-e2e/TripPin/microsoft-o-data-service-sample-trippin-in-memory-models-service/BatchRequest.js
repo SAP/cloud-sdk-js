92718fde6987a4abc551c3de0fd98997ffb7f318
Deeksha Sinha <88374536+deekshas8@users.noreply.github.com>
2025-01-06 12:31:40 +0100

    update clients (#5335)

--- test-packages/test-services-e2e/TripPin/microsoft-o-data-service-sample-trippin-in-memory-models-service/BatchRequest.js
+++ test-packages/test-services-e2e/TripPin/microsoft-o-data-service-sample-trippin-in-memory-models-service/BatchRequest.js
@@ -4,7 +4,7 @@ exports.defaultMicrosoftODataServiceSampleTrippinInMemoryModelsServicePath = voi
 exports.batch = batch;
 exports.changeset = changeset;
 /*
- * Copyright (c) 2024 SAP SE or an SAP affiliate company. All rights reserved.
+ * Copyright (c) 2025 SAP SE or an SAP affiliate company. All rights reserved.
  *
  * This is a generated file powered by the SAP Cloud SDK for JavaScript.
  */
--- test-packages/test-services-e2e/v4/test-service/operations.js
+++ test-packages/test-services-e2e/v4/test-service/operations.js
@@ -11,7 +11,7 @@ exports.returnSapCloudSdk = returnSapCloudSdk;
 exports.createTestEntityById = createTestEntityById;
 exports.createTestEntityByIdReturnId = createTestEntityByIdReturnId;
 /*
- * Copyright (c) 2024 SAP SE or an SAP affiliate company. All rights reserved.
+ * Copyright (c) 2025 SAP SE or an SAP affiliate company. All rights reserved.
  *
  * This is a generated file powered by the SAP Cloud SDK for JavaScript.
  */

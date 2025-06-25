9bfb72d7abd4773acbb9b824eff1d1e64f5ecb5c
i341658 <kavitha.sivakumar@sap.com>
2025-06-25 16:10:37 +0200

    chore: cleanup a bunch of things that are required

--- packages/connectivity/src/scp-cf/environment-accessor/xsuaa.ts
+++ packages/connectivity/src/scp-cf/environment-accessor/xsuaa.ts
@@ -1,35 +1,8 @@
 import { XsuaaService } from '@sap/xssec';
-import { getServiceCredentials } from './service-credentials';
-import type { JwtPayload } from '../jsonwebtoken-type';
 import type {
   ServiceCredentials,
   XsuaaServiceCredentials
 } from './environment-accessor-types';
-
-/**
- * @internal
- * Takes a decoded JWT and uses the client_id and audience claims to determine the XSUAA service instance
- * that issued the JWT. Returns the credentials if a match is found, otherwise throws an error.
- * If no decoded JWT is specified, then returns the first existing XSUAA credential service plan "application".
- * @param token - Either an encoded or decoded JWT.
- * @returns The credentials for a match, otherwise `null`.
- */
-export function getXsuaaServiceCredentials(
-  token?: JwtPayload | string
-): XsuaaServiceCredentials {
-  const credentials = getServiceCredentials<XsuaaServiceCredentials>(
-    'xsuaa',
-    token
-  );
-  if (!credentials) {
-    throw new Error(
-      token
-        ? 'Could not find XSUAA service binding matching the token.'
-        : 'Could not find XSUAA service binding.'
-    );
-  }
-  return credentials;
-}
 
 const xsuaaServices: Record<string, XsuaaService> = {};
 
@@ -46,7 +19,7 @@ export function clearXsuaaServices(): void {
  * @internal
  * @param options.disableCache - Value to enable or disable JWKS cache in xssec library. Defaults to false.
  * @param options.credentials - Xsuaa service credentials. Required to create the XSUAA instance.
- * @returns An instance of the xsuaa service for the provided credentials.
+ * @returns An instance of the xsuaa xssec service for the provided credentials.
  */
 export function getXsuaaInstanceFromSuppliedCredentials(options: {
   disableCache?: boolean;
@@ -69,6 +42,9 @@ export function getXsuaaInstanceFromSuppliedCredentials(options: {
   const cacheKey = `${credentials.clientid}:${disableCache}`;
 
   if (!xsuaaServices[cacheKey]) {
+    // XsuaaService is a class that extends Service from @sap/xssec and is a representation of XSUAA credentials
+    // extracted either from a reuse service like for e.g destination service
+    // or is a representation of a bound XSUAA service
     xsuaaServices[cacheKey] = new XsuaaService(
       credentials as XsuaaServiceCredentials,
       serviceConfig as any
--- packages/connectivity/src/scp-cf/xsuaa-service.ts
+++ packages/connectivity/src/scp-cf/xsuaa-service.ts
@@ -18,7 +18,7 @@ interface XsuaaParameters {
 }
 
 /**
- * Make a client credentials request against the XSUAA service.
+ * Make a client credentials request against the XSUAA credentials inside {@param service}.
  * @param service - Service as it is defined in the environment variable.
  * @param jwt - User JWT or object containing the `iss` property.
  * @returns Client credentials token.
@@ -67,7 +67,7 @@ export async function getClientCredentialsToken(
 }
 
 /**
- * Make a user token request against the XSUAA service.
+ * Make a user token request against the XSUAA credentials inside {@param service}.
  * @param service - Service as it is defined in the environment variable.
  * @param userJwt - User JWT.
  * @returns User token.

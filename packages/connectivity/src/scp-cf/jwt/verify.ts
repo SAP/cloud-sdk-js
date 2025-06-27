88b474c914594dd93db750d255da6317ca0012af
i341658 <kavitha.sivakumar@sap.com>
2025-06-27 10:03:55 +0200

    chore: add files back

---
+++ packages/connectivity/src/scp-cf/jwt/verify.ts
@@ -0,0 +1,50 @@
+import { createSecurityContext } from '@sap/xssec';
+import { ErrorWithCause } from '@sap-cloud-sdk/util';
+import {
+  getXsuaaInstanceFromSuppliedCredentials,
+  getXsuaaServiceCredentials
+} from '../environment-accessor';
+import type { JwtPayload } from '../jsonwebtoken-type';
+
+/**
+ * Verifies the given JWT and returns the decoded payload.
+ * @param jwt - JWT to be verified
+ * @param options - Options to control certain aspects of JWT verification behavior.
+ * @returns A Promise to the decoded and verified JWT.
+ * @internal
+ */
+export async function verifyJwt(
+  jwt: string,
+  options?: VerifyJwtOptions
+): Promise<JwtPayload> {
+  const disableCache = !{ ...defaultVerifyJwtOptions, ...options }
+    .cacheVerificationKeys;
+
+  const xsuaaServiceCredentials = getXsuaaServiceCredentials(jwt);
+  const xsuaaService = getXsuaaInstanceFromSuppliedCredentials({
+    disableCache,
+    credentials: xsuaaServiceCredentials
+  });
+
+  const { token } = await createSecurityContext(xsuaaService, {
+    jwt
+  }).catch(e => {
+    throw new ErrorWithCause('Failed to verify JWT.', e);
+  });
+
+  return token.payload;
+}
+
+/**
+ * Options to control certain aspects of JWT verification behavior.
+ */
+export interface VerifyJwtOptions {
+  /**
+   * The verification keys are cached if set to true.
+   */
+  cacheVerificationKeys?: boolean;
+}
+
+const defaultVerifyJwtOptions: VerifyJwtOptions = {
+  cacheVerificationKeys: true
+};

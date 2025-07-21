b79be5be3c9b2b175a50ed981e69d90141f66161
i341658 <kavitha.sivakumar@sap.com>
2025-07-21 16:45:44 +0200

    chore: fix version

--- packages/connectivity/src/scp-cf/jwt/verify.ts
+++ packages/connectivity/src/scp-cf/jwt/verify.ts
@@ -1,11 +1,11 @@
 /**
  * Options to control certain aspects of JWT verification behavior.
- * @deprecated Since v4.0.3. This interface is no longer used as the incoming JWT is not verified during destination retrieval.
+ * @deprecated Since v4.1.0. This interface is no longer used as the incoming JWT is not verified during destination retrieval.
  */
 export interface VerifyJwtOptions {
   /**
    * The verification keys are cached if set to true.
-   * @deprecated Since v4.0.3. This property has no effect as the incoming JWT is no longer verified during destination retrieval.
+   * @deprecated Since v4.1.0. This property has no effect as the incoming JWT is no longer verified during destination retrieval.
    */
   cacheVerificationKeys?: boolean;
 }

5f8c283464cabc4153a2eaaeecd509b22b8dd117
Marika Marszalkowski <marika.marszalkowski@sap.com>
2026-02-18 13:08:10 +0100

    chore: Move eslint rules to project config instead of package (#6337)

--- packages/connectivity/src/scp-cf/destination/destination-service.spec.ts
+++ packages/connectivity/src/scp-cf/destination/destination-service.spec.ts
@@ -1,9 +1,7 @@
 import * as jwt123 from 'jsonwebtoken';
 import nock from 'nock';
-// eslint-disable-next-line import/named
 import * as resilienceMethods from '@sap-cloud-sdk/resilience/internal';
 import { circuitBreakers } from '@sap-cloud-sdk/resilience/internal';
-// eslint-disable-next-line import/named
 import axios from 'axios';
 import { mockCertificateCall } from '../../../../../test-resources/test/test-util';
 import { destinationServiceUri } from '../../../../../test-resources/test/test-util/environment-mocks';

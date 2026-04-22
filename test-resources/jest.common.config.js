f6b9880936f7dfb60828daea502ad4279d38080a
David Knaack <david.knaack@sap.com>
2026-04-22 17:02:32 +0200

    revert cahgne

--- test-resources/jest.common.config.js
+++ test-resources/jest.common.config.js
@@ -5,12 +5,6 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest'
   },
   moduleFileExtensions: ['js', 'json', 'ts'],
-  moduleNameMapper: {
-    '^@sap-cloud-sdk/test-util-internal/(.+)$':
-      '<rootDir>/../../test-resources/test/test-util/$1.ts',
-    '^@sap-cloud-sdk/test-util-internal$':
-      '<rootDir>/../../test-resources/test/test-util/index.ts'
-  },
   globalSetup: '<rootDir>/../../test-resources/bootstrap-global.js',
   setupFiles: ['<rootDir>/../../test-resources/bootstrap-test.js'],
   setupFilesAfterEnv: [

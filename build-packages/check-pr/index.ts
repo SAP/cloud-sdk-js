cadfcb7066040221fabe09dfc0e515661e89d039
David Knaack <david.knaack@sap.com>
2026-05-20 15:44:01 +0200

    chore: Allow dependabot to user lowercase PR titles

--- build-packages/check-pr/index.ts
+++ build-packages/check-pr/index.ts
@@ -2,9 +2,18 @@ import { setFailed } from '@actions/core';
 import { context } from '@actions/github';
 import { validateTitle, validateBody } from './validators.js';
 
+function capitalizeAfterColon(title: string): string {
+  return title.replace(/^([^:]+:\s*)(\w)/, (_, pre, char) =>
+    pre + char.toUpperCase()
+  );
+}
+
 try {
-  validateTitle(context.payload.pull_request?.title);
-  validateBody(context.payload.pull_request?.body?.replace(/\r\n/g, '\n'));
+  const pr = context.payload.pull_request;
+  const isDependabot = pr?.user?.login === 'dependabot[bot]';
+  const title = isDependabot && pr?.title ? capitalizeAfterColon(pr.title) : pr?.title;
+  validateTitle(title);
+  validateBody(pr?.body?.replace(/\r\n/g, '\n'));
 } catch (err: any) {
   setFailed(err);
 }
